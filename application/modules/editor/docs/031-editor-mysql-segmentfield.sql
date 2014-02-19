35c941de9b3d761a6ffe5d6993aeae351325f315
Thomas Lauria <thomas@marcmittag.de>
2014-02-19 10:11:05 +0100

    unfied db changes

--- application/modules/editor/docs/031-editor-mysql-segmentfield.sql
+++ application/modules/editor/docs/031-editor-mysql-segmentfield.sql
@@ -1,41 +1,73 @@
--- phpMyAdmin SQL Dump
--- version 3.5.8.1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Feb 10, 2014 at 08:46 AM
--- Server version: 5.5.32
--- PHP Version: 5.4.17
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `translate5`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `LEK_segment_field`
---
-
+--  /*
+--  START LICENSE AND COPYRIGHT
+--  
+--  This file is part of Translate5 Editor PHP Serverside and build on Zend Framework
+--  
+--  Copyright (c) 2013 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+-- 
+--  Contact:  http://www.MittagQI.com/  /  service (ÄTT) MittagQI.com
+-- 
+--  This file may be used under the terms of the GNU General Public License version 3.0
+--  as published by the Free Software Foundation and appearing in the file gpl3-license.txt 
+--  included in the packaging of this file.  Please review the following information 
+--  to ensure the GNU General Public License version 3.0 requirements will be met:
+--  http://www.gnu.org/copyleft/gpl.html.
+-- 
+--  For this file you are allowed to make use of the same FLOSS exceptions to the GNU 
+--  General Public License version 3.0 as specified by Sencha for Ext Js. 
+--  Please be aware, that Marc Mittag / MittagQI take no warranty  for any legal issue, 
+--  that may arise, if you use these FLOSS exceptions and recommend  to stick to GPL 3. 
+--  For further information regarding this topic please see the attached license.txt
+--  of this software package.
+--  
+--  MittagQI would be open to release translate5 under EPL or LGPL also, if this could be
+--  brought in accordance with the ExtJs license scheme. You are welcome to support us
+--  with legal support, if you are interested in this.
+--  
+--  
+--  @copyright  Marc Mittag, MittagQI - Quality Informatics
+--  @author     MittagQI - Quality Informatics
+--  @license    GNU General Public License version 3.0 http://www.gnu.org/copyleft/gpl.html
+--              with FLOSS exceptions (see floss-exception.txt and ux-exception.txt at the root level)
+--  
+--  END LICENSE AND COPYRIGHT 
+--  */
+-- 
 CREATE TABLE IF NOT EXISTS `LEK_segment_field` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `taskGuid` varchar(38) NOT NULL COMMENT 'Foreign Key to LEK_task',
   `name` varchar(300) NOT NULL COMMENT 'contains the label without invalid chars',
   `label` varchar(300) NOT NULL COMMENT 'field label as provided by CSV / directory',
   `rankable` tinyint(1) NOT NULL COMMENT 'defines if this field is rankable in the ranker',
   `editable` tinyint(1) NOT NULL COMMENT 'defines if only the readOnly Content column is provided',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  PRIMARY KEY (`id`),
+  CONSTRAINT FOREIGN KEY (`taskGuid`) REFERENCES `LEK_task` (`taskGuid`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE IF NOT EXISTS `LEK_segment_history_data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `taskGuid` varchar(38) NOT NULL,
+  `segmentHistoryId` int(11) NOT NULL,
+  `name` varchar(300) NOT NULL,
+  `edited` longtext,
+  PRIMARY KEY (`id`),
+  CONSTRAINT FOREIGN KEY (`segmentHistoryId`) REFERENCES `LEK_segment_history` (`id`) ON DELETE CASCADE,
+  KEY `taskGuid` (`taskGuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `LEK_segment_data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `taskGuid` varchar(38) NOT NULL,
+  `name` varchar(300) NOT NULL,
+  `segmentId` int(11) NOT NULL,
+  `mid` varchar(1000) DEFAULT NULL,
+  `original` longtext NOT NULL,
+  `originalMd5` varchar(32) NOT NULL,
+  `originalToSort` varchar(300) DEFAULT NULL,
+  `edited` longtext,
+  `editedMd5` varchar(32) NOT NULL,
+  `editedToSort` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT FOREIGN KEY (`segmentId`) REFERENCES `LEK_segments` (`id`) ON DELETE CASCADE,
+  KEY `taskGuid` (`taskGuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
