11ba1e57e05a79058d20841711522fc798a13b98
Thomas Lauria <thomas@marcmittag.de>
2014-05-15 18:10:20 +0200

    TRANSLATE-113: use the configured workflow as initial value for tasks on import

--- application/modules/editor/Models/Import.php
+++ application/modules/editor/Models/Import.php
@@ -173,6 +173,7 @@ class editor_Models_Import {
      * @param string $importFolderPath
      */
     public function import(editor_Models_Import_DataProvider_Abstract $dataProvider) {
+        $config = Zend_Registry::get('config');
         if(is_null($this->_taskGuid)){
             throw new Zend_Exception('taskGuid not set - please set using $this->setTask');
         }
@@ -189,6 +190,8 @@ class editor_Models_Import {
             //reset given relais language value if no relais data is provided / feature is off
             $this->task->setRelaisLang(0); 
         }
+        
+        $this->task->setWorkflow(editor_Workflow_Abstract::getId($config->import->taskWorkflow));
         $this->task->setReferenceFiles($this->hasReferenceFiles());
         $this->task->save(); //Task erst Speichern wenn die obigen validates und checks durch sind.
         
