59f58aa332cf955cedf161b3a6d3b2621dc75d89
Stephan Bergmann <Stephan@MittagQI.com>
2014-11-25 15:37:56 +0100

    TRANSLATE-22: added TermTagger-Config.sql

---
+++ application/modules/editor/database/040-editor-mysql-TRANSLATE-22-TermTagger-config-inserts.sql
@@ -0,0 +1,41 @@
+--  /*
+--  START LICENSE AND COPYRIGHT
+--  
+--  This file is part of Translate5 Editor PHP Serverside and build on Zend Framework
+--  
+--  Copyright (c) 2013 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+-- 
+--  Contact:  http://www.MittagQI.com/  /  service (ÄTT) MittagQI.com
+-- 
+--  This file may be used under the terms of the GNU General Public License version 3.0
+--  as published by the Free Software Foundation and appearing in the file gpl3-license.txt 
+--  included in the packaging of this file.  Please review the following information 
+--  to ensure the GNU General Public License version 3.0 requirements will be met:
+--  http://www.gnu.org/copyleft/gpl.html.
+-- 
+--  For this file you are allowed to make use of the same FLOSS exceptions to the GNU 
+--  General Public License version 3.0 as specified by Sencha for Ext Js. 
+--  Please be aware, that Marc Mittag / MittagQI take no warranty  for any legal issue, 
+--  that may arise, if you use these FLOSS exceptions and recommend  to stick to GPL 3. 
+--  For further information regarding this topic please see the attached license.txt
+--  of this software package.
+--  
+--  MittagQI would be open to release translate5 under EPL or LGPL also, if this could be
+--  brought in accordance with the ExtJs license scheme. You are welcome to support us
+--  with legal support, if you are interested in this.
+--  
+--  
+--  @copyright  Marc Mittag, MittagQI - Quality Informatics
+--  @author     MittagQI - Quality Informatics
+--  @license    GNU General Public License version 3.0 http://www.gnu.org/copyleft/gpl.html
+--              with FLOSS exceptions (see floss-exception.txt and ux-exception.txt at the root level)
+--  
+--  END LICENSE AND COPYRIGHT 
+--  */
+-- 
+
+INSERT INTO Zf_configuration (`name`, `confirmed`, `module`, `category`, `value`, `default`, `defaults`, `type`, `description`) VALUES 
+('runtimeOptions.termTagger.url.default', 1, 'plugin', 'termtagger', '', '', '', 'list', 'List of available TermTagger-URLs. At least one available URL must be defined'),
+('runtimeOptions.termTagger.url.import', 1, 'plugin', 'termtagger', '', '', '', 'list', 'Optional list of TermTagger-URL to use for task-import processing. Fallback is list runtimeOptions.termTagger.url.default'),
+('runtimeOptions.termTagger.url.gui', 1, 'plugin', 'termtagger', '', '', '', 'list', 'Optional list of TermTagger-URL to use for gui-response processing. Fallback is list runtimeOptions.termTagger.url.default'),
+('runtimeOptions.termTagger.segmentsPerCall', 1, 'plugin', 'termtagger', '20', '20', '', 'integer', 'Maximal number of segments the TermTagger will process in one step');
