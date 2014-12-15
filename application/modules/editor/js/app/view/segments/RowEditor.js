01ff98c649d77585202d4d3c3fcfc25b372147cc
Thomas Lauria <thomas@marcmittag.de>
2014-12-15 09:42:45 +0100

    TRANSLATE-347 and TRANSLATE-348

--- application/modules/editor/js/app/view/segments/RowEditor.js
+++ application/modules/editor/js/app/view/segments/RowEditor.js
@@ -292,15 +292,17 @@ Ext.define('Editor.view.segments.RowEditor', {
             hasToSwap = false,
             fieldToDisable = null;
         
+        if(col.segmentField) {
+            me.mainEditor.fieldTypeToEdit = col.segmentField.get('type');
+        }
         //if user clicked on a not content column open default dataindex (also if it is a content column but not editable)
         if(!col.segmentField || !col.segmentField.get('editable')) {
             toEdit = firstTarget;
-            me.mainEditor.fieldTypeToEdit = col.segmentField.TYPE_TARGET;
+            me.mainEditor.fieldTypeToEdit = Editor.model.segment.Field.prototype.TYPE_TARGET;
         }
         //if its the readonly column take the edit one
         else if(col instanceof Editor.view.segments.column.Content) {
             toEdit = col.dataIndex+'Edit';
-            me.mainEditor.fieldTypeToEdit = col.segmentField.get('type');
         }
         //no swap if last edited column was the same
         hasToSwap = me.columnToEdit !== toEdit;
