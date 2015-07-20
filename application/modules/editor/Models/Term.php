translate5/translate5

commit 7e7a6d1d6d6c5086b0951ec76c2dbc62b48af454

TRANSLATE-477: get term infos from segment text

application/modules/editor/Models/Term.php
@@ -66,13 +66,13 @@ class editor_Models_Term extends ZfExtended_Models_Entity_Abstract {
     protected $dbInstanceClass = 'editor_Models_Db_Terms';
 
     /**
-     * Gibt für eine übergebene termId die mit dem zugehörigen Termentry assozierten Terme zurück. 
+     * returns for a termId the associated termentries by group 
      * @param string $taskGuid
      * @param string $termId
      * @param int $langId
      * @return array
      */
-    public function getTermEntryTermsByTaskGuidTermIdLangId($taskGuid, $termId,$langId) {
+    public function getTermGroupEntries($taskGuid, $termId,$langId) {
         $s1 = $this->db->getAdapter()->select()
         ->from(array('t1' => 'LEK_terms'),
                 array('t1.groupId'))
@@ -196,10 +196,32 @@ class editor_Models_Term extends ZfExtended_Models_Entity_Abstract {
      * @return type array values are the mids of the terms in the string
      */
     public function getTermMidsFromSegment(string $seg) {
-        $getTermIdRegEx = '/<div[^>]+data-tbxid="([^"]*)"[^>]*>/';
-        preg_match_all($getTermIdRegEx, $seg, $matches);
-        return $matches[1];
+        return array_keys($this->getTermInfosFromSegment($seg));
     }
+    
+    /**
+     * returns mids and term flags (css classes) found in a string
+     * @param string $seg
+     * @return array key is the mid, value is an array of term flags (css classes)
+     */
+    public function getTermInfosFromSegment(string $seg) {
+        $getTermRegEx = '/<div[^>]+((class="([^"]*)"[^>]+data-tbxid="([^"]*)")|(data-tbxid="([^"]*)"[^>]+class="([^"]*)"))[^>]*>/';
+        
+        preg_match_all($getTermRegEx, $seg, $matches, PREG_SET_ORDER);
+        $result = array();
+        foreach($matches as $match) {
+            //class before data-tbxid
+            if(empty($match[5])) {
+                $result[$match[4]] = explode(' ', $match[3]);
+            }
+            //data-tbxid before class 
+            else {
+                $result[$match[6]] = explode(' ', $match[7]);
+            }
+        }
+        return $result;
+    }
+    
     /**
      * Returns a multidimensional array.
      * 1. level: keys: groupId, values: array of terms grouped by groupId

commit 661d3fb6b605599b1a661eef0ed200e64558e08d

TRANSLATE-404: implement transFound/transNotFound implementation on translate5 side; Fix for bug which was introduced later by commit 7e7a6d1 2015-03-26 Approve TRANSLATE-477: get term infos from segment text AND another fix for marking the correct number of terms

application/modules/editor/Models/Term.php
@@ -187,10 +187,16 @@ class editor_Models_Term extends ZfExtended_Models_Entity_Abstract {
     
     /**
      * @param string $seg
+     * @param boolean $unify if true: Only one entry for identical mids in the return array
      * @return type array values are the mids of the terms in the string
      */
-    public function getTermMidsFromSegment(string $seg) {
-        return array_keys($this->getTermInfosFromSegment($seg));
+    public function getTermMidsFromSegment(string $seg,$unify=true) {
+        if($unify)
+            return array_keys($this->getTermInfosFromSegment($seg));
+        
+        $getTermIdRegEx = '/<div.*?data-tbxid="(term_.*?)".*?>/';
+        preg_match_all($getTermIdRegEx, $seg, $matches);
+        return $matches[1];
     }
     
     /**

application/modules/editor/Plugins/TermTagger/Worker/Abstract.php
@@ -149,8 +149,8 @@ abstract class editor_Plugins_TermTagger_Worker_Abstract extends ZfExtended_Work
             $seg->source = preg_replace('" ?transN?o?t?Found ?"', ' ', $seg->source);
             $seg->target = preg_replace('" ?transN?o?t?Found ?"', ' ', $seg->target);
 
-            $sourceMids = $this->termModel->getTermMidsFromSegment($seg->source);
-            $targetMids = $this->termModel->getTermMidsFromSegment($seg->target);
+            $sourceMids = $this->termModel->getTermMidsFromSegment($seg->source,false);
+            $targetMids = $this->termModel->getTermMidsFromSegment($seg->target,false);
             $toMarkMemory = array();
             foreach ($sourceMids as $sourceMid) {
                 $groupedTerms = $this->termModel->getAllTermsOfGroupByMid($this->task->getTaskGuid(),$sourceMid, array($this->task->getTargetLang()));
@@ -187,7 +187,7 @@ abstract class editor_Plugins_TermTagger_Worker_Abstract extends ZfExtended_Work
                     $modifiedMatch = str_replace('<div', '<div class=""', $modifiedMatch);
                 }
                 $modifiedMatch = preg_replace('/( class="[^"]*)"/', '\\1 '.$cssClassToInsert.'"', $modifiedMatch);
-                $seg = str_replace($match, $modifiedMatch, $seg);
+                $seg = preg_replace('/'.$match.'/', $modifiedMatch, $seg, 1);
             }
         };
         