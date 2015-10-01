4489ad5fb228c6379cd00cc6da126ed9c8f164f3
Thomas Lauria <thomas@marcmittag.de>
2015-10-01 19:06:14 +0200

    enable config controller

--- application/modules/editor/Bootstrap.php
+++ application/modules/editor/Bootstrap.php
@@ -92,7 +92,7 @@ class Editor_Bootstrap extends Zend_Application_Module_Bootstrap
     {
         $restRoute = new Zend_Rest_Route($this->front, array(), array(
             'editor' => array(  'file', 'segment', 'alikesegment', 'referencefile', 'qmstatistics', 'comment',
-                                'task', 'user', 'taskuserassoc', 'segmentfield', 'workflowuserpref', 'worker', 'taskmeta'),
+                                'task', 'user', 'taskuserassoc', 'segmentfield', 'workflowuserpref', 'worker', 'taskmeta', 'config'),
         ));
         $this->front->getRouter()->addRoute('editorRestDefault', $restRoute);
 
