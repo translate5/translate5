888d77288c034150ea855b1fd865f853299e85f7
Thomas Lauria <thomas@marcmittag.de>
2015-10-23 10:11:53 +0200

    Error 'Result of opening a TBX was not OK' less verbose in error mail, logs to error_log now

--- application/modules/editor/Plugins/TermTagger/Service.php
+++ application/modules/editor/Plugins/TermTagger/Service.php
@@ -177,10 +177,11 @@ class editor_Plugins_TermTagger_Service {
         }
         if(!$this->wasSuccessfull()) {
             $msg = 'TermTagger HTTP Status was: '.$this->getLastStatus();
-            $msg .= "\n URL: ".$httpClient->getUri(true)."\n\nRequested Data: ";
-            $msg .= print_r($serverCommunication,true)."\n\nPlain Server Response: ";
-            $msg .= print_r($response,true);
-            $this->log->logError('Result of opening a TBX was not OK!', $msg);
+            $msg .= "\n URL: ".$httpClient->getUri(true);
+            $this->log->logError('Result of opening a TBX was not OK! More details in error log.', $msg);
+            $msg .= "\n\nPlain Server Response: ".print_r($response,true);
+            $msg .= "\n\nRequested Data: ".print_r($serverCommunication,true);
+            error_log($msg);
             return null;
         }
         
