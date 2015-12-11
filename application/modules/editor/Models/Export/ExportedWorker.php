5ac50b4167279c73e25ae3fdf318271bf2ad07ff
Thomas Lauria <thomas@marcmittag.de>
2015-12-11 21:45:54 +0100

    BEOSPHERE-60: notify beo-data about finished export after all workers are finished

---
+++ application/modules/editor/Models/Export/ExportedWorker.php
@@ -0,0 +1,65 @@
+<?php
+/*
+START LICENSE AND COPYRIGHT
+
+ This file is part of translate5
+ 
+ Copyright (c) 2013 - 2015 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+
+ Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
+
+ This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
+ as published by the Free Software Foundation and appearing in the file agpl3-license.txt 
+ included in the packaging of this file.  Please review the following information 
+ to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3.0 requirements will be met:
+ http://www.gnu.org/licenses/agpl.html
+
+ There is a plugin exception available for use with this release of translate5 for
+ open source applications that are distributed under a license other than AGPL:
+ Please see Open Source License Exception for Development of Plugins for translate5
+ http://www.translate5.net/plugin-exception.txt or as plugin-exception.txt in the root
+ folder of translate5.
+  
+ @copyright  Marc Mittag, MittagQI - Quality Informatics
+ @author     MittagQI - Quality Informatics
+ @license    GNU AFFERO GENERAL PUBLIC LICENSE version 3 with plugin-execptions
+			 http://www.gnu.org/licenses/agpl.html http://www.translate5.net/plugin-exception.txt
+
+END LICENSE AND COPYRIGHT
+*/
+
+/**
+ * This class provides a general Worker which can be configured with a callback method which.
+ * This class is designed for simple workers which dont need a own full blown worker class.
+ * 
+ * The following parameters are needed: 
+ * class → the class which should be instanced on work. Classes with Constructor Parameters are currently not supported!
+ * method → the class method which is called on work. The method receives the taskguid and the whole parameters array.
+ * 
+ * Be careful: This class can not be used in worker_dependencies !
+ */
+class editor_Models_Export_ExportedWorker extends ZfExtended_Worker_Abstract {
+    /**
+     * (non-PHPdoc)
+     * @see ZfExtended_Worker_Abstract::validateParameters()
+     */
+    protected function validateParameters($parameters = array()) {
+        return empty($parameters);
+    }
+    
+    /**
+     * (non-PHPdoc)
+     * @see ZfExtended_Worker_Abstract::work()
+     */
+    public function work() {
+        $task = ZfExtended_Factory::get('editor_Models_Task');
+        /* @var $task editor_Models_Task */
+        $task->loadByTaskGuid($this->taskGuid);
+        
+        //no unlocking or so here, since task locking on export and should be reworked so that a lockjing is not necessary anymore.
+        
+        $eventManager = ZfExtended_Factory::get('ZfExtended_EventManager', array(__CLASS__));
+        $eventManager->trigger('exportCompleted', $this, array('task' => $task));
+        return true;
+    }
+}
