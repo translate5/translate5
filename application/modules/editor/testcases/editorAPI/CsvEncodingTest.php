2df38f4014ac37b5b10cbdab5ab9e83a07904a00
Thomas Lauria <thomas@marcmittag.de>
2016-01-19 11:43:07 +0100

    Fixing license issues

--- application/modules/editor/testcases/editorAPI/CsvEncodingTest.php
+++ application/modules/editor/testcases/editorAPI/CsvEncodingTest.php
@@ -1,4 +1,38 @@
 <?php
+/*
+START LICENSE AND COPYRIGHT
+
+ This file is part of translate5
+ 
+ Copyright (c) 2013 - 2015 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+
+ Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
+
+ This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
+ as published by the Free Software Foundation and appearing in the file agpl3-license.txt 
+ included in the packaging of this file.  Please review the following information 
+ to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3.0 requirements will be met:
+ http://www.gnu.org/licenses/agpl.html
+
+ There is a plugin exception available for use with this release of translate5 for
+ open source applications that are distributed under a license other than AGPL:
+ Please see Open Source License Exception for Development of Plugins for translate5
+ http://www.translate5.net/plugin-exception.txt or as plugin-exception.txt in the root
+ folder of translate5.
+  
+ @copyright  Marc Mittag, MittagQI - Quality Informatics
+ @author     MittagQI - Quality Informatics
+ @license    GNU AFFERO GENERAL PUBLIC LICENSE version 3 with plugin-execptions
+			 http://www.gnu.org/licenses/agpl.html http://www.translate5.net/plugin-exception.txt
+
+END LICENSE AND COPYRIGHT
+*/
+
+/**
+ * CsvEncodingTest imports a CSV with several special characters
+ * The test task will be edited and exported. The generated changes.xml and 
+ * exported file will then be checked for correct encoded content.
+ */
 class CsvEncodingTest extends \ZfExtended_Test_ApiTestcase {
     /**
      * Setting up the test task by fresh import, adds the lector and translator users
