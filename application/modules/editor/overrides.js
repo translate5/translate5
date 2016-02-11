6bf5a0077ccb4a91ee5c910461742653b045c018
Thomas Lauria <thomas@marcmittag.de>
2016-02-11 11:20:14 +0100

    EXT6UPD-33: PageMap Errors after using filters after using the roweditor

--- application/modules/editor/overrides.js
+++ application/modules/editor/overrides.js
@@ -4,3 +4,19 @@ Ext.define('Ext.ux.fixed.BufferedStore', {
         return this.indexOf(record) > -1;
     }
 });
+
+
+/**
+ * Fix for EXT6UPD-33
+ * needed for ext-6.0.0
+ * should be solved natively with ext-6.0.1
+ */
+Ext.define('Ext.ux.fixed.PageMap', {
+    override: 'Ext.data.PageMap',
+    getByInternalId: function(internalId) {
+        var index = this.indexMap[internalId];
+        if (index != null) {
+            return this.getAt(index);
+        }
+    }
+});
