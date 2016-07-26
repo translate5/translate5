18f7367d9cf6f961794cf760efb0b5cb2da88cb2
Thomas Lauria <thomas@mittagqi.com>
2016-07-26 21:28:55 +0200

    enable application zip override on commandline

--- install-and-update.sh
+++ install-and-update.sh
@@ -6,4 +6,10 @@ CMD_MYSQL=/usr/bin/mysql
 type -P $CMD_PHP &>/dev/null || { echo "$CMD_PHP not found. Set \$CMD_PHP in $0"; exit 1; }
 type -P $CMD_MYSQL &>/dev/null || { echo "$CMD_MYSQL not found. Set \$CMD_MYSQL in $0"; exit 1; }
 
-$CMD_PHP -r "require_once('application/modules/default/Models/Installer/Standalone.php'); Models_Installer_Standalone::mainLinux(array('mysql_bin' => '$CMD_MYSQL'));"
+if [ "$1" == "" ]; then 
+ZIP_OVERRIDE=""
+else
+ZIP_OVERRIDE=",'applicationZipOverride' => '$1'"
+fi;
+
+$CMD_PHP -r "require_once('application/modules/default/Models/Installer/Standalone.php'); Models_Installer_Standalone::mainLinux(array('mysql_bin' => '$CMD_MYSQL'${ZIP_OVERRIDE}));"
