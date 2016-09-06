9a8d6ea5fec9cf13c28cc2faf2e26f37d8c0ec1f
aleksandar <aleksandar@mittagqi.com>
2016-09-06 10:10:09 +0200

    TRANSLATE-637: Inform users about new features (plugin comit)

---
+++ application/modules/editor/Plugins/ChangeLog/Init.php
@@ -0,0 +1,96 @@
+<?php
+/*
+START LICENSE AND COPYRIGHT
+
+ This file is part of translate5
+ 
+ Copyright (c) 2013 - 2015 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+
+ Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
+
+ This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
+ as published by the Free Software Foundation and appearing in the file agpl3-license.txt 
+ included in the packaging of this file.  Please review the following information 
+ to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3.0 requirements will be met:
+ http://www.gnu.org/licenses/agpl.html
+
+ There is a plugin exception available for use with this release of translate5 for
+ open source applications that are distributed under a license other than AGPL:
+ Please see Open Source License Exception for Development of Plugins for translate5
+ http://www.translate5.net/plugin-exception.txt or as plugin-exception.txt in the root
+ folder of translate5.
+  
+ @copyright  Marc Mittag, MittagQI - Quality Informatics
+ @author     MittagQI - Quality Informatics
+ @license    GNU AFFERO GENERAL PUBLIC LICENSE version 3 with plugin-execptions
+			 http://www.gnu.org/licenses/agpl.html http://www.translate5.net/plugin-exception.txt
+
+END LICENSE AND COPYRIGHT
+*/
+
+class editor_Plugins_ChangeLog_Init extends ZfExtended_Plugin_Abstract {
+       
+    /**
+     * Contains the Plugin Path relativ to APPLICATION_PATH or absolut if not under APPLICATION_PATH
+     * @var array
+     */
+    protected $frontendControllers = array(
+        'pluginChangeLogChangelog' => 'Editor.plugins.ChangeLog.controller.Changelog',
+    );
+    
+    protected $localePath = 'locales';
+    
+    public function getFrontendControllers() {
+        $result = array();
+        $userSession = new Zend_Session_Namespace('user');
+        if(empty($userSession) || empty($userSession->data)) {
+            return $result;
+        }
+        $acl = ZfExtended_Acl::getInstance();
+        /* @var $acl ZfExtended_Acl */
+        if(!$acl->has('frontend')) {
+            return $result;
+        }
+        foreach($this->frontendControllers as $right => $controller) {
+            if($acl->isInAllowedRoles($userSession->data->roles, 'frontend', $right)) {
+                $result[] = $controller;
+            }
+        }
+        return $result;
+    }
+    public function init() {
+        $this->initEvents();
+        $this->initRoutes();
+    }
+    
+    protected function initEvents() {
+       
+    }
+    
+    public function injectFrontendConfig(Zend_EventManager_Event $event) {
+       
+    }
+    
+    public function initJsTranslations(Zend_EventManager_Event $event) {
+        $view = $event->getParam('view');
+        $view->pluginLocale()->add($this, 'views/localizedjsstrings.phtml');
+    }
+    
+    
+    /**
+     * defines all URL routes of this plug-in
+     */
+    protected function initRoutes() {
+        $f = Zend_Registry::get('frontController');
+        /* @var $f Zend_Controller_Front */
+        $f->addControllerDirectory(APPLICATION_PATH.'/'.$this->getPluginPath().'/Controllers', '_plugins_'.__CLASS__);
+        $r = $f->getRouter();
+        
+        $restRoute = new Zend_Rest_Route($f, array(), array(
+                'editor' => array('plugins_changelog_changelog',
+                ),
+        ));
+        $r->addRoute('plugins_changelog_restdefault', $restRoute);
+ 
+    }
+}
