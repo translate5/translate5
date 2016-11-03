1dd801fd32b9eceedecdb088f80573440d9861d3
Thomas Lauria <thomas@marcmittag.de>
2016-11-03 15:33:29 +0100

    changelog

---
+++ application/modules/editor/database/094-editor-mysql-CHANGELOG-2016-11-03.sql
@@ -0,0 +1,30 @@
+-- /*
+-- START LICENSE AND COPYRIGHT
+-- 
+--  This file is part of translate5
+--  
+--  Copyright (c) 2013 - 2015 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+-- 
+--  Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
+-- 
+--  This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
+--  as published by the Free Software Foundation and appearing in the file agpl3-license.txt 
+--  included in the packaging of this file.  Please review the following information 
+--  to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3.0 requirements will be met:
+--  http://www.gnu.org/licenses/agpl.html
+-- 
+--  There is a plugin exception available for use with this release of translate5 for
+--  open source applications that are distributed under a license other than AGPL:
+--  Please see Open Source License Exception for Development of Plugins for translate5
+--  http://www.translate5.net/plugin-exception.txt or as plugin-exception.txt in the root
+--  folder of translate5.
+--   
+--  @copyright  Marc Mittag, MittagQI - Quality Informatics
+--  @author     MittagQI - Quality Informatics
+--  @license    GNU AFFERO GENERAL PUBLIC LICENSE version 3 with plugin-execptions
+-- 			 http://www.gnu.org/licenses/agpl.html http://www.translate5.net/plugin-exception.txt
+-- 
+-- END LICENSE AND COPYRIGHT
+-- */
+
+INSERT INTO `LEK_change_log` (`dateOfChange`, `jiraNumber`, `type`, `title`, `description`, `userGroup`) VALUES ('2016-11-03', 'TRANSLATE-758', 'bugfix', 'DbUpdater under Windows can not deal with DB Passwords with special characters', 'Only Windows users were affected by this issue, when using a MySQL DB Password with special characters. In this case the special character was removed by a PHP method before passing the password to MySQL. Then the script could not authenticate itself to the MySQL DB.', '8'),('2016-11-03', 'TRANSLATE-761', 'bugfix', 'Task was not reloaded completely when switching from state import to open', 'This inclompete reload led to the problem that the source and target columns were missing when opening this task directly after import.', '12');
