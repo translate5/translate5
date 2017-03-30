71154e628f1fdb62d91f6ae30ab612029585e7eb
Thomas Lauria <thomas@mittagqi.com>
2017-03-30 15:30:10 +0200

    TRANSLATE-830: Integrate OpenTM2 Installer into translate5 installer and updater - downloader empty target fix

--- application/modules/default/Models/Installer/Standalone.php
+++ application/modules/default/Models/Installer/Standalone.php
@@ -48,7 +48,7 @@ class Models_Installer_Standalone {
      * Increase this value to force a restart of the updater while updating
      * @var integer
      */
-    const INSTALLER_VERSION = 2;
+    const INSTALLER_VERSION = 3;
     
     /**
      * @var string
