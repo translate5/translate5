bea7f590a88d872024f5e0aafffd3d972c5be27c
Thomas Lauria <thomas@mittagqi.com>
2017-04-06 10:08:21 +0200

    MITTAGQI-36: Add new license plug-in exception

--- application/modules/default/database/016-sql-changelog-2016-10-17.sql
+++ application/modules/default/database/016-sql-changelog-2016-10-17.sql
@@ -1,3 +1,31 @@
+-- /*
+-- START LICENSE AND COPYRIGHT
+-- 
+--  This file is part of translate5
+--  
+--  Copyright (c) 2013 - 2017 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+-- 
+--  Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
+-- 
+--  This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
+--  as published by the Free Software Foundation and appearing in the file agpl3-license.txt 
+--  included in the packaging of this file.  Please review the following information 
+--  to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3 requirements will be met:
+--  http://www.gnu.org/licenses/agpl.html
+--   
+--  There is a plugin exception available for use with this release of translate5 for
+--  translate5 plug-ins that are distributed under GNU AFFERO GENERAL PUBLIC LICENSE version 3:
+--  Please see http://www.translate5.net/plugin-exception.txt or plugin-exception.txt in the root
+--  folder of translate5.
+--   
+--  @copyright  Marc Mittag, MittagQI - Quality Informatics
+--  @author     MittagQI - Quality Informatics
+--  @license    GNU AFFERO GENERAL PUBLIC LICENSE version 3 with plugin-execption
+-- 			 http://www.gnu.org/licenses/agpl.html http://www.translate5.net/plugin-exception.txt
+-- 
+-- END LICENSE AND COPYRIGHT
+-- */
+
 INSERT INTO `LEK_change_log` (`dateOfChange`, `jiraNumber`, `title`, `description`, `userGroup`) VALUES ('2016-10-17', 'TRANSLATE-726', 'New Column "type" in ChangeLog Plugin', 'A new column „type“ was added in the change-log grid, to show directly the type (bugfix, new feature, change) of a change log entry.', '14'),
 ('2016-10-17', 'TRANSLATE-743', 'Implement filters in change-log grid', 'The change-log grid can be filtered right now.', '14'),
 ('2016-10-17', 'TRANSLATE-612', 'User-Authentication via API - enable session deletion, login counter', 'Already existing sessions can now be deleted via API, the counter of invalid logins increases on invalid logins via API (restrictions are currently not implemented).', '8'),
