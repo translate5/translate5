3bcd8ab386dfaf32530cfbdbbffdcc28f8b0ffa1
Livewin Windows Machine <support@translate5.net>
2017-12-05 10:25:13 +0100

    TRANSLATE-1076: install-and-update script overwrites custom mysql path

--- install-and-update.bat
+++ install-and-update.bat
@@ -1,3 +1,29 @@
-echo "Please note: This file is made to update an installation based on translate5-windows-installer.exe. If you did a manual windows install of translate5, you may need to adjust the paths to your php.exe and mysql.exe"
-"c:\xampp\php\php.exe" -r "require_once('application/modules/default/Models/Installer/Standalone.php'); Models_Installer_Standalone::mainLinux(array('mysql_bin' => 'c:\xampp\mysql\bin\mysql.exe'));"
+@echo off
+cls
+set CONFIG_FILE=windows-installer-config.ini
+if NOT exist %CONFIG_FILE% (
+  echo.
+  echo   You are using the installer the first time!
+  echo   The file 'windows-installer-config.ini' was generated right now 
+  echo   with default configuration values: 
+  echo.
+  echo   INSTALL_PHP_PATH=c:\xampp\php\php.exe
+  echo   INSTALL_MYSQL_PATH=c:\xampp\mysql\bin\mysql.exe
+  echo.
+  echo   Please adjust the paths to php.exe and mysql.exe in that config file, 
+  echo   if you did a manual windows installation of translate5.
+  echo INSTALL_PHP_PATH=c:\xampp\php\php.exe > %CONFIG_FILE%
+  echo INSTALL_MYSQL_PATH=c:\xampp\mysql\bin\mysql.exe >> %CONFIG_FILE%
+  exit /B
+)
+
+for /f "delims=" %%x in (%CONFIG_FILE%) do (set "%%x")
+echo.
+echo   Starting translate5 installer / updater:
+echo.
+echo   using as php.exe:  %INSTALL_PHP_PATH%
+echo   using as mysql.exe:  %INSTALL_MYSQL_PATH%
+echo.
+"%INSTALL_PHP_PATH%" -r "require_once('application/modules/default/Models/Installer/Standalone.php'); Models_Installer_Standalone::mainLinux(array('mysql_bin' => '%INSTALL_MYSQL_PATH%'));"
+echo.
 pause
