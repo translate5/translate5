06ae5786ffc6761a2572507a8d1267b410ee929b
Thomas Lauria <thomas@mittagqi.com>
2018-03-14 10:56:44 +0100

    introducing install-and-update2 based on modules, bringing all the tools also to windows

---
+++ install-and-update2.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+CMD_PHP=/usr/bin/php
+CMD_MYSQL=/usr/bin/mysql
+ 
+# make sure PHP and MySQL binary exist; else die with an error message
+type -P $CMD_PHP &>/dev/null || { echo "$CMD_PHP not found. Set \$CMD_PHP in $0"; exit 1; }
+type -P $CMD_MYSQL &>/dev/null || { echo "$CMD_MYSQL not found. Set \$CMD_MYSQL in $0"; exit 1; }
+
+MODULE=$1
+shift
+$CMD_PHP -r "require_once('application/modules/default/Models/Installer/Standalone2.php'); \
+\$config = ['mysql_bin' => '$CMD_MYSQL', 'arguments' => \$_SERVER['argv'], 'module' => '$MODULE']; \
+Models_Installer_Standalone2::mainLinux(\$config);" -- "$@"
