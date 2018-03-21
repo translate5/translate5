e4742a8ddcecb27d47c88d71dfa4e188cfbe739e
Ines-Paul <ines-paul@mittagqi.com>
2018-03-21 10:52:06 +0100

    (refactored code according to usage as collecting data into ONE ToolTip)

--- application/modules/editor/js/app/view/ToolTip.js
+++ application/modules/editor/js/app/view/ToolTip.js
@@ -59,10 +59,9 @@ Ext.define('Editor.view.ToolTip', {
         beforeshow : function(tip) {
             var t = tip.triggerElement,
                 fly = Ext.fly(t); 
-            if(fly.hasCls('qmflag')) {
-                this.handleQmFlag(t, tip);
-            } else if (fly.hasCls('trackchanges')) {
-                this.handleTrackChanges(t, tip);
+            if(fly.hasCls('qmflag') || fly.hasCls('trackchanges')) {
+                // Don't show multiple ToolTips that overlap, but collect data into one single ToolTip
+                this.handleCollectedTooltip(t, tip);
             }
             //else if hasClass for other ToolTip Types
         }
@@ -84,24 +83,47 @@ Ext.define('Editor.view.ToolTip', {
         return this.callParent(arguments);
     },
     
-    handleQmFlag: function(t, tip) {
+    /**
+     * Collect data for"common" ToolTip.
+     * First the node itself is checked, but (if necessary) be careful to check for parent or child-Nodes, too.
+     */
+    handleCollectedTooltip: function(node, tip) {
         var me = this,
-            qmFlagData = me.getQmFlagData(t),
-            meta = {
-                qmFlag: qmFlagData
-            }
-        // add tooltip for trackChanges?
-        if (/(^|[\s])trackchanges([\s]|$)/.test(t.parentNode.className)) {
-            meta.trackChanges = me.getTrackChangesData(t.parentNode);
+            fly = Ext.fly(node),
+            tplData = {};
+        // 'default'
+        tplData.qmFlag = '';
+        tplData.trackChanges = '';
+        // add tooltip for qmFlag?
+        if(fly.hasCls('qmflag')) {
+            tplData.qmFlag = me.getQmFlagData(node);
         } else {
-            meta.trackChanges = '';
+            var allQmFlagNodes = node.getElementsByClassName('qmflag');
+            if (allQmFlagNodes.length == 1) {
+                tplData.qmFlag = me.getQmFlagData(allQmFlagNodes[0]);
+            } else {
+                // a) there is no qmFlag-Node
+                // b) there are many qmFlag-Nodes and we don't know which exactly the mouseover refers to
+                tplData.qmFlag = '';
+            }
         }
-        if(!me.qmflagTpl) {
-            me.qmflagTpl = new Ext.Template('{qmFlag}{trackChanges}');
-            me.qmflagTpl.compile();
+        // add tooltip for trackChanges?
+        if(fly.hasCls('trackchanges')) {
+            tplData.trackChanges = me.getTrackChangesData(node);
+        } else if (/(^|[\s])trackchanges([\s]|$)/.test(node.parentNode.className)) {
+            tplData.trackChanges = me.getTrackChangesData(node.parentNode);
         }
-        tip.update(me.qmflagTpl.apply(meta));		
+        if(!me.toolTipCollectedTpl) {
+            me.toolTipCollectedTpl = new Ext.Template('{qmFlag}{trackChanges}');
+            me.toolTipCollectedTpl.compile();
+        }
+        tip.update(me.toolTipCollectedTpl.apply(tplData));
     },
+    
+    // ------------------------------------------------------------------
+    // ----------------- get data ---------------------------------------
+    // ------------------------------------------------------------------
+    
     getQmFlagData: function(node) {
         var me = this, 
             qmtype,
@@ -121,30 +143,8 @@ Ext.define('Editor.view.ToolTip', {
         } else {
             meta.comment = '<br />'+ meta.comment
         }
-        // For Tooltip:
+        // => For Tooltip:
         return '<b>'+meta.qmtype+'</b><br />'+meta.sevTitle+': '+meta.sev+meta.comment+'<br />';
-    },
-    
-    handleTrackChanges: function(node, tip) {
-        var me = this,
-            trackChangesData = me.getTrackChangesData(node),
-            tplData = {
-                trackChanges: trackChangesData
-            };
-        // add tooltip for qmFlag?
-        var allQmFlagNodes = node.getElementsByClassName('qmflag');
-        if (allQmFlagNodes.length == 1) {
-            tplData.qmFlag = me.getQmFlagData(allQmFlagNodes[0]);
-        } else {
-            // a) there is no qmFlag-Node
-            // b) there are many qmFlag-Nodes and we don't know which exactly the mouseover refers to
-            tplData.qmFlag = '';
-        }
-        if(!me.trackChangesTpl) {
-            me.trackChangesTpl = new Ext.Template('{qmFlag}{trackChanges}');
-            me.trackChangesTpl.compile();
-        }
-        tip.update(me.trackChangesTpl.apply(tplData));
     },
     getTrackChangesData: function(node) {
         var me = this,
@@ -208,6 +208,8 @@ Ext.define('Editor.view.ToolTip', {
         // => For Tooltip:
         return '<b>'+nodeAction+'</b><br>'+nodeUser+'<br>'+nodeDate+'<br>'+ nodeHistory;
     },
+    
+    // ------------------------------------------------------------------
     
     /**
      * Override of default setTarget, only change see below.
