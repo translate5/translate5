f74d7beecb6909f812e8b9cd946161d36595cb2f
Thomas Lauria <thomas@marcmittag.de>
2018-05-09 15:41:40 +0200

    changelog

---
+++ application/modules/editor/database/sql-changelog-2018-05-09.sql
@@ -0,0 +1,30 @@
+
+-- /*
+-- START LICENSE AND COPYRIGHT
+-- 
+--  This file is part of translate5
+--  
+--  Copyright (c) 2013 - 2017 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+-- 
+--  Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
+-- 
+--  This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
+--  as published by the Free Software Foundation and appearing in the file agpl3-license.txt 
+--  included in the packaging of this file.  Please review the following information 
+--  to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3 requirements will be met:
+--  http://www.gnu.org/licenses/agpl.html
+--   
+--  There is a plugin exception available for use with this release of translate5 for
+--  translate5: Please see http://www.translate5.net/plugin-exception.txt or 
+--  plugin-exception.txt in the root folder of translate5.
+--   
+--  @copyright  Marc Mittag, MittagQI - Quality Informatics
+--  @author     MittagQI - Quality Informatics
+--  @license    GNU AFFERO GENERAL PUBLIC LICENSE version 3 with plugin-execption
+-- 			 http://www.gnu.org/licenses/agpl.html http://www.translate5.net/plugin-exception.txt
+-- 
+-- END LICENSE AND COPYRIGHT
+-- */
+
+INSERT INTO `LEK_change_log` (`dateOfChange`, `jiraNumber`, `type`, `title`, `description`, `userGroup`) VALUES ('2018-05-09', 'TRANSLATE-1243', 'bugfix', 'IE 11: Could not start the application due to an error in Segment.js', 'IE 11: Could not start the application due to an error in Segment.js', '14'),
+('2018-05-09', 'TRANSLATE-1239', 'bugfix', 'JS: Uncaught TypeError: Cannot read property \'length\' of undefined', 'This error happens only with very small amount of segments in the task, and if the users presses then segment grid reload.', '8');
