647921995caf2f1f12e42737730e4dab87105852
Ines-Paul <ines-paul@mittagqi.com>
2018-07-25 13:00:29 +0200

    TRANSLATE-1372 Multiple empty spaces from TrackChanges after export - replace not only exactly two, but multiple spaces with one space after removing deleted words with one OR MORE spaces at both sides

--- application/modules/editor/Models/Segment/TrackChangeTag.php
+++ application/modules/editor/Models/Segment/TrackChangeTag.php
@@ -171,13 +171,13 @@ class editor_Models_Segment_TrackChangeTag extends editor_Models_Segment_TagAbst
     /**
      * removes TrackChanges-Tags:
      * - INS => markup-Tag ONLY is removed (doing this first is important in order to catch the spaces in the next step:)
-     * - DEL => avoid double space after removing a deleted word with one space at both sides
+     * - DEL => avoid multiple space after removing a deleted word with one or more space at both sides
      * - DEL => markup-Tag AND content inbetween is removed
      */
     public function removeTrackChanges(string $segment) {
         $segment = $this->protect($segment);
         $segment= preg_replace(self::REGEX_INS, '', $segment);
-        $segment= preg_replace('/ <'.self::PLACEHOLDER_TAG_DEL.'[^>]+> /', ' ', $segment);
+        $segment= preg_replace('/ +<'.self::PLACEHOLDER_TAG_DEL.'[^>]+> +/', ' ', $segment);
         $segment= preg_replace('/<'.self::PLACEHOLDER_TAG_DEL.'[^>]+>/', '', $segment);
         return $segment;
     }
