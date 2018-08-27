09dda27b0fe73a5f4bbcb3f6c74e15655c709a3e
aleksandar <aleksandar@mittagqi.com>
2018-08-27 19:27:26 +0200

    TRANSLATE-1342 Administration of engines and resources, customers and users (rfc to iso language mapping sql)

---
+++ application/modules/editor/database/151-editor-mysql-TRANSLATE-1342-instanttranslate-language-rfc-iso.sql
@@ -0,0 +1,82 @@
+/*
+START LICENSE AND COPYRIGHT
+
+ This file is part of translate5
+ 
+ Copyright (c) 2013 - 2017 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+
+ Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
+
+ This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
+ as published by the Free Software Foundation and appearing in the file agpl3-license.txt 
+ included in the packaging of this file.  Please review the following information 
+ to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3 requirements will be met:
+ http://www.gnu.org/licenses/agpl.html
+  
+ There is a plugin exception available for use with this release of translate5 for
+ translate5 plug-ins that are distributed under GNU AFFERO GENERAL PUBLIC LICENSE version 3:
+ Please see http://www.translate5.net/plugin-exception.txt or plugin-exception.txt in the root
+ folder of translate5.
+  
+ @copyright  Marc Mittag, MittagQI - Quality Informatics
+ @author     MittagQI - Quality Informatics
+ @license    GNU AFFERO GENERAL PUBLIC LICENSE version 3 with plugin-execption
+			 http://www.gnu.org/licenses/agpl.html http://www.translate5.net/plugin-exception.txt
+
+END LICENSE AND COPYRIGHT
+*/
+
+UPDATE `LEK_languages` SET `iso6393`='ger' WHERE `rfc5646`='de';
+UPDATE `LEK_languages` SET `iso6393`='eng' WHERE `rfc5646`='en';
+UPDATE `LEK_languages` SET `iso6393`='spa' WHERE `rfc5646`='es';
+UPDATE `LEK_languages` SET `iso6393`='eng' WHERE `rfc5646`='en-GB';
+UPDATE `LEK_languages` SET `iso6393`='eng' WHERE `rfc5646`='en-US';
+UPDATE `LEK_languages` SET `iso6393`='fra' WHERE `rfc5646`='fr';
+UPDATE `LEK_languages` SET `iso6393`='ita' WHERE `rfc5646`='it';
+UPDATE `LEK_languages` SET `iso6393`='bul' WHERE `rfc5646`='bg';
+UPDATE `LEK_languages` SET `iso6393`='dan' WHERE `rfc5646`='da';
+UPDATE `LEK_languages` SET `iso6393`='est' WHERE `rfc5646`='ee';
+UPDATE `LEK_languages` SET `iso6393`='fin' WHERE `rfc5646`='fi';
+UPDATE `LEK_languages` SET `iso6393`='gre' WHERE `rfc5646`='el';
+UPDATE `LEK_languages` SET `iso6393`='hrv' WHERE `rfc5646`='hr';
+UPDATE `LEK_languages` SET `iso6393`='nld' WHERE `rfc5646`='nl';
+UPDATE `LEK_languages` SET `iso6393`='cht' WHERE `rfc5646`='zh-TW';
+UPDATE `LEK_languages` SET `iso6393`='chi' WHERE `rfc5646`='zh-CN';
+UPDATE `LEK_languages` SET `iso6393`='ara' WHERE `rfc5646`='ar-SA';
+UPDATE `LEK_languages` SET `iso6393`='bul' WHERE `rfc5646`='bg-BG';
+UPDATE `LEK_languages` SET `iso6393`='cze' WHERE `rfc5646`='cs-CZ';
+UPDATE `LEK_languages` SET `iso6393`='dan' WHERE `rfc5646`='da-DK';
+UPDATE `LEK_languages` SET `iso6393`='ger' WHERE `rfc5646`='de-DE';
+UPDATE `LEK_languages` SET `iso6393`='gre' WHERE `rfc5646`='el-GR';
+UPDATE `LEK_languages` SET `iso6393`='spa' WHERE `rfc5646`='es-ES';
+UPDATE `LEK_languages` SET `iso6393`='est' WHERE `rfc5646`='et-EE';
+UPDATE `LEK_languages` SET `iso6393`='per' WHERE `rfc5646`='fa-IR';
+UPDATE `LEK_languages` SET `iso6393`='fin' WHERE `rfc5646`='fi-FI';
+UPDATE `LEK_languages` SET `iso6393`='fra' WHERE `rfc5646`='fr-FR';
+UPDATE `LEK_languages` SET `iso6393`='heb' WHERE `rfc5646`='he-IL';
+UPDATE `LEK_languages` SET `iso6393`='hin' WHERE `rfc5646`='hi-IN';
+UPDATE `LEK_languages` SET `iso6393`='hrv' WHERE `rfc5646`='hr-HR';
+UPDATE `LEK_languages` SET `iso6393`='hun' WHERE `rfc5646`='hu-HU';
+UPDATE `LEK_languages` SET `iso6393`='ind' WHERE `rfc5646`='id-ID';
+UPDATE `LEK_languages` SET `iso6393`='ita' WHERE `rfc5646`='it-IT';
+UPDATE `LEK_languages` SET `iso6393`='jpn' WHERE `rfc5646`='ja-JP';
+UPDATE `LEK_languages` SET `iso6393`='kor' WHERE `rfc5646`='ko-KR';
+UPDATE `LEK_languages` SET `iso6393`='lit' WHERE `rfc5646`='lt-LT';
+UPDATE `LEK_languages` SET `iso6393`='lav' WHERE `rfc5646`='lv-LV';
+UPDATE `LEK_languages` SET `iso6393`='may' WHERE `rfc5646`='ms-MY';
+UPDATE `LEK_languages` SET `iso6393`='mlt' WHERE `rfc5646`='mt-MT';
+UPDATE `LEK_languages` SET `iso6393`='nor' WHERE `rfc5646`='nb-NO';
+UPDATE `LEK_languages` SET `iso6393`='dut' WHERE `rfc5646`='nl-NL';
+UPDATE `LEK_languages` SET `iso6393`='pol' WHERE `rfc5646`='pl-PL';
+UPDATE `LEK_languages` SET `iso6393`='por' WHERE `rfc5646`='pt-PT';
+UPDATE `LEK_languages` SET `iso6393`='rum' WHERE `rfc5646`='ro-RO';
+UPDATE `LEK_languages` SET `iso6393`='rus' WHERE `rfc5646`='ru-RU';
+UPDATE `LEK_languages` SET `iso6393`='slo' WHERE `rfc5646`='sk-SK';
+UPDATE `LEK_languages` SET `iso6393`='slv' WHERE `rfc5646`='sl-SI';
+UPDATE `LEK_languages` SET `iso6393`='alb' WHERE `rfc5646`='sq-AL';
+UPDATE `LEK_languages` SET `iso6393`='swe' WHERE `rfc5646`='sv-SE';
+UPDATE `LEK_languages` SET `iso6393`='tha' WHERE `rfc5646`='th-TH';
+UPDATE `LEK_languages` SET `iso6393`='tur' WHERE `rfc5646`='tr-TR';
+UPDATE `LEK_languages` SET `iso6393`='ukr' WHERE `rfc5646`='uk-UA';
+UPDATE `LEK_languages` SET `iso6393`='urd' WHERE `rfc5646`='ur-PK';
+UPDATE `LEK_languages` SET `iso6393`='vie' WHERE `rfc5646`='vi-VN';
