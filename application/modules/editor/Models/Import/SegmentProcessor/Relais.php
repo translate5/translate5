c37d971aa247579d67cad71f906792a3d56a198c
Thomas Lauria <thomas@marcmittag.de>
2018-09-27 20:24:55 +0200

    TRANSLATE-1437: Collect relais file alignment errors instead mail and log each error separatly

--- application/modules/editor/Models/Import/SegmentProcessor/Relais.php
+++ application/modules/editor/Models/Import/SegmentProcessor/Relais.php
@@ -171,17 +171,17 @@ class editor_Models_Import_SegmentProcessor_Relais extends editor_Models_Import_
             ];
             if(!empty($this->errors['source-not-found'])){
                 $errors[] = 'For the following MIDs the source segment was not found to the MID of relais file. The Relais segment was ignored.';
-                $errors[] = join($this->errors['source-not-found'])."\n";
+                $errors[] = join(', ', $this->errors['source-not-found'])."\n";
             }
             if(!empty($this->errors['source-different'])){
                 $errors[] = 'Source of relais file is not identical with source of translated file.';
                 $errors[] = 'Relais target is left empty.';
-                $errors[] = join($this->errors['source-different'])."\n";
+                $errors[] = join("\n", $this->errors['source-different'])."\n";
             }
             if(!empty($this->errors['source-missing'])){
                 $errors[] = 'Errors in adding relais segment: Source of original segment and source of relais segment are identical,';
                 $errors[] = 'but still original Segment not found in the database:';
-                $errors[] = join($this->errors['source-missing'])."\n";
+                $errors[] = join("\n", $this->errors['source-missing'])."\n";
             }
             
             $log = ZfExtended_Factory::get('ZfExtended_Log');
