6041fdf1b7e31d1b6ab807c9a7fd9ba377bac42b
Thomas Lauria <thomas@marcmittag.de>
2018-11-08 19:07:05 +0100

    T5DEV-253: Open Issues MatchAnalysis and Pre-Translation - wrong validation value

--- application/modules/editor/Models/Validator/Segment.php
+++ application/modules/editor/Models/Validator/Segment.php
@@ -76,7 +76,7 @@ class editor_Models_Validator_Segment extends ZfExtended_Models_Validator_Abstra
         $this->addValidator('userGuid', 'guid');
         $this->addValidator('userName', 'stringLength', array('min' => 0, 'max' => 255)); //es wird kein assoc Array benötigt, aber so ist besser lesbar
         $this->addValidator('taskGuid', 'guid');
-        $this->addValidator('matchRate', 'between', array('min' => 0, 'max' => 103));
+        $this->addValidator('matchRate', 'between', array('min' => 0, 'max' => 104));
         $this->addValidator('matchRateType', 'stringLength', array('min' => 0, 'max' => 60));
         $this->addValidator('workflowStepNr', 'int');
         
