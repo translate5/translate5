864eb04c11882d880a4528d04c204d4a0e681e54
aleksandar <aleksandar@mittagqi.com>
2019-01-19 11:04:09 +0100

    TRANSLATE-1541 For title tag of TermPortal and InstantTranslate translation mechanism is not used

--- application/modules/editor/Controllers/TermportalController.php
+++ application/modules/editor/Controllers/TermportalController.php
@@ -30,6 +30,12 @@ class Editor_TermportalController extends ZfExtended_Controllers_Action {
     
     public function indexAction(){
 
+        Zend_Layout::getMvcInstance()->setLayout('termportal');
+        Zend_Layout::getMvcInstance()->setLayoutPath(APPLICATION_PATH.'/modules/editor/layouts/scripts');
+        $this->view->render('termportal/layoutConfig.php');
+        
+        $this->view->Php2JsVars()->set('termportal.restPath', APPLICATION_RUNDIR.'/'.Zend_Registry::get('module').'/');
+        
         //if term param exist, open this term on portal load
         if($this->getRequest()->getParam('term')!=null){
             $this->view->Php2JsVars()->set('term', $this->getRequest()->getParam('term'));
@@ -53,11 +59,6 @@ class Editor_TermportalController extends ZfExtended_Controllers_Action {
         
         $model=ZfExtended_Factory::get('editor_Models_Languages');
         /* @var $model editor_Models_Languages */
-        
-        Zend_Layout::getMvcInstance()->setLayout('termportal');
-        Zend_Layout::getMvcInstance()->setLayoutPath(APPLICATION_PATH.'/modules/editor/layouts/scripts');
-        $this->view->render('termportal/layoutConfig.php');
-        
         
         $collection=ZfExtended_Factory::get('editor_Models_TermCollection_TermCollection');
         /* @var $collection editor_Models_TermCollection_TermCollection */
@@ -130,9 +131,6 @@ class Editor_TermportalController extends ZfExtended_Controllers_Action {
         $this->view->collectionIds=$collectionIds;
         
         $this->view->preselectedLang=$preselectedLang;
-        
-        
-        $this->view->restPath=APPLICATION_RUNDIR.'/'.Zend_Registry::get('module').'/';
         
         $this->view->Php2JsVars()->set('termStatusMap', $config->runtimeOptions->tbx->termLabelMap->toArray());
         $this->view->Php2JsVars()->set('termStatusLabel', [
