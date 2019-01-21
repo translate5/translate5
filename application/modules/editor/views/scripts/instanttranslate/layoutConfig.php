dd59824a9be71523e0636f012a87f3722740d31f
aleksandar <aleksandar@mittagqi.com>
2019-01-21 14:28:24 +0100

    TRANSLATE-1541 For title tag of TermPortal and InstantTranslate translation mechanism is not used

--- application/modules/editor/views/scripts/instanttranslate/layoutConfig.php
+++ application/modules/editor/views/scripts/instanttranslate/layoutConfig.php
@@ -1,7 +1,3 @@
 <?php 
-$this->layout()->title = $this->headTitle();//get the default title 
-
-//add the default CSS. When overwriting this file
-//FIXME das hier mit client specific testen!
-//$this->headLink()->appendStylesheet(APPLICATION_RUNDIR.'/client-specific/instanttranslate.css');
-
+$translate = ZfExtended_Zendoverwrites_Translate::getInstance();
+$this->headTitle($translate->_('translate5-termportal'));
