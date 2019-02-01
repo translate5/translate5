141dcd68fdec8c4082236977f71aa126fba15d0a
aleksandar <aleksandar@mittagqi.com>
2019-02-01 15:09:48 +0100

    TRANSLATE-1230 ExtJS and therefore Translate5 is not usable on browsers with touch device

--- application/modules/editor/overrides.js
+++ application/modules/editor/overrides.js
@@ -1007,3 +1007,37 @@ Ext.override(Ext.grid.feature.GroupStore, {
         return ret;
     }
 });
+
+/**
+ * Workaround for bug in ExtJs 6.2.0.
+ * Resolved in current yet unreleased version
+ * Fix link: https://stackoverflow.com/questions/43236899/extjs-6-2-classic-does-not-work-with-firefox-and-a-touchscreen
+ */
+Ext.define('My.override.dom.Element', {
+    override: 'Ext.dom.Element'
+},
+function(){
+    var additiveEvents = this.prototype.additiveEvents,
+        eventMap = this.prototype.eventMap;
+    if(Ext.supports.TouchEvents && Ext.firefoxVersion >= 52 && Ext.os.is.Desktop){
+    	console.log("call")
+        eventMap['touchstart'] = 'mousedown';
+        eventMap['touchmove'] = 'mousemove';
+        eventMap['touchend'] = 'mouseup';
+        eventMap['touchcancel'] = 'mouseup';
+        eventMap['click'] = 'click';
+        eventMap['dblclick'] = 'dblclick';
+        additiveEvents['mousedown'] = 'mousedown';
+        additiveEvents['mousemove'] = 'mousemove';
+        additiveEvents['mouseup'] = 'mouseup';
+        additiveEvents['touchstart'] = 'touchstart';
+        additiveEvents['touchmove'] = 'touchmove';
+        additiveEvents['touchend'] = 'touchend';
+        additiveEvents['touchcancel'] = 'touchcancel';
+
+        additiveEvents['pointerdown'] = 'mousedown';
+        additiveEvents['pointermove'] = 'mousemove';
+        additiveEvents['pointerup'] = 'mouseup';
+        additiveEvents['pointercancel'] = 'mouseup';
+    }
+});
