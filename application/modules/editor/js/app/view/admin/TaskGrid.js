d38759cf658f073105ee4deb6cf8ab336fece39f
aleksandar <aleksandar@mittagqi.com>
2019-02-27 10:42:17 +0100

    TRANSLATE-1581 Mail to from task list project manager name column

--- application/modules/editor/js/app/view/admin/TaskGrid.js
+++ application/modules/editor/js/app/view/admin/TaskGrid.js
@@ -458,7 +458,7 @@ Ext.define('Editor.view.admin.TaskGrid', {
             	  	  ret=v;
             	  if(Editor.data.frontend.tasklist.pmMailTo){
             		  tooltip=rec.get('pmMail');
-            		  ret='<a alt="'+tooltip+'" href=mailto:'+tooltip+'>'+v+'</a>';
+            		  ret='<a alt="'+tooltip+'" href="mailto:'+tooltip+'">'+v+'</a>';
             		  meta.tdAttr = 'data-qtip="'+tooltip+'"';
             	  }
                   return ret;
