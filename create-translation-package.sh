ecdf6d432c5247cc1a005b73bce37970480b5486
Thomas Lauria <thomas@mittagqi.com>
2019-03-04 10:42:14 +0100

    cleans the target tag

--- create-translation-package.sh
+++ create-translation-package.sh
@@ -4,12 +4,21 @@ if [ -f $ZIP ]; then
     rm $ZIP
 fi
 find . -not -path "*/data/*" -iname "de.xliff"|while read FILENAME; do
+  # copy to zxliff
+  ZXLIFFNAME=`echo $FILENAME | sed -e "s/xliff$/zxliff/"`
+  cp $FILENAME $ZXLIFFNAME
+  # remove target content
+  perl -pi -e 's/<target>.*?<\/target>/<target><\/target>/gms' $ZXLIFFNAME
   # add to zip
-  zip $ZIP $FILENAME
-  FILENAME=`echo $FILENAME | sed -e "s/^.\///"`
-  NEWNAME=`echo "proofRead/$FILENAME" | sed -e "s/xliff$/zxliff/"`
-  # rename in zip, add proofread and convert to zxliff
+  zip $ZIP $ZXLIFFNAME
+  rm $ZXLIFFNAME
+  # rename in zip, add proofread
+  FILENAME=`echo $ZXLIFFNAME | sed -e "s/^.\///"`
+  NEWNAME=`echo "proofRead/$FILENAME"`
   printf "@ ${FILENAME}\n@=${NEWNAME}\n" | zipnote -w $ZIP
 done
-
+echo
+echo "created $ZIP"
+echo " Remove non wanted private Plugin XLFs!"
+echo " After translating and exporting from Translate5 rename *.zxliff back to *.xliff"
 
