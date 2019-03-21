09cb8e9710e7a6397266c03042029aa6967288dd
Thomas Lauria <thomas@mittagqi.com>
2019-03-21 15:49:19 +0100

    Add english translation as relais language

--- create-translation-package.sh
+++ create-translation-package.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-ZIP=translate5-de-de.zip
+ZIP=translate5-de-en-TARGET.zip
 if [ -f $ZIP ]; then
     rm $ZIP
 fi
@@ -8,13 +8,23 @@ find . -not -path "*/data/*" -iname "de.xliff"|while read FILENAME; do
   ZXLIFFNAME=`echo $FILENAME | sed -e "s/xliff$/zxliff/"`
   cp $FILENAME $ZXLIFFNAME
   # remove target content
-  perl -pi -e 's/<target>.*?<\/target>/<target><\/target>/gms' $ZXLIFFNAME
+  perl -0777 -pi -e 's/<target>.*?<\/target>/<target><\/target>/gs' $ZXLIFFNAME
   # add to zip
   zip $ZIP $ZXLIFFNAME
   rm $ZXLIFFNAME
   # rename in zip, add proofread
   FILENAME=`echo $ZXLIFFNAME | sed -e "s/^.\///"`
   NEWNAME=`echo "proofRead/$FILENAME"`
+  printf "@ ${FILENAME}\n@=${NEWNAME}\n" | zipnote -w $ZIP
+done
+
+#add en as relais content
+find . -not -path "*/data/*" -iname "en.xliff"|while read FILENAME; do
+  # add to zip
+  zip $ZIP $FILENAME
+  # rename in zip, add proofread
+  FILENAME=`echo $FILENAME | sed -e "s/^.\///"`
+  NEWNAME=`echo "relais/$FILENAME" | sed -e "s/en.xliff$/de.zxliff/"`
   printf "@ ${FILENAME}\n@=${NEWNAME}\n" | zipnote -w $ZIP
 done
 echo
