d80978571b683e08d1e0fc8995a7de0c0bdf6b05
Thomas Lauria <thomas@marcmittag.de>
2019-04-16 18:27:20 +0200

    fix archive tables

--- application/modules/editor/Plugins/ArchiveTaskBeforeDelete/DbTables.php
+++ application/modules/editor/Plugins/ArchiveTaskBeforeDelete/DbTables.php
@@ -71,7 +71,7 @@ class editor_Plugins_ArchiveTaskBeforeDelete_DbTables {
             'LEK_plugin_segmentstatistic_terms' => 'taskGuid',
             'LEK_plugin_segmentstatistics' => 'taskGuid',
             'LEK_qmsubsegments' => 'taskGuid',
-            'LEK_request_log' => false, //its just logging, we dont archive that
+            'LEK_workflow_log' => false, //its just logging, we dont archive that
             'LEK_segment_data' => 'taskGuid',
             'LEK_segment_field' => 'taskGuid',
             'LEK_segment_history' => 'taskGuid',
