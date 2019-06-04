ce62cad33faf8baa12e54e47b6a169ca7eac24ea
ines_paul <ines-paul@mittagqi.com>
2019-06-04 09:59:13 +0200

    TRANSLATE-1277 Proposing of termEntries and terms - proposing new Term: show termcollection-select with the same skeleton and at the same position - proposing new Term: fixes for default data - proposal-buttons for attribute: position fixed

--- application/modules/editor/termportal/js/Term.js
+++ application/modules/editor/termportal/js/Term.js
@@ -41,7 +41,7 @@ const Term={
             this.$_resultTermsHolderHeader=$('#resultTermsHolderHeader');
             this.$_termTable=$('#termTable');
             this.$_termEntryAttributesTable = $('#termAttributeTable');
-            this.$_termCollectionSelect = $('#termCollectionSelect');
+            this.$_termCollectionSelect = $('#termcollectionSelectContainer');
             
             // TODO: these are the same as above => choose ONE and use those throughout
             this.$_TermEntryHeader = this.$_resultTermsHolderHeader
@@ -81,6 +81,12 @@ const Term={
 				collectionIds = me.getFilteredCollections(),
                 processStats = me.getFilteredProcessStats();
 			
+			// "reset" data for proposing a new Term
+            me.newTermAttributes = Attribute.renderNewTermAttributes(); // currently just an empty Array
+            me.newTermCollectionId = null;  // will be selected by user
+            me.newTermGroupId = null;       // will be set from result's select-list
+            me.newTermLanguageId = null;    // will be selected by user
+            
 			if(!lng){
 				lng=$("input[name='language']:checked").val();
 			}
@@ -254,15 +260,16 @@ const Term={
 			var me=this;
 		    console.log("findTermsAndAttributes() for: " + termGroupid);
 		    Attribute.languageDefinitionContent=[];
-		    
+            
+            // data for proposing a new Term
+            me.newTermGroupId = termGroupid;
+            
 		    //check the cache
 		    if(!me.reloadTermEntry && me.termGroupsCache[termGroupid]){
 		    	TermEntry.drawTermEntryAttributes(me.termGroupsCache[termGroupid].rows[TermEntry.KEY_TERM_ENTRY_ATTRIBUTES]);
 		        me.drawTermGroups(me.termGroupsCache[termGroupid].rows[me.KEY_TERM_ATTRIBUTES]);
 		        return;
 		    }
-		    
-		    me.newTermGroupId = termGroupid;
 		    
 		    $.ajax({
 		        url: Editor.data.termportal.restPath+"termcollection/searchattribute",
@@ -619,15 +626,22 @@ const Term={
             console.log('renderNewTermData...');
             var me = this,
                 newTermData = {},
-                attributes = me.newTermAttributes, // eg via Attribute.renderNewTermAttributes()
-                collectionId = me.newTermCollectionId,
-                groupId = me.newTermGroupId,
-                languageId = me.newTermLanguageId,
-                termName = "Add new term-proposal"; // TODO: use translation
+                attributes,
+                collectionId,
+                groupId,
+                languageId,
+                termName;
+            // "default": blank term within TermEntry:
+            attributes = me.newTermAttributes;
+            collectionId = me.newTermCollectionId;
+            groupId = me.newTermGroupId;
+            languageId = me.newTermLanguageId;
+            termName = "Add new term-proposal"; // TODO: use translation
+            // if a search has no result:
             if (me.$_searchErrorNoResults.is(":visible")) {
-                termName = $('#search').val();
 	            languageId = $('#language').val();
-	            console.log('renderNewTermData for searched item: ' + termName);
+                termName = $('#search').val();
+                console.log('renderNewTermData for searched item: ' + termName);
             }
             newTermData = {0: {
                 'attributes': attributes,
@@ -653,11 +667,6 @@ const Term={
             var me = eventData.data.scope,
                 filteredCollections = me.getFilteredCollections();
             console.log('onAddTermEntryClick');
-            // "reset" data for new Term
-            me.newTermAttributes = null;
-            me.newTermCollectionId = null;
-            me.newTermGroupId = null;
-            me.newTermLanguageId = null;
             if (filteredCollections.length == 1) {
                 collectionId = filteredCollections[0];
                 me.drawTermProposal();
@@ -732,7 +741,7 @@ const Term={
             }
             console.log('choose collection (filteredCollections: ' + filteredCollections.length + ')');
             me.emptyResultTermsHolder(false);
-            me.$_resultTermsHolderHeader.hide();
+            me.$_resultTermsHolderHeader.show();
             me.$_resultTermsHolder.show();
             collectionSelectHeader = '<h3>Choose a collection for the new TermEntry:</h3>'; // TODO: use translation
             if (filteredCollections.length == 0) {
@@ -747,10 +756,11 @@ const Term={
                 collectionSelectOptions += '<option value="'+filteredCollectionId+'">'+collectionsNames[filteredCollectionId]+'</option>';
             }
             collectionSelect = '<select name="chooseCollection" id="chooseCollection">'+collectionSelectOptions+'</select>';
-            $('#termCollectionSelect').append(collectionSelectHeader).append(collectionSelect);
+            me.$_termCollectionSelect.append(collectionSelectHeader).append(collectionSelect).show();
             $('#chooseCollection').selectmenu({
                 select: function() {
                     me.newTermCollectionId = $(this).val();
+                    me.$_termCollectionSelect.empty().hide();
                     me.drawTermProposal();
                 }
             });
