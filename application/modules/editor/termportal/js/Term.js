274e062adfe19cb780db984389f44e62712b3874
aleksandar <aleksandar@mittagqi.com>
2019-07-04 20:55:39 +0200

    T5DEV-270 Open issues TermProposal system

--- application/modules/editor/termportal/js/Term.js
+++ application/modules/editor/termportal/js/Term.js
@@ -661,7 +661,7 @@ const Term={
             var me = this,
                 languageSelect,
                 html = '';
-            if (!Editor.data.app.user.isInstantTranslateAllowed) {
+            if (!Editor.data.app.user.isTermProposalAllowed || !Editor.data.app.user.isInstantTranslateAllowed) {
                 console.log('do NOT renderInstantTranslateIntegrationForTerm');
                 return '';
             }
