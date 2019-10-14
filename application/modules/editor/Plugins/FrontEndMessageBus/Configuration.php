277efde25ab6564033dcb074bc8fa1b805e721ed
aleksandar <aleksandar@mittagqi.com>
2019-10-14 14:25:42 +0200

    TRANSLATE-1167 Edit task simultanously with multiple users

---
+++ application/modules/editor/Plugins/FrontEndMessageBus/Configuration.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ START LICENSE AND COPYRIGHT
+
+ This file is part of translate5
+
+ Copyright (c) 2013 - 2017 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+
+ Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
+
+ This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
+ as published by the Free Software Foundation and appearing in the file agpl3-license.txt
+ included in the packaging of this file.  Please review the following information
+ to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3 requirements will be met:
+ http://www.gnu.org/licenses/agpl.html
+
+ There is a plugin exception available for use with this release of translate5 for
+ translate5: Please see http://www.translate5.net/plugin-exception.txt or
+ plugin-exception.txt in the root folder of translate5.
+
+ @copyright  Marc Mittag, MittagQI - Quality Informatics
+ @author     MittagQI - Quality Informatics
+ @license    GNU AFFERO GENERAL PUBLIC LICENSE version 3 with plugin-execption
+ http://www.gnu.org/licenses/agpl.html http://www.translate5.net/plugin-exception.txt
+
+ END LICENSE AND COPYRIGHT
+ */
+
+/**
+ * Message bus client config
+ */
+class editor_Plugins_FrontEndMessageBus_Configuration{
+    
+    /***
+     * Configured message bus client domain
+     * @var string
+     */
+    protected $domain;
+    
+    /***
+     * Configured message bus client server port
+     * @var string
+     */
+    protected $port;
+    
+    public function __construct() {
+        //$db = Zend_Db_Table::getDefaultAdapter();
+        //$config = Zend_Registry::get('config');
+        //TODO: from where do we get the config from
+        //TODO: new config for the domain and port ? 
+        //or use the existing domain config: runtimeOptions.server.name
+        $this->domain='translate5.local';
+        $this->port='9056';
+    }
+    
+    public function getDomain():string{
+        return $this->domain;
+    }
+    
+    public function getPort():string {
+        return $this->port;
+    }
+    
+}
