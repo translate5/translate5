abce0953956dbef897268f4af63d6398afb220d6
Thomas Lauria <thomas@marcmittag.de>
2020-03-24 15:28:20 +0100

    TRANSLATE-2004: Send termtagger-down-mails only to admin users of instance which are triggering the termtagger

--- application/modules/editor/Workflow/Abstract.php
+++ application/modules/editor/Workflow/Abstract.php
@@ -295,6 +295,12 @@ abstract class editor_Workflow_Abstract {
             $this->newTask = $event->getParam('task');
             $this->handleBeforeImport();
         });
+        
+        $events->attach('editor_Models_Import_Worker_FinalStep', 'importCompleted', function(Zend_EventManager_Event $event){
+            $this->newTask = $event->getParam('task');
+            $this->importConfig = $event->getParam('importConfig');
+            $this->handleImportCompleted();
+        });
     }
     
     /**
@@ -1421,6 +1427,11 @@ abstract class editor_Workflow_Abstract {
      * will be called directly before import is started, task is already created and available
      */
     abstract protected function handleBeforeImport();
+    
+    /**
+     * will be called after import (in set task to open worker) after the task is opened and the import is complete.
+     */
+    abstract protected function handleImportCompleted();
     
     /**
      * will be called after a user has finished a task
