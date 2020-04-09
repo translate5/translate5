2f98d774bd50efb456333d6d91189af95c9be9cf
Axel <axel@mittagqi.com>
2020-04-09 15:36:01 +0200

    Starting Font-parsing Feature: * Datamodel definitions * DB-datamodel classes * Datamodel Validator classes * Model classes * Font-Replacer Subworker TextReflow Converter * added fonts to rexroth testcase * added rexroth-full testcase, that makes problems in chrome

--- application/modules/editor/Utils.php
+++ application/modules/editor/Utils.php
@@ -25,6 +25,48 @@ END LICENSE AND COPYRIGHT
 /* 
  */
 class editor_Utils {
+    
+    /**
+     * Some general mappings to turn UTF chars to ascii chars (e.g. ü => ue, ß => sz, etc)
+     * among other adjustments for unwanted special chars like +, (, )
+     * @var array
+     */
+    private static $asciiMap = [
+        'Ä' => 'ae', 'Ü' => 'ue', 'Ö' => 'oe', 'ä' => 'ae', 'ü' => 'ue', 'ö' => 'oe', 'ß' => 'ss', 'Þ' => 'th', 'þ' => 'th', 'Ð' => 'dh', 'ð' => 'dh', 'Œ' => 'oe', 'œ' => 'oe', 'Æ' => 'ae', 'æ' => 'ae', 'µ' => 'u', 'Š' => 's', 'Ž' => 'z', 'š' => 's',
+        'ž' => 'z', 'Ÿ' => 'y', 'À' => 'a', 'Á' => 'a', 'Â' => 'a', 'Ã' => 'a', 'Å' => 'a', 'Ç' => 'c', 'Č' => 'c', 'È' => 'e', 'É' => 'e', 'Ê' => 'e', 'Ë' => 'e', 'Ì' => 'i', 'Í' => 'i', 'Î' => 'i', 'Ï' => 'i', 'Ñ' => 'n', 'Ò' => 'o', 'Ó' => 'o',
+        'Ô' => 'o', 'Õ' => 'o', 'Ø' => 'o', 'Ù' => 'u', 'Ú' => 'u', 'Û' => 'u', 'Ů' => 'u', 'Ý' => 'y', 'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a', 'å' => 'a', 'ç' => 'c', 'č' => 'c', 'è' => 'e', 'é' => 'e', 'ê' => 'e', 'ë' => 'e', 'ì' => 'i',
+        'í' => 'i', 'î' => 'i', 'ï' => 'i', 'ñ' => 'n', 'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'õ' => 'o', 'ø' => 'o', 'ù' => 'u', 'ů' => 'u', 'û' => 'u', 'û' => 'u', 'ý' => 'y', 'ÿ' => 'y',
+        '(' => '-', ')' => '-', '+' => 'plus', '&' => 'and', '#' => '-', '?' => '' ];
+    
+    /**
+     * Ascifies a string
+     * The string may still contains UTF chars after the conversion, this is just a "first step" ...
+     * @param string $name
+     * @return string
+     */
+    public static function asciify($name){
+        $name = trim($name);
+        // Only using array map does ensure correct multibyte character handling
+        $name = strtr($name, self::$asciiMap);
+        $name = preg_replace(array('/\s/', '/\.[\.]+/', '/[^\w_\.\-]/'), array('-', '.', ''), $name);
+        return trim(preg_replace('/[\-]+/i', '-', $name), '-');
+    }
+    /**
+     * Generates a websafe filename out of any string
+     * The resulting string is lowercase and has all whitespace stripped, blanks are replaced with "-"
+     * Take into account the returned string may be empty !
+     * @param string $name
+     * @return string
+     */
+    public static function secureFilename($name){
+        // first, some ASCII transformations
+        $name = self::asciify($name);
+        // now for security and lowercase
+        $name = strtolower(urlencode($name));
+        $name = preg_replace('/%[0-9a-z][0-9a-z]/', '',  $name);
+        $name = preg_replace('/-{2,}/', '-',  $name);
+        return $name;
+    }
 
     /**
      * splits the text up into HTML tags / entities on one side and plain text on the other side
