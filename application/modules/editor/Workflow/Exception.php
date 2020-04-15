25c88d99c193ba372cbb11a6f38bbbfe5d7d4a5d
Thomas Lauria <thomas@marcmittag.de>
2020-04-15 11:48:20 +0200

    TRANSLATE-382: error logging in the workflow manager

---
+++ application/modules/editor/Workflow/Exception.php
@@ -0,0 +1,43 @@
+<?php
+/*
+START LICENSE AND COPYRIGHT
+
+ This file is part of translate5
+ 
+ Copyright (c) 2013 - 2017 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+
+ Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
+
+ This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
+ as published by the Free Software Foundation and appearing in the file agpl3-license.txt
+ included in the packaging of this file.  Please review the following information
+ to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3 requirements will be met:
+ http://www.gnu.org/licenses/agpl.html
+  
+ There is a plugin exception available for use with this release of translate5 for
+ translate5: Please see http://www.translate5.net/plugin-exception.txt or
+ plugin-exception.txt in the root folder of translate5.
+  
+ @copyright  Marc Mittag, MittagQI - Quality Informatics
+ @author     MittagQI - Quality Informatics
+ @license    GNU AFFERO GENERAL PUBLIC LICENSE version 3 with plugin-execption
+			 http://www.gnu.org/licenses/agpl.html http://www.translate5.net/plugin-exception.txt
+
+END LICENSE AND COPYRIGHT
+*/
+
+/**
+ * Workflow Manager Exceptions
+ */
+class editor_Workflow_Exception extends ZfExtended_ErrorCodeException {
+    /**
+     * @var string
+     */
+    protected $domain = 'editor.workflow.manager';
+    
+    static protected $localErrorCodes = [
+        'E1251' => 'Workflow to class "{className}" not found!',
+        'E1252' => 'Workflow with ID "{workflowId}" not found!',
+        'E1253' => '{className}::$labels has to much / or missing labels!',
+    ];
+}
