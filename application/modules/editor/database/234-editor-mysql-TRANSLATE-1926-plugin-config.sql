f6174270b56fb3013f7185ceb9d472827a343391
ines_paul <ines-paul@mittagqi.com>
2020-04-21 16:36:57 +0200

    TRANSLATE-1926 Add LanguageResources: show all services that translate5 can handle - for current LanguageResource-plugins: "make sure that all LanguageResource-plugins are active after installation and updates"

---
+++ application/modules/editor/database/234-editor-mysql-TRANSLATE-1926-plugin-config.sql
@@ -0,0 +1,56 @@
+-- /*
+-- START LICENSE AND COPYRIGHT
+-- 
+--  This file is part of translate5
+--  
+--  Copyright (c) 2013 - 2017 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+-- 
+--  Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
+-- 
+--  This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
+--  as published by the Free Software Foundation and appearing in the file agpl3-license.txt 
+--  included in the packaging of this file.  Please review the following information 
+--  to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3 requirements will be met:
+--  http://www.gnu.org/licenses/agpl.html
+--   
+--  There is a plugin exception available for use with this release of translate5 for
+--  translate5: Please see http://www.translate5.net/plugin-exception.txt or 
+--  plugin-exception.txt in the root folder of translate5.
+--   
+--  @copyright  Marc Mittag, MittagQI - Quality Informatics
+--  @author     MittagQI - Quality Informatics
+--  @license    GNU AFFERO GENERAL PUBLIC LICENSE version 3 with plugin-execption
+-- 			 http://www.gnu.org/licenses/agpl.html http://www.translate5.net/plugin-exception.txt
+-- 
+-- END LICENSE AND COPYRIGHT
+-- */
+
+-- add the LanguageResource-plugins into an existing plugin config
+
+-- 1) value
+
+UPDATE  `Zf_configuration`
+SET  `value` = REPLACE(`value`, '"]', '","editor_Plugins_DeepL_Init"]')
+WHERE  `Zf_configuration`.`name` ="runtimeOptions.plugins.active" and not `Zf_configuration`.`value` like '%editor_Plugins_DeepL_Init%';
+
+UPDATE  `Zf_configuration`
+SET  `value` = REPLACE(`value`, '"]', '","editor_Plugins_GroupShare_Init"]')
+WHERE  `Zf_configuration`.`name` ="runtimeOptions.plugins.active" and not `Zf_configuration`.`value` like '%editor_Plugins_GroupShare_Init%';
+
+UPDATE  `Zf_configuration`
+SET  `value` = REPLACE(`value`, '"]', '","editor_Plugins_NecTm_Init"]')
+WHERE  `Zf_configuration`.`name` ="runtimeOptions.plugins.active" and not `Zf_configuration`.`value` like '%editor_Plugins_NecTm_Init%';
+
+-- 2) default
+
+UPDATE  `Zf_configuration`
+SET  `default` = REPLACE(`default`, '"]', '","editor_Plugins_DeepL_Init"]')
+WHERE  `Zf_configuration`.`name` ="runtimeOptions.plugins.active" and not `Zf_configuration`.`default` like '%editor_Plugins_DeepL_Init%';
+
+UPDATE  `Zf_configuration`
+SET  `default` = REPLACE(`default`, '"]', '","editor_Plugins_GroupShare_Init"]')
+WHERE  `Zf_configuration`.`name` ="runtimeOptions.plugins.active" and not `Zf_configuration`.`default` like '%editor_Plugins_GroupShare_Init%';
+
+UPDATE  `Zf_configuration`
+SET  `default` = REPLACE(`default`, '"]', '","editor_Plugins_NecTm_Init"]')
+WHERE  `Zf_configuration`.`name` ="runtimeOptions.plugins.active" and not `Zf_configuration`.`default` like '%editor_Plugins_NecTm_Init%';
