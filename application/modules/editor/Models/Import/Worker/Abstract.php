5756da2f0575b6c5a174b9d090bb679c927ae401
Thomas Lauria <thomas@marcmittag.de>
2020-05-11 17:08:14 +0200

    FATAL in core: E1027 - PHP E_ERROR: Uncaught Error: Call to a member function wakeupScheduled() on null

--- application/modules/editor/Models/Import/Worker/Abstract.php
+++ application/modules/editor/Models/Import/Worker/Abstract.php
@@ -9,13 +9,13 @@ START LICENSE AND COPYRIGHT
  Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
 
  This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
- as published by the Free Software Foundation and appearing in the file agpl3-license.txt 
- included in the packaging of this file.  Please review the following information 
+ as published by the Free Software Foundation and appearing in the file agpl3-license.txt
+ included in the packaging of this file.  Please review the following information
  to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3 requirements will be met:
  http://www.gnu.org/licenses/agpl.html
   
  There is a plugin exception available for use with this release of translate5 for
- translate5: Please see http://www.translate5.net/plugin-exception.txt or 
+ translate5: Please see http://www.translate5.net/plugin-exception.txt or
  plugin-exception.txt in the root folder of translate5.
   
  @copyright  Marc Mittag, MittagQI - Quality Informatics
@@ -52,9 +52,9 @@ abstract class editor_Models_Import_Worker_Abstract extends ZfExtended_Worker_Ab
         if(isset($wm)){
             $wm->setState($wm::STATE_DEFUNCT);
             $wm->save();
+            //wake up remaining - if any
+            $this->wakeUpAndStartNextWorkers();
         }
-        //wake up remaining - if any
-        $this->wakeUpAndStartNextWorkers();
         //if no worker model is set, we don't have to call parent / init a worker model,
         // since we don't even need it in the DB when the task already has errors
         return false;
