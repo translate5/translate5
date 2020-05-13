6547f98d295eb386ca209f75b868b3dda36dd65c
ines_paul <ines-paul@mittagqi.com>
2020-05-13 15:17:54 +0200

    TRANSLATE-1901 Support lines in pixel-based lenth check - new testcase (AT WORK)

---
+++ application/modules/editor/testcases/editorAPI/XlfSegmentLinesPixelLengthTest.php
@@ -0,0 +1,149 @@
+<?php
+/*
+START LICENSE AND COPYRIGHT
+
+ This file is part of translate5
+ 
+ Copyright (c) 2013 - 2017 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+
+ Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
+
+ This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
+ as published by the Free Software Foundation and appearing in the file agpl3-license.txt 
+ included in the packaging of this file.  Please review the following information 
+ to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3 requirements will be met:
+ http://www.gnu.org/licenses/agpl.html
+  
+ There is a plugin exception available for use with this release of translate5 for
+ translate5: Please see http://www.translate5.net/plugin-exception.txt or 
+ plugin-exception.txt in the root folder of translate5.
+  
+ @copyright  Marc Mittag, MittagQI - Quality Informatics
+ @author     MittagQI - Quality Informatics
+ @license    GNU AFFERO GENERAL PUBLIC LICENSE version 3 with plugin-execption
+			 http://www.gnu.org/licenses/agpl.html http://www.translate5.net/plugin-exception.txt
+
+END LICENSE AND COPYRIGHT
+*/
+
+/**
+ * XlfSegmentLinesPixelLengthTest imports a simple task and checks imported values about the segment lengths,
+ * edits segments and checks then the edited ones again on correct content
+ */
+class XlfSegmentLinesPixelLengthTest extends \ZfExtended_Test_ApiTestcase {
+    public static function setUpBeforeClass(): void {
+        self::$api = $api = new ZfExtended_Test_ApiHelper(__CLASS__);
+        
+        $task = array(
+            'sourceLang' => 'en',
+            'targetLang' => 'de',
+            'edit100PercentMatch' => true,
+            'lockLocked' => 1,
+        );
+        
+        $appState = self::assertAppState();
+        self::assertNotContains('editor_Plugins_LockSegmentsBasedOnConfig_Bootstrap', $appState->pluginsLoaded, 'Plugin LockSegmentsBasedOnConfig should not be activated for this test case!');
+        self::assertNotContains('editor_Plugins_NoMissingTargetTerminology_Bootstrap', $appState->pluginsLoaded, 'Plugin NoMissingTargetTerminology should not be activated for this test case!');
+        
+        self::assertNeededUsers(); //last authed user is testmanager
+        self::assertLogin('testmanager');
+        
+        $zipfile = $api->zipTestFiles('testfiles/','XLF-test.zip');
+        
+        $api->addImportFile($zipfile);
+        $api->import($task);
+        
+        $api->addUser('testlector');
+        
+        //login in setUpBeforeClass means using this user in whole testcase!
+        $api->login('testlector');
+        
+        $task = $api->getTask();
+        //open task for whole testcase
+        $api->requestJson('editor/task/'.$task->id, 'PUT', array('userState' => 'edit', 'id' => $task->id));
+    }
+    
+    /**
+     * Testing segment values directly after import
+     * Other constellations of the segment length count are implicitly tested in the XlfImportTest!
+     */
+    public function testSegmentValuesAfterImport() {
+        
+        //get segment list (just the ones of the first file for that tests)
+        $segments = $this->api()->requestJson('editor/segment?page=1&start=0&limit=20');
+        
+        $data = array_map([self::$api,'removeUntestableSegmentContent'], $segments);
+        //file_put_contents("/home/tlauria/www/translate5-master/application/modules/editor/testcases/editorAPI/XlfSegmentPixelLengthTest/expectedSegments-new.json", json_encode($data,JSON_PRETTY_PRINT));
+        $this->assertEquals(self::$api->getFileContent('expectedSegments.json'), $data, 'Imported segments are not as expected!');
+    }
+    
+    /**
+     * @depends testSegmentValuesAfterImport
+     */
+    public function testSegmentEditing() {
+        //get segment list (just the ones of the first file for that tests)
+        $segments = $this->api()->requestJson('editor/segment?page=1&start=0&limit=20');
+        $this->assertNotEmpty($segments, 'No segments are found in the Task!');
+        
+        require_once 'Models/Segment/TagAbstract.php';
+        require_once 'Models/Segment/InternalTag.php';
+
+        //the first three segments remain unedited, since content is getting to long with edited content
+        foreach($segments as $idx => $segToEdit) {
+            if(empty($segToEdit->editable)) {
+                continue;
+            }
+            if(empty($segToEdit->targetEdit)) {
+                $contentToUse = $segToEdit->source;
+            }
+            else {
+                $contentToUse = $segToEdit->targetEdit;
+            }
+            $editedData = $contentToUse.' - edited'.$segToEdit->segmentNrInTask;
+            $segmentData = $this->api()->prepareSegmentPut('targetEdit', $editedData, $segToEdit->id);
+            $this->api()->requestJson('editor/segment/'.$segToEdit->id, 'PUT', $segmentData);
+        }
+        
+        $segments = $this->api()->requestJson('editor/segment?page=1&start=0&limit=20');
+        $data = array_map([self::$api,'removeUntestableSegmentContent'], $segments);
+        //file_put_contents("/home/tlauria/www/translate5-master/application/modules/editor/testcases/editorAPI/XlfSegmentPixelLengthTest/expectedSegmentsEdited-new.json", json_encode($data,JSON_PRETTY_PRINT));
+        $this->assertEquals(self::$api->getFileContent('expectedSegmentsEdited.json'), $data, 'Edited segments are not as expected!');
+        
+        $task = $this->api()->getTask();
+        //start task export 
+        $this->checkExport($task, 'editor/task/export/id/'.$task->id, 'mrkothercontentlength-en-de.xlf', 'expected-export.xlf');
+    }
+    
+    /**
+     * tests the export results
+     * @param stdClass $task
+     * @param string $exportUrl
+     * @param string $fileToExport
+     * @param string $fileToCompare
+     */
+    protected function checkExport(stdClass $task, $exportUrl, $fileToExport, $fileToCompare) {
+        $this->api()->login('testmanager');
+        $this->api()->request($exportUrl);
+
+        //get the exported file content
+        $path = $this->api()->getTaskDataDirectory();
+        $pathToZip = $path.'export.zip';
+        $this->assertFileExists($pathToZip);
+        $exportedFile = $this->api()->getFileContentFromZipPath($pathToZip, $task->taskGuid.'/'.$fileToExport);
+        //compare it
+        $expectedResult = $this->api()->getFileContent($fileToCompare);
+        //file_put_contents('/home/tlauria/foo1.xlf', rtrim($expectedResult));
+        //file_put_contents('/home/tlauria/foo2.xlf', rtrim($exportedFile));
+        //file_put_contents('/home/tlauria/foo-'.$fileToCompare, rtrim($exportedFile));
+        $this->assertEquals(rtrim($expectedResult), rtrim($exportedFile), 'Exported result does not equal to '.$fileToCompare);
+    }
+    
+    public static function tearDownAfterClass(): void {
+        $task = self::$api->getTask();
+        //open task for whole testcase
+        self::$api->login('testlector');
+        self::$api->requestJson('editor/task/'.$task->id, 'PUT', array('userState' => 'open', 'id' => $task->id));
+        self::$api->login('testmanager');
+        self::$api->requestJson('editor/task/'.$task->id, 'DELETE');
+    }
+}
