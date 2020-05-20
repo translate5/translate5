42bdc6b12eb6b8702bdb1a4acfc66a4f6ab550b3
Thomas Lauria <thomas@marcmittag.de>
2020-05-20 10:07:49 +0200

    TRANSLATE-2043: Use Composer to manage all the PHP dependencies in development

--- application/modules/editor/Services/Google/HttpApi.php
+++ application/modules/editor/Services/Google/HttpApi.php
@@ -9,13 +9,13 @@ START LICENSE AND COPYRIGHT
  Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
 
  This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
- as published by the Free Software Foundation and appearing in the file agpl3-license.txt 
- included in the packaging of this file.  Please review the following information 
+ as published by the Free Software Foundation and appearing in the file agpl3-license.txt
+ included in the packaging of this file.  Please review the following information
  to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3 requirements will be met:
  http://www.gnu.org/licenses/agpl.html
   
  There is a plugin exception available for use with this release of translate5 for
- translate5: Please see http://www.translate5.net/plugin-exception.txt or 
+ translate5: Please see http://www.translate5.net/plugin-exception.txt or
  plugin-exception.txt in the root folder of translate5.
   
  @copyright  Marc Mittag, MittagQI - Quality Informatics
@@ -25,8 +25,6 @@ START LICENSE AND COPYRIGHT
 
 END LICENSE AND COPYRIGHT
 */
-
-require APPLICATION_PATH.'/../library/google-translate-api/vendor/autoload.php';
 
 use Google\Cloud\Translate\TranslateClient;
 use Google\Cloud\Core\Exception\BadRequestException;
@@ -41,7 +39,7 @@ class editor_Services_Google_HttpApi {
     protected $error = array();
     
     /**
-     * 
+     *
      * @var Google\Cloud\Translate\TranslateClient
      */
     protected $translateClient;
@@ -53,7 +51,7 @@ class editor_Services_Google_HttpApi {
     protected $apiKey;
     
     /***
-     * 
+     *
      * @var string
      */
     protected $projectId;
@@ -83,7 +81,7 @@ class editor_Services_Google_HttpApi {
     
     /**
      * Get the google translate api client.
-     * 
+     *
      * @return \Google\Cloud\Translate\TranslateClient
      */
     protected function getTranslateClient() {
@@ -96,7 +94,7 @@ class editor_Services_Google_HttpApi {
     
     /**
      * Search the api for given source/target language by domainCode
-     * 
+     *
      * @param string $text
      * @param string $sourceLang
      * @param string $targetLang
