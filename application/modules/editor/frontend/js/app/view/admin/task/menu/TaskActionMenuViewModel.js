4d781a13a85c4a5d30e1fdd86df697660b330058
aleksandar <aleksandar@mittagqi.com>
2020-06-05 17:06:19 +0200

    TRANSLATE-1610 Bundle tasks to projects (export menu fixes)

--- application/modules/editor/frontend/js/app/view/admin/task/menu/TaskActionMenuViewModel.js
+++ application/modules/editor/frontend/js/app/view/admin/task/menu/TaskActionMenuViewModel.js
@@ -102,7 +102,10 @@ Ext.define('Editor.view.admin.task.menu.TaskActionMenuViewModel', {
             	var task=this.get('task'),
             		downloadable=task.isErroneous() && Editor.app.authenticatedUser.isAllowed('downloadImportArchive'),
             		allowed=this.isMenuAllowed('editorShowexportmenuTask',task);
-            	return allowed && (!task.isErroneous() || downloadable) && !task.isImporting() && !task.isPending() && (!task.isCustomState() || task.get('state') == 'ExcelExported');
+            	if(downloadable && allowed){
+            	    return true;
+            	}
+            	return allowed && !task.isImporting() && !task.isPending() && (!task.isCustomState() || task.get('state') == 'ExcelExported');
             },
             bind: '{task}'
         },
@@ -130,6 +133,28 @@ Ext.define('Editor.view.admin.task.menu.TaskActionMenuViewModel', {
         isMenuGroupVisible:{
         	get: function(get) {
         		return this.get('isEditorPreferencesTask');
+            },
+            bind: '{task}'
+        },
+        
+        /***
+         * On task change, reconfigure the export menu
+         */
+        exportMenuConfig:{
+            get: function(task) {
+                //the old menu will be destroyed by the owner component
+                var me = this,
+                    hasQm = task.hasQmSub(),
+                    exportAllowed =Editor.app.authenticatedUser.isAllowed('editorExportTask', task),
+                    menu;
+                
+                menu = Ext.widget('adminExportMenu', {
+                    task: task,
+                    fields: hasQm ? task.segmentFields() : false
+                });
+                menu.down('#exportItem') && menu.down('#exportItem').setVisible(exportAllowed);
+                menu.down('#exportDiffItem') && menu.down('#exportDiffItem').setVisible(exportAllowed);
+                return menu;
             },
             bind: '{task}'
         }
