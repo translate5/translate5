0b1fead9fc8de1805d200070c33524ffed5cd619
Stephan Bergmann <Stephan@MittagQI.com>
2020-11-13 11:20:20 +0100

    translate5Cli: - added cd RUNDIR in translate5.sh (now works also with translate5.sh as SymLink)

--- translate5.sh
+++ translate5.sh
@@ -5,7 +5,7 @@ CMD_PHP="${CMD_PHP:-/usr/bin/php}"
 type -p $CMD_PHP &>/dev/null || { echo "$CMD_PHP not found. Set \$CMD_PHP in $0"; exit 1; }
 
 # change to the current directory
-RUNDIR=`dirname $0`;
+RUNDIR=`dirname $(realpath "$0")`;
 cd $RUNDIR;
 
 $CMD_PHP ./Translate5/maintenance-cli.php "$@"
