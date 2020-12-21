926a877ee69ad0695684098c5314b971309e24ba
Thomas Lauria <thomas@mittagqi.com>
2020-12-21 10:18:10 +0100

    TRANSLATE-2249: Length restriction for sdlxliff files

--- application/modules/editor/Models/Import/FileParser/Sdlxliff/Exception.php
+++ application/modules/editor/Models/Import/FileParser/Sdlxliff/Exception.php
@@ -9,13 +9,13 @@ START LICENSE AND COPYRIGHT
  Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
 
  This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
- as published by the Free Software Foundation and appearing in the file agpl3-license.txt 
- included in the packaging of this file.  Please review the following information 
+ as published by the Free Software Foundation and appearing in the file agpl3-license.txt
+ included in the packaging of this file.  Please review the following information
  to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3 requirements will be met:
  http://www.gnu.org/licenses/agpl.html
   
  There is a plugin exception available for use with this release of translate5 for
- translate5: Please see http://www.translate5.net/plugin-exception.txt or 
+ translate5: Please see http://www.translate5.net/plugin-exception.txt or
  plugin-exception.txt in the root folder of translate5.
   
  @copyright  Marc Mittag, MittagQI - Quality Informatics
@@ -41,7 +41,8 @@ class editor_Models_Import_FileParser_Sdlxliff_Exception extends editor_Models_I
         'E1005' => '<sdl:seg-defs was not found in the current transunit: "{transunit}"',
         'E1006' => 'Loading the tag information from the SDLXLIFF header has failed!',
         'E1007' => 'The tag "{tagname}" is not defined in the "_tagDefMapping" list.',
-        'E1009' => 'The source and target segment count does not match in transunit: "{transunit}".', 
+        'E1009' => 'The source and target segment count does not match in transunit: "{transunit}".',
         'E1010' => 'The tag "{tagname}" was used in the segment but is not defined in the "_tagDefMapping" list!',
+        'E1323' => 'The transUnit contains sdl:cxt tags, but we assume that tags only in the group tag!',
     ];
 }
