12abcba2678cba2f806200433bec5a31ca64142d
Axel <axel@mittagqi.com>
2021-04-27 10:20:00 +0200

    - removed hint

--- application/modules/editor/Models/Db/SegmentQualityRow.php
+++ application/modules/editor/Models/Db/SegmentQualityRow.php
@@ -100,7 +100,7 @@ class editor_Models_Db_SegmentQualityRow extends Zend_Db_Table_Row_Abstract {
         }
         return (count($ours) == count($theirs));
     }
-    /** can be used for debugging TODO AUTOQA: remove
+    /** can be used for debugging
     public function save(){
         error_log('SAVED QUALITY '.$this->id.': type: '.$this->type.' category: '.$this->category.' falsePositive: '.$this->falsePositive);
         parent::save();
