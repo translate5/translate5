678ede6dc39a98d20b99108d2ff435b10318bc6b
Thomas Lauria <thomas@mittagqi.com>
2021-05-06 15:14:52 +0200

    TRANSLATE-2077: JS fixes

--- application/modules/editor/Plugins/MatchAnalysis/public/js/view/AnalysisPanel.js
+++ application/modules/editor/Plugins/MatchAnalysis/public/js/view/AnalysisPanel.js
@@ -228,23 +228,15 @@ Ext.define('Editor.plugins.MatchAnalysis.view.AnalysisPanel', {
                     dockedItems: [{
                         xtype: 'toolbar',
                         dock: 'bottom',
-                        layout: {
-                            type: 'vbox',
-                            align: 'left'
-                        },
-                        items: [{
-                            xtype: 'container',
-                            padding: '10',
-                            html:"¹ "+me.strings.noAnalysis,
-                            dock : 'bottom'
-                        },{ 
+                        items: [{ 
                             xtype: 'button',
                             glyph: 'f1c3@FontAwesome5FreeSolid',
                             itemId:'exportExcel',
                             text:me.strings.exportAnalysis,
                             listeners:{
                                 click:'onExcelExportClick'
-                            }},{
+                            }
+                        },{
                             xtype: 'button',
                             glyph: 'f1c3@FontAwesome5FreeSolid',
                             itemId:'exportXml',
@@ -253,6 +245,11 @@ Ext.define('Editor.plugins.MatchAnalysis.view.AnalysisPanel', {
                                 click:'onXmlExportClick'
                             }
                         }]
+                    },{
+                        xtype: 'container',
+                        padding: '10',
+                        html:"¹ "+me.strings.noAnalysis,
+                        dock : 'bottom'
                     },{
                         xtype: 'toolbar',
                         dock: 'top',
