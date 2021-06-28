e0ef45ff7b00b21983aaf6fd6d181e91b94f2a24
Thomas Lauria <thomas@mittagqi.com>
2021-06-28 09:55:59 +0200

    TRANSLATE-2545: Flexibilize workflow - workflow clean up and refactoring

---
+++ application/modules/editor/Workflow/Default/JobHandler.php
@@ -0,0 +1,267 @@
+<?php
+/*
+START LICENSE AND COPYRIGHT
+
+ This file is part of translate5
+ 
+ Copyright (c) 2013 - 2017 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+
+ Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
+
+ This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
+ as published by the Free Software Foundation and appearing in the file agpl3-license.txt
+ included in the packaging of this file.  Please review the following information
+ to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3 requirements will be met:
+ http://www.gnu.org/licenses/agpl.html
+  
+ There is a plugin exception available for use with this release of translate5 for
+ translate5: Please see http://www.translate5.net/plugin-exception.txt or
+ plugin-exception.txt in the root folder of translate5.
+  
+ @copyright  Marc Mittag, MittagQI - Quality Informatics
+ @author     MittagQI - Quality Informatics
+ @license    GNU AFFERO GENERAL PUBLIC LICENSE version 3 with plugin-execption
+			 http://www.gnu.org/licenses/agpl.html http://www.translate5.net/plugin-exception.txt
+
+END LICENSE AND COPYRIGHT
+*/
+
+/**
+ * Handler methods for task hookins
+ */
+class editor_Workflow_Default_JobHandler extends editor_Workflow_Default_AbstractHandler {
+    const HANDLE_JOB_UNFINISH            = 'doUnfinish';
+    
+    const HANDLE_JOB_FIRSTCONFIRMOFAROLE = 'handleFirstConfirmOfARole';
+    const HANDLE_JOB_FIRSTCONFIRM        = 'handleFirstConfirm';
+    const HANDLE_JOB_ALLCONFIRMOFAROLE   = 'handleAllConfirmOfARole';
+    const HANDLE_JOB_ALLCONFIRM          = 'handleAllConfirm';
+    const HANDLE_JOB_CONFIRM             = 'handleConfirm';
+    
+    const HANDLE_JOB_ADD                 = 'handleUserAssociationAdded';
+    const HANDLE_JOB_DELETE              = 'handleUserAssociationDeleted';
+    const HANDLE_JOB_EDITED              = 'handleUserAssociationEdited';
+    
+    /**
+     * {@inheritDoc}
+     * @see editor_Workflow_Default_AbstractHandler::execute()
+     */
+    public function execute(editor_Workflow_Actions_Config $actionConfig): ?string {
+        $this->config = $actionConfig;
+        
+        if($actionConfig == self::HANDLE_JOB_DELETE) {
+            $tua = $actionConfig->newTua;
+            $actionConfig->task = ZfExtended_Factory::get('editor_Models_Task');
+            $actionConfig->task->loadByTaskGuid($tua->getTaskGuid());
+            
+            /* @var $finishHandler editor_Workflow_Default_JobHandler_Finish */
+            $finishHandler = ZfExtended_Factory::get('editor_Workflow_Default_JobHandler_Finish');
+            $finishHandler->executeOnDeleteJob($actionConfig);
+        }
+        $this->handleUserAssociationChanged();
+        return $actionConfig->trigger;
+    }
+    
+    /**
+     * Executes the Job Handler with a callback where the job can be saved outside
+     * @param editor_Workflow_Actions_Config $actionConfig
+     * @param callable $saveCallback
+     * @return string|NULL
+     */
+    public function executeSave(editor_Workflow_Actions_Config $actionConfig, callable $saveCallback): ?string {
+        $this->config = $actionConfig;
+        
+        $state = $this->getTriggeredState($actionConfig->oldTua, $actionConfig->newTua, 'before');
+        $actionConfig->events->trigger($state, $this->workflow, ['oldTua' => $actionConfig->oldTua, 'newTua' => $actionConfig->newTua]);
+        
+        //call here stuff which must be done between the before trigger and the other code (normally saving the TUA)
+        $saveCallback();
+        
+        //ensure that segment MV is createad
+        $actionConfig->task->createMaterializedView();
+        $state = $this->getTriggeredState($actionConfig->oldTua, $actionConfig->newTua);
+        $this->doDebug($state);
+        if(!empty($state)) {
+            if(method_exists($this, $state)) {
+                $this->{$state}();
+            }
+            $actionConfig->events->trigger($state, $actionConfig->workflow, ['oldTua' => $actionConfig->oldTua, 'newTua' => $actionConfig->newTua, 'task' => $actionConfig->task]);
+        }
+        
+        //finally set the trigger to edited and call the job changed handler
+        $actionConfig->trigger = self::HANDLE_JOB_EDITED;
+        $this->handleUserAssociationChanged();
+        $actionConfig->workflow->getStepRecalculation()->recalculateWorkflowStep($actionConfig->newTua);
+        return $actionConfig->trigger;
+    }
+    
+    /**
+     * will be called when a new task user association is created
+     */
+    protected function handleUserAssociationChanged() {
+        $this->doDebug($this->config->trigger);
+        if(empty($this->config->task)) {
+            $this->config->task = ZfExtended_Factory::get('editor_Models_Task');
+            $this->config->task->loadByTaskGuid($this->config->newTua->getTaskGuid());
+        }
+        $this->callActions($this->config, $this->config->task->getWorkflowStepName(), $this->config->newTua->getRole(), $this->config->newTua->getState());
+    }
+    
+    /**
+     * method returns the triggered state as string ready to use in events, these are mainly:
+     * doUnfinish, doView, doEdit, doFinish, doWait, doConfirm
+     * beforeUnfinish, beforeView, beforeEdit, beforeFinish, beforeWait, beforeConfirm
+     *
+     * @param editor_Models_TaskUserAssoc $oldTua
+     * @param editor_Models_TaskUserAssoc $newTua
+     * @param string $prefix optional, defaults to "do"
+     * @return string
+     */
+    protected function getTriggeredState(editor_Models_TaskUserAssoc $oldTua, editor_Models_TaskUserAssoc $newTua, $prefix = 'do') {
+        $oldState = $oldTua->getState();
+        $newState = $newTua->getState();
+        if($oldState == $newState) {
+            return null;
+        }
+        
+        if($oldState == $this->workflow::STATE_FINISH && $newState != $this->workflow::STATE_FINISH) {
+            return $prefix.'Unfinish';
+        }
+        
+        if($oldState == $this->workflow::STATE_UNCONFIRMED && $newState == $this->workflow::STATE_EDIT) {
+            return $prefix.'Confirm';
+        }
+        
+        $result = null;
+        switch($newState) {
+            case $this->workflow::STATE_OPEN:
+                $result = $prefix.'Open';
+                break;
+            case $this->workflow::STATE_VIEW:
+                $result = $prefix.'View';
+                break;
+            case $this->workflow::STATE_EDIT:
+                $result = $prefix.'Edit';
+                break;
+            case $this->workflow::STATE_FINISH:
+                $result = $prefix.'Finish';
+                break;
+            case $this->workflow::STATE_WAITING:
+                $result = $prefix.'Wait';
+                break;
+            default:
+                $result = null;
+        }
+        return $result;
+    }
+    
+    /**
+     * is called on finishin a task
+     * evaluates the role and states of the User Task Association and calls the matching handlers:
+     */
+    protected function doFinish() {
+        /* @var $finishHandler editor_Workflow_Default_JobHandler_Finish */
+        $finishHandler = ZfExtended_Factory::get('editor_Workflow_Default_JobHandler_Finish');
+        $finishHandler->execute($this->getActionConfig());
+    }
+    
+    /**
+     * is called on reopening / unfinishing a task
+     * unfinish a finished task (taskassoc-specific unfinish in contrast to task-specific reopening)
+     * Set all REVIEWED_UNTOUCHED segments to TRANSLATED
+     * will be called after a task has been unfinished (after was finished - taskassoc-specific)
+     */
+    protected function doUnfinish() {
+        $this->doDebug(self::HANDLE_JOB_UNFINISH);
+        /* @var $actions editor_Workflow_Actions */
+        $this->callActions(self::HANDLE_JOB_UNFINISH, $this->config->task->getWorkflowStepName(), $this->config->newTua->getRole(), $this->config->newTua->getState());
+    }
+    
+    /**
+     * is called when a user confirms his job (job was unconfirmed and is set to edit)
+     * No handler functions for confirm available, everything is handled via actions
+     */
+    protected function doConfirm() {
+        $stat = $this->calculateConfirm();
+        $this->doDebug(__FUNCTION__.print_r($stat,1));
+        
+        $toTrigger = [];
+        if($stat['roleFirstConfirmed']) {
+            $toTrigger[] = self::HANDLE_JOB_FIRSTCONFIRMOFAROLE;
+        }
+        if($stat['firstConfirmed']) {
+            $toTrigger[] = self::HANDLE_JOB_FIRSTCONFIRM;
+        }
+        if($stat['roleAllConfirmed']) {
+            $toTrigger[] = self::HANDLE_JOB_ALLCONFIRMOFAROLE;
+        }
+        if($stat['allConfirmed']) {
+            $toTrigger[] = self::HANDLE_JOB_ALLCONFIRM;
+        }
+        $toTrigger[] = self::HANDLE_JOB_CONFIRM;
+        
+        $newTua = $this->config->newTua;
+        $oldStep = $this->config->task->getWorkflowStepName();
+        foreach($toTrigger as $trigger) {
+            $this->doDebug($trigger);
+            $this->callActions($trigger, $oldStep, $newTua->getRole(), $newTua->getState());
+        }
+    }
+    
+    /**
+     * Calculates the workflow step confirmation status
+     * Warning: this function may only be called in doConfirm (which is called if there was a state unconfirmed which is now set to edit)
+     *  For all other usages the calculation will not be correct, since we don't know if a state was unconfirmed before,
+     *  we see only that all states are now not unconfirmed.
+     */
+    protected function calculateConfirm() {
+        $userTaskAssoc = $this->config->newTua;
+        $stat = $userTaskAssoc->getUsageStat();
+        $sum = 0;
+        $roleSum = 0;
+        $otherSum = 0;
+        $roleUnconfirmedSum = 0;
+        foreach($stat as $entry) {
+            $sum += (int)$entry['cnt'];
+            $isRole = $entry['role'] === $userTaskAssoc->getRole();
+            $isUnconfirmed = $entry['state'] === $this->config->workflow::STATE_UNCONFIRMED;
+            if($isRole) {
+                $roleSum += (int)$entry['cnt'];
+                if($isUnconfirmed) {
+                    $roleUnconfirmedSum += (int)$entry['cnt'];
+                }
+            }
+            if(!$isUnconfirmed) {
+                $otherSum += (int)$entry['cnt'];
+            }
+        }
+        return [
+            'allConfirmed' => $sum > 0 && $otherSum === $sum,
+            'roleAllConfirmed' => $roleUnconfirmedSum === 0,
+            'roleFirstConfirmed' => $roleSum - 1 === $roleUnconfirmedSum,
+            //firstConfirmed is working only if really all other jobs are unconfirmed, what is seldom, since the other states will be waiting / finished etc.
+            'firstConfirmed' => $otherSum === 1,
+        ];
+    }
+    
+    /**
+     * debugging workflow
+     * @param string $msg
+     * @param array $data optional debuggin data
+     * @param bool $levelInfo optional, if true log in level info instead debug
+     */
+    protected function doDebug($msg, array $data = [], $levelInfo = false) {
+        $log = $this->config->workflow->getLogger($this->config->task);
+        
+        //add the job / tua
+        if(!empty($this->config->newTua)) {
+            $data['job'] = $this->config->newTua;
+        }
+        if($levelInfo) {
+            $log->info('E1013', $msg, $data);
+        }
+        else {
+            $log->debug('E1013', $msg, $data);
+        }
+    }
+}
---
+++ application/modules/editor/Workflow/Default/JobHandler/Finish.php
@@ -0,0 +1,242 @@
+<?php
+/*
+START LICENSE AND COPYRIGHT
+
+ This file is part of translate5
+ 
+ Copyright (c) 2013 - 2017 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+
+ Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
+
+ This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
+ as published by the Free Software Foundation and appearing in the file agpl3-license.txt
+ included in the packaging of this file.  Please review the following information
+ to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3 requirements will be met:
+ http://www.gnu.org/licenses/agpl.html
+  
+ There is a plugin exception available for use with this release of translate5 for
+ translate5: Please see http://www.translate5.net/plugin-exception.txt or
+ plugin-exception.txt in the root folder of translate5.
+  
+ @copyright  Marc Mittag, MittagQI - Quality Informatics
+ @author     MittagQI - Quality Informatics
+ @license    GNU AFFERO GENERAL PUBLIC LICENSE version 3 with plugin-execption
+			 http://www.gnu.org/licenses/agpl.html http://www.translate5.net/plugin-exception.txt
+
+END LICENSE AND COPYRIGHT
+*/
+
+/**
+ * Handler methods for task hookins
+ */
+class editor_Workflow_Default_JobHandler_Finish extends editor_Workflow_Default_AbstractHandler {
+    
+    const HANDLE_JOB_FINISH = 'handleFinish';
+    const HANDLE_JOB_FIRSTFINISH = 'handleFirstFinish';
+    const HANDLE_JOB_FIRSTFINISHOFAROLE = 'handleFirstFinishOfARole';
+    const HANDLE_JOB_ALLFINISH = 'handleAllFinish';
+    const HANDLE_JOB_ALLFINISHOFAROLE = 'handleAllFinishOfARole';
+    
+    /**
+     * @param editor_Workflow_Actions_Config $actionConfig
+     */
+    public function execute(editor_Workflow_Actions_Config $actionConfig): ?string {
+        $this->config = $actionConfig;
+        $stat = $this->calculateFinish();
+        $this->doDebug(__CLASS__.'::'.print_r($stat,1));
+        
+        if($stat['roleFirstFinished']) {
+            $this->handleFirstFinishOfARole();
+        }
+        if($stat['firstFinished']) {
+            $this->handleFirstFinish();
+        }
+        if($stat['roleAllFinished']) {
+            $this->handleAllFinishOfARole();
+        }
+        if($stat['allFinished']) {
+            $this->handleAllFinish();
+        }
+        $this->handleFinish();
+        return $this->config->trigger;
+    }
+    
+    /**
+     * trigger finish calculation after deleting a job (since the task may get finished after deleting the last opened job when all others are already finished)
+     * @param editor_Workflow_Actions_Config $actionConfig
+     */
+    public function executeOnDeleteJob(editor_Workflow_Actions_Config $actionConfig): ?string {
+        $this->config = $actionConfig;
+        $originalState = $actionConfig->newTua->getState();
+        //if the deleted tua was not finished, we have to recheck the allFinished events after deleting it!
+        $wasNotFinished = ($originalState !== $this->config->workflow::STATE_FINISH);
+        
+        $stat = $this->calculateFinish();
+        $this->doDebug(__FUNCTION__. ' OriginalState: '.$originalState.'; Finish Stat: '.print_r($stat,1));
+        if($wasNotFinished && $stat['roleAllFinished']) {
+            //in order to trigger the actions correctly we have to assume that the deleted one was "finished"
+            $actionConfig->newTua->setState($this->config->workflow::STATE_FINISH);
+            $this->handleAllFinishOfARole();
+        }
+        if($wasNotFinished && $stat['allFinished']) {
+            //in order to trigger the actions correctly we have to assume that the deleted one was "finished"
+            $actionConfig->newTua->setState($this->config->workflow::STATE_FINISH);
+            $this->handleAllFinish();
+        }
+        $actionConfig->newTua->setState($originalState);
+        return $this->config->trigger;
+    }
+    
+    /**
+     * calculates which of the "finish" handlers can be called accordingly to the currently existing tuas of a task
+     * @return boolean[]
+     */
+    protected function calculateFinish() {
+        $userTaskAssoc = $this->config->newTua;
+        $stat = $userTaskAssoc->getUsageStat();
+        //we have to initialize $allFinished with true for proper working but with false if there is no tua at all
+        $allFinished = !empty($stat);
+        
+        //we have to initialize $roleAllFinished with true for proper working but with false if there is no tua with the current tuas role
+        $usedRoles = array_column($stat, 'role');
+        $roleAllFinished = in_array($userTaskAssoc->getRole(), $usedRoles);
+        $roleFirstFinished = false;
+        $sum = 0;
+        foreach($stat as $entry) {
+            $isRole = $entry['role'] === $userTaskAssoc->getRole();
+            $isFinish = $entry['state'] === $this->workflow::STATE_FINISH;
+            if($isRole && $roleAllFinished && ! $isFinish) {
+                $roleAllFinished = false;
+            }
+            if($allFinished && ! $isFinish) {
+                $allFinished = false;
+            }
+            if($isRole && $isFinish && (int)$entry['cnt'] === 1) {
+                $roleFirstFinished = true;
+            }
+            if($isFinish) {
+                $sum += (int)$entry['cnt'];
+            }
+        }
+        return [
+            'allFinished' => $allFinished,
+            'roleAllFinished' => $roleAllFinished,
+            'roleFirstFinished' => $roleFirstFinished,
+            'firstFinished' => $sum === 1,
+        ];
+    }
+    
+    /**
+     * will be called after all users of a role has finished a task
+     * @param array $finishStat contains the info which of all different finishes are applicable
+     */
+    protected function handleAllFinishOfARole() {
+        $newTua = $this->config->newTua;
+        $this->config->trigger = self::HANDLE_JOB_ALLFINISHOFAROLE;
+        $taskGuid = $newTua->getTaskGuid();
+        $task = ZfExtended_Factory::get('editor_Models_Task');
+        /* @var $task editor_Models_Task */
+        $task->loadByTaskGuid($taskGuid);
+        $oldStep = $task->getWorkflowStepName();
+        
+        //this remains as default behaviour
+        $nextStep = $newTua->getWorkflowStepName();
+        $this->doDebug($this->config->trigger." Next Step: ".$nextStep.' to role '.$newTua->getRole().' with step '.$nextStep."; Old Step in Task: ".$oldStep);
+        if($nextStep) {
+            //Next step triggert ebenfalls eine callAction → aber irgendwie so, dass der neue Wert verwendet wird! Henne Ei!
+            $this->setNextStep($task, $nextStep);
+            $nextRole = $this->config->workflow->getRoleOfStep($nextStep);
+            $this->doDebug($this->config->trigger." Next Role: ".$nextRole);
+            if($nextRole) {
+                $isComp = $task->getUsageMode() == $task::USAGE_MODE_COMPETITIVE;
+                $newTua->setStateForRoleAndTask($isComp ? $this->config->workflow::STATE_UNCONFIRMED : $this->config->workflow::STATE_OPEN, $nextRole);
+            }
+        }
+        
+        //provide here oldStep, since this was the triggering one. The new step is given to handleNextStep trigger
+        $this->callActions($this->config, $oldStep, $newTua->getRole(), $newTua->getState());
+    }
+    
+    /**
+     * will be called after a user has finished a task
+     * @param array $finishStat contains the info which of all different finishes are applicable
+     */
+    protected function handleFinish() {
+        $this->doDebug(self::HANDLE_JOB_FINISH);
+        $this->config->trigger = self::HANDLE_JOB_FINISH;
+        $newTua = $this->config->newTua;
+        $taskGuid = $newTua->getTaskGuid();
+        $task = ZfExtended_Factory::get('editor_Models_Task');
+        /* @var $task editor_Models_Task */
+        $task->loadByTaskGuid($taskGuid);
+        $oldStep = $task->getWorkflowStepName();
+        
+        //set the finished date when the user finishes a role
+        if($newTua->getState() == $this->config->workflow::STATE_FINISH){
+            $newTua->setFinishedDate(NOW_ISO);
+            $newTua->save();
+        }
+        
+        $this->callActions($this->config, $oldStep, $newTua->getRole(), $newTua->getState());
+    }
+    
+    /**
+     * will be called after all associated users of a task has finished a task
+     * @param array $finishStat contains the info which of all different finishes are applicable
+     */
+    protected function handleAllFinish() {
+        $this->doDebug(self::HANDLE_JOB_ALLFINISH);
+    }
+    
+    /**
+     * will be called after first user of a role has finished a task
+     * @param array $finishStat contains the info which of all different finishes are applicable
+     */
+    protected function handleFirstFinishOfARole(){
+        $task = $this->config->task;
+        $taskState = $task->getState();
+        if($taskState == $task::STATE_UNCONFIRMED) {
+            //we have to confirm the task and retrigger task workflow triggers
+            // if task was unconfirmed but a lektor is set to finish, this implies confirming
+            $this->config->oldTask = clone $task;
+            $task->setState($task::STATE_OPEN);
+            
+            /* @var $taskHandler editor_Workflow_Default_TaskHandler */
+            $taskHandler = ZfExtended_Factory::get('editor_Workflow_Default_TaskHandler');
+            $taskHandler->execute($this->config);
+            
+            $task->save();
+            $task->setState($task::STATE_OPEN);
+        }
+        $this->doDebug(self::HANDLE_JOB_FIRSTFINISHOFAROLE);
+    }
+    
+    /**
+     * will be called after a user has finished a task
+     * @param array $finishStat contains the info which of all different finishes are applicable
+     */
+    protected function handleFirstFinish(){
+        $this->doDebug(self::HANDLE_JOB_FIRSTFINISH);
+    }
+    
+    /**
+     * debugging workflow
+     * @param string $msg
+     * @param array $data optional debuggin data
+     * @param bool $levelInfo optional, if true log in level info instead debug
+     */
+    protected function doDebug($msg, array $data = [], $levelInfo = false) {
+        $log = $this->config->workflow->getLogger($this->config->task);
+        
+        //add the job / tua
+        if(!empty($this->config->newTua)) {
+            $data['job'] = $this->config->newTua;
+        }
+        if($levelInfo) {
+            $log->info('E1013', $msg, $data);
+        }
+        else {
+            $log->debug('E1013', $msg, $data);
+        }
+    }
+}
