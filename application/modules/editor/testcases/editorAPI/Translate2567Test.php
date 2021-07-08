d002bd5ae222314443ca06cd995c7dc30fae3317
Thomas Lauria <thomas@mittagqi.com>
2021-07-08 16:24:01 +0200

    TRANSLATE-2567: TagProtection can not deal with line breaks in HTML attributes - tests created with invalid result

---
+++ application/modules/editor/testcases/editorAPI/Translate2567Test.php
@@ -0,0 +1,84 @@
+<?php
+/*
+START LICENSE AND COPYRIGHT
+
+ This file is part of translate5
+ 
+ Copyright (c) 2013 - 2021 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+
+ Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
+
+ This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
+ as published by the Free Software Foundation and appearing in the file agpl3-license.txt
+ included in the packaging of this file.  Please review the following information
+ to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3 requirements will be met:
+ http://www.gnu.org/licenses/agpl.html
+  
+ There is a plugin exception available for use with this release of translate5 for
+ translate5: Please see http://www.translate5.net/plugin-exception.txt or
+ plugin-exception.txt in the root folder of translate5.
+  
+ @copyright  Marc Mittag, MittagQI - Quality Informatics
+ @author     MittagQI - Quality Informatics
+ @license    GNU AFFERO GENERAL PUBLIC LICENSE version 3 with plugin-execption
+			 http://www.gnu.org/licenses/agpl.html http://www.translate5.net/plugin-exception.txt
+
+END LICENSE AND COPYRIGHT
+*/
+
+/**
+ * Testcase for TRANSLATE-2567 TagProtection can not deal with line breaks in HTML attributes
+ * For details see the issue.
+ */
+class Translate2567Test extends editor_Test_JsonTest {
+    public static function setUpBeforeClass(): void {
+        self::$api = $api = new ZfExtended_Test_ApiHelper(__CLASS__);
+        
+        $task = array(
+            'sourceLang' => 'de',
+            'targetLang' => 'en',
+            'edit100PercentMatch' => true,
+            'lockLocked' => 1,
+        );
+        
+        $appState = self::assertAppState();
+
+        self::assertNotContains('editor_Plugins_LockSegmentsBasedOnConfig_Bootstrap', $appState->pluginsLoaded, 'Plugin LockSegmentsBasedOnConfig should not be activated for this test case!');
+        self::assertNotContains('editor_Plugins_NoMissingTargetTerminology_Bootstrap', $appState->pluginsLoaded, 'Plugin NoMissingTargetTerminology should not be activated for this test case!');
+        
+        self::assertNeededUsers(); //last authed user is testmanager
+        self::assertLogin('testmanager');
+        
+        $zipfile = $api->zipTestFiles('testfiles/','testTask.zip');
+        
+        $api->addImportFile($zipfile);
+        $api->import($task);
+        
+        $api->addUser('testlector');
+        
+        //login in setUpBeforeClass means using this user in whole testcase!
+        $api->login('testlector');
+        
+        $task = $api->getTask();
+        //open task for whole testcase
+        $api->requestJson('editor/task/'.$task->id, 'PUT', array('userState' => 'edit', 'id' => $task->id));
+    }
+    
+    /**
+     * Testing segment values directly after import
+     */
+    public function testSegmentValuesAfterImport() {
+        $jsonFileName = 'expectedSegments.json';
+        $segments = $this->api()->getJson('editor/segment?page=1&start=0&limit=10', [], $jsonFileName);
+        $this->assertModelsEqualsJsonFile('Segment', $jsonFileName, $segments, 'Imported segments are not as expected!');
+    }
+
+    public static function tearDownAfterClass(): void {
+        $task = self::$api->getTask();
+        //open task for whole testcase
+        self::$api->login('testlector');
+        self::$api->requestJson('editor/task/'.$task->id, 'PUT', array('userState' => 'open', 'id' => $task->id));
+        self::$api->login('testmanager');
+        //self::$api->requestJson('editor/task/'.$task->id, 'DELETE');
+    }
+}
