e3ea378dad001dd7df3421cbfa0bb00c42873d06
aleksandar <aleksandar@mittagqi.com>
2021-07-22 15:41:05 +0200

    TRANSLATE-2583 Save config record instead of model sync

--- public/modules/editor/js/app/view/admin/config/GridViewController.js
+++ public/modules/editor/js/app/view/admin/config/GridViewController.js
@@ -114,14 +114,12 @@ Ext.define('Editor.view.admin.config.GridViewController', {
         if(Ext.Array.contains([record.CONFIG_LEVEL_INSTANCE,record.CONFIG_LEVEL_CLIENT],parseInt(record.get('level')))){
             Ext.Msg.alert('',view.strings.configChangeReloadMessageBoxText);
         }
-        view.getStore().sync({
+        record.save({
             success: function(rec, operation){
                 Editor.MessageBox.addSuccess(view.strings.updateConfigSuccessMessage);
-                record.commit();
             },
-            failure: function(response) {
-                Editor.app.getController('ServerException').handleException(response);
-                record.reject();
+            failure: function(rec, operation) {
+                Editor.app.getController('ServerException').handleCallback(rec, operation, false);
             }
         });
     },
