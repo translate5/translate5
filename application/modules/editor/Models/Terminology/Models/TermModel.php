ff99b1589e16b460a3c729b31fa86ac63fe6d7eb
Pavel Perminov <pavel.perminov.23@gmail.com>
2021-08-24 18:04:37 +0300

    1.TermModel->searchByParams(): WHERE clause logic amended:  - for processStatus-column  - added MATCH (`term` [, `proposal`]) AGAINST(':keyword' IN BOOLEAN MODE) usage    - wildcard fix  - refactored LOWER(<col>) LIKE LOWER(:keyword) building 2.added sql migration to  - change terms_term.(term|proposal) columns from TEXT to TINYTEXT  - add FULLTEXT index for above columns

--- application/modules/editor/Models/Terminology/Models/TermModel.php
+++ application/modules/editor/Models/Terminology/Models/TermModel.php
@@ -746,8 +746,11 @@ class editor_Models_Terminology_Models_TermModel extends ZfExtended_Models_Entit
         $where = [
             '`t`.`languageId` IN (' . $params['language'] . ')',
             '`t`.`collectionId` IN (' . $params['collectionIds'] . ')',
-            '`t`.`processStatus` IN ("' . str_replace(',', '","', $params['processStatus']) . '")',
         ];
+
+        // Append clause for prosessStatus
+        if ($params['processStatus']) $where []= '`t`.`processStatus` IN ("' . str_replace(',', '","', $params['processStatus']) . '")';
+        else if (!$isProposer)        $where []= '`t`.`processStatus` != "' . self::PROCESS_STATUS_UNPROCESSED . '"';
 
         // Mind attr-filters in WHERE clause
         if ($termEntryIds) array_unshift($where, '`t`.`termEntryId` IN (' . $termEntryIds . ')');
@@ -778,34 +781,53 @@ class editor_Models_Terminology_Models_TermModel extends ZfExtended_Models_Entit
           `t`.`termEntryTbxId` 
         ';
 
-        // Term query template
-        $termQueryTpl = '
-            SELECT %s 
-            FROM `terms_term` `t` %s
-            WHERE %s AND ' . implode(' AND ', $where) . '
-            ORDER BY `t`.`term` ASC
-        ';
-
         // Assume limit arg can be comma-separated string containing '<LIMIT>,<OFFSET>'
         list($limit, $offset) = explode(',', $params['limit']);
 
-        // Keyword WHERE clauses
-        $keywordWHERE = [
-            'LOWER(`t`.`term`)     LIKE LOWER(:keyword) COLLATE utf8mb4_bin',
-            'LOWER(`t`.`proposal`) LIKE LOWER(:keyword) COLLATE utf8mb4_bin',
-        ];
+        // Cols that we're going to search in by default
+        $cols = ['`t`.`term`', '`t`.`proposal`'];
 
         // If we should only search for `term`-column (e.g. `proposal`-column won't be involved)
-        if (!$isProposer || !in_array(self::PROCESS_STATUS_UNPROCESSED, editor_Utils::ar($params['processStatus'])))
+        if (!$isProposer || ($params['processStatus'] && !in_array(self::PROCESS_STATUS_UNPROCESSED, editor_Utils::ar($params['processStatus']))))
 
-            // Drop proposal-clause from $keywordWHERE
-            array_pop($keywordWHERE);
+            // Drop proposal-col from $cols, so it won't be mentioned in $keywordWHERE
+            array_pop($cols);
+
+        // Keyword WHERE clauses using LIKE
+        foreach ($cols as $col) $keywordWHERE []= sprintf('LOWER(%s) LIKE LOWER(:keyword) COLLATE utf8mb4_bin', $col);
 
         // Render keyword WHERE string
         $keywordWHERE = '(' . implode(' OR ', $keywordWHERE) . ')';
 
+        // If wildcards are used, convert them to the mysql syntax
+        $against = str_replace('?', '*', $params['query']);
+
+        // If we're not going to count $total - it means we're in autocomplete mode
+        if ($total === false) $against = trim($against, '*') . '*';
+
+        // Build match-against WHERE string
+        $againstWHERE = 'MATCH(' . implode(', ', $cols) . ') AGAINST(:against IN BOOLEAN MODE)';
+
+        // If it's a non '*'-query (e.g. non 'any'-query)
+        if (!preg_match('~^\*+$~', $against)) {
+
+            // Prepend $where with $keywordWHERE
+            array_unshift($where, $keywordWHERE);
+
+            // Prepend $where with $againstWHERE
+            array_unshift($where, $againstWHERE);
+        }
+
+        // Term query template
+        $termQueryTpl = '
+            SELECT %s 
+            FROM `terms_term` `t` %s
+            WHERE ' . implode(' AND ', $where) . '
+            ORDER BY `t`.`term` ASC
+        ';
+
         // Prepare params array
-        $bindParam = [':keyword' => $keyword];
+        $bindParam = [':keyword' => $keyword, ':against' => $against];
 
         // If we have to calculate total
         if ($total === true) {
