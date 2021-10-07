137d5051afe6bfe88053d24c328a1945e4456f40
Thomas Lauria <thomas@mittagqi.com>
2021-10-07 19:25:05 +0200

    fix tests respecting automatically added administrativeStatus attributes

--- application/modules/editor/testcases/editorAPI/TbxImportApiTest.php
+++ application/modules/editor/testcases/editorAPI/TbxImportApiTest.php
@@ -63,27 +63,27 @@ class TbxImportApiTest extends \ZfExtended_Test_ApiTestcase {
 
 
         //import the first tbx file,
-        $this->singleTest('Term.tbx', 8, 41, 6,3);
+        $this->singleTest('Term.tbx', 8, 49, 6,3);
 
         //change existing term entry attribute
         //change existing term attribute
         //change existing term content
         //add new term to term collection
-        $this->singleTest('Term1.tbx', 9, 45, 6,3);
+        $this->singleTest('Term1.tbx', 9, 54, 6,3);
 
         // different term entry id, different term id, same language and term content -> update the term and
         // check if the other terms in the tbx term entry can be merged
         // Merge: the term <term id="462bed50-6779-4cb1-be6b-223e78b54f26">Desk</term> will be merged to: <term id="462bed50-6779-4cb1-be6b-223e78b54f26">Table</term>
         // reason is because other terms in the importing term entry are merged and the found term entry is used to merge "Desk" to "Table"
-        $this->singleTest('Term2.tbx', 9, 45, 6,3);
+        $this->singleTest('Term2.tbx', 9, 54, 6,3);
 
         //add new terms to the term collection
         //handle the unknown tags
-        $this->singleTest('Export.tbx', 11, 64, 10,7);
+        $this->singleTest('Export.tbx', 11, 75, 10,7);
 
         // Change term content and add 2 new attributes (admin and note attributes)
         // On the second term, add 2 new attributes (termNote with custom type and note)
-        $this->singleTest('ExportTermChange.tbx', 11, 68, 10,7);
+        $this->singleTest('ExportTermChange.tbx', 11, 79, 10,7);
     }
 
     /***
