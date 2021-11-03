ffd0c641bd99d6aa3ccea68ad78528e685c3cd78
aleksandar <aleksandar@mittagqi.com>
2021-11-03 09:54:34 +0100

    TRANSLATE-2690 task is null: User association in import wizard

--- public/modules/editor/js/app/view/admin/task/UserAssocWizardViewController.js
+++ public/modules/editor/js/app/view/admin/task/UserAssocWizardViewController.js
@@ -152,7 +152,7 @@ Ext.define('Editor.view.admin.task.UserAssocWizardViewController', {
             success: function() {
                 me.getView().down('grid').getStore().load();
                 Editor.MessageBox.addSuccess('Assoc saved');
-                me.resetRecord();
+                me.onAddAssocBtnClick();
                 taskStore.load();
             }
         });
