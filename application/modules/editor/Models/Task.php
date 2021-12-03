efc0bc501f8ccde1e62b384c607f7d2b5812e2c4
aleksandar <aleksandar@mittagqi.com>
2021-12-03 12:55:10 +0100

    TRANSLATE-2732 Advanced filter users list anonymized users query

--- application/modules/editor/Models/Task.php
+++ application/modules/editor/Models/Task.php
@@ -319,14 +319,14 @@ class editor_Models_Task extends ZfExtended_Models_Entity_Abstract {
 
         $anonSql = '';
         if(!$ignoreAnonStuff) {
-            //filter out all anonymited tasks
+            //filter out all anonimized tasks
             //task is anonymized if runtimeOptions.customers.anonymizeUsers is set to 1 on task level
-            //if anonymizeUsers is not defined on taks level, the task customer anonymizeUsers value is used
+            //if anonymizeUsers is not defined on task level, the task customer anonymizeUsers value is used
             //if anonymizeUsers is also not defined on customer level, then the instance anonymizeUsers value is used
             $anonSql = 'AND filter.taskGuid NOT IN(SELECT IF((SELECT IF(t.value IS NOT NULL,t.value, if(c.value IS NOT NULL,c.value,z.value = 1)) FROM Zf_configuration z
                         LEFT JOIN LEK_customer_config c on z.name = c.name
                         LEFT JOIN LEK_task_config t on t.name = z.name
-                        WHERE (t.taskGuid = LEK_task.taskGuid OR c.customerId = LEK_task.customerId)
+                        WHERE ((t.taskGuid = LEK_task.taskGuid) OR (c.customerId = LEK_task.customerId AND t.taskGuid IS NULL)) 
                         AND z.name =  "runtimeOptions.customers.anonymizeUsers") = 1,LEK_task.taskGuid,NULL) AS s
                         FROM LEK_task
                         GROUP BY LEK_task.taskGuid
