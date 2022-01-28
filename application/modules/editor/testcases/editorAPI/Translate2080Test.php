acd32f077242178159d8041c65af36771fc65e0f
aleksandar <aleksandar@mittagqi.com>
2022-01-28 09:56:38 +0100

    TRANSLATE-2080 Round up project creation wizard by refactoring and enhancing first screen (tests)

--- application/modules/editor/testcases/editorAPI/Translate2080Test.php
+++ application/modules/editor/testcases/editorAPI/Translate2080Test.php
@@ -26,14 +26,15 @@ START LICENSE AND COPYRIGHT
 END LICENSE AND COPYRIGHT
 */
 
-/**
+/**+
+ * Create project with 2 tasks using importUpload_language and importUpload_type api endpoints.
+ * On the backend side, the editor_Models_Import_DataProvider_Project data provider will be used to generate the file structure for the uploaded files.
  */
 class Translate2080Test extends editor_Test_JsonTest {
 
     public static function setUpBeforeClass(): void {
 
         self::$api = $api = new ZfExtended_Test_ApiHelper(__CLASS__);
-        
 
         self::assertNeededUsers(); //last authed user is testmanager
         self::assertCustomer();//assert the test customer
@@ -41,36 +42,37 @@ class Translate2080Test extends editor_Test_JsonTest {
     }
 
     /***
+     * Create 2 project tasks. The second task will have "it" as relais language.
+     * The relais file will be matched based on the name.
      */
-    public function testImportProject(){
-        $this->createTask();
-    }
-
-    /***
-     * Create and import the task
-     */
-    protected function createTask(){
+    public function testImportProjectWithRelais(){
         $task =[
             'taskName' => 'API Testing::'.__CLASS__, //no date in file name possible here!
             'sourceLang' => 'de',
             'targetLang' => ['en','mk'],
+            'relaisLang' => 'it',
             'customerId'=>self::api()->getCustomer()->id,
             'edit100PercentMatch' => true,
-            'importUpload_language' => ['en','mk'],
-            'importUpload_type' => ['workfile','workfile'],
-            'autoStartImport'=>1
+            'importUpload_language' => ['en','mk','it'],
+            'importUpload_type' => ['workfiles','workfiles','pivot'],
+            'autoStartImport' => 1
         ];
         self::assertLogin('testmanager');
-        self::$api->addImportFile(self::$api->getFile('en.xlf'));
-        self::$api->addImportFile(self::$api->getFile('mk.xlf'));
+        self::$api->addImportFiles(self::$api->getFile('en.xlf'));
+        self::$api->addImportFiles(self::$api->getFile('mk.xlf'));
+        self::$api->addImportFiles(self::$api->getFile('mk.xlf'));
         self::$api->import($task,false);
         error_log('Task created. '.$this->api()->getTask()->taskName);
+
+        $projectTasks = self::$api->getTask()->projectTasks;
+
+        $this->assertEquals(count($projectTasks), 2, 'No project tasks created.');
+
+        $task = self::$api->getTask();
+        self::$api->requestJson('editor/task/'.$task->id, 'DELETE');
     }
 
     public static function tearDownAfterClass(): void {
-        $task = self::$api->getTask();
-        //open task for whole testcase
-        self::$api->login('testmanager');
-        self::$api->requestJson('editor/task/'.$task->id, 'DELETE');
+
     }
 }
