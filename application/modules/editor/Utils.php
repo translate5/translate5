363f32e413b0973f13521bb66f00fe71c319f9b6
Axel <axel@mittagqi.com>
2022-02-15 11:31:40 +0100

    HOTFIX: escapeshellarg does strip non-ASCII Chars leading to problems with user-files containing such chars

--- application/modules/editor/Utils.php
+++ application/modules/editor/Utils.php
@@ -213,6 +213,28 @@ class editor_Utils {
         return trim(preg_replace('/[\-]+/i', '-', $name), '-');
     }
     /**
+     * A replacement for escapeshellarg that does NOT do any locale-specific UTF-8 stripping
+     * 
+     * escapeshellarg() adds single quotes around a string and quotes/escapes any existing single quotes allowing you to pass a string directly to a shell function and having it be treated as a single safe argument. This function should be used to escape individual arguments to shell functions coming from user input.
+     * The shell functions include exec(), system() and the backtick operator.
+     *
+     * On Windows, escapeshellarg() instead replaces percent signs, exclamation marks (delayed variable substitution) and double quotes with spaces and adds double quotes around the string. 
+     * Furthermore, each streak of consecutive backslashes (\) is escaped by one additional backslash. 
+     *
+     * @param string $command
+     * @return string
+     */
+    public static function escapeshellarg(string $command) : string {
+        // Windows specific escaping: remove 
+        if(PHP_OS_FAMILY == 'Windows'){
+            // QUIRK: on windows the PHP implementation is used since I cannot test the result.
+            // TODO/FIXME: Replicate Windows logic if neccessary / if the UTF-8 stripping happens on Windows too
+            return escapeshellarg($command);
+        }
+        // UNIX specific escaping: only Ticks
+        return "'".implode("'\''", explode("'", $command))."'";
+    }
+    /**
      * Generates a websafe filename out of any string
      * The resulting string is lowercase and has all whitespace stripped, blanks are replaced with "-"
      * Take into account the returned string may be empty !
