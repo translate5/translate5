c9f7c548cc403160e93cdca1b5dc12cad2ff44d2
Thomas Lauria <thomas@mittagqi.com>
2022-03-10 10:23:43 +0100

    create and run tests also from plugins

--- application/modules/editor/testcases/editorAPI/BasicSegmentEditingTest.php
+++ application/modules/editor/testcases/editorAPI/BasicSegmentEditingTest.php
@@ -61,7 +61,7 @@ class BasicSegmentEditingTest extends editor_Test_JsonTest {
         ];
         self::$api->testConfig($tests);
         
-        $api->addImportFile('editorAPI/MainTest/simple-en-de.zip');
+        $api->addImportFile('MainTest/simple-en-de.zip');
         $api->import($task);
         
         $api->addUser('testlector');
