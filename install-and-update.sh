0b71e3db58f0f790d204f3413680907716aaf2d0
Thomas Lauria <thomas@mittagqi.com>
2022-03-17 15:29:34 +0100

    improve error message

--- install-and-update.sh
+++ install-and-update.sh
@@ -3,8 +3,8 @@ CMD_PHP="${CMD_PHP:-/usr/bin/php}"
 CMD_MYSQL="${CMD_MYSQL:-/usr/bin/mysql}"
 
 # make sure PHP and MySQL binary exist; else die with an error message
-type -p $CMD_PHP &>/dev/null || { echo "$CMD_PHP not found. Set \$CMD_PHP in $0"; exit 1; }
-type -p $CMD_MYSQL &>/dev/null || { echo "$CMD_MYSQL not found. Set \$CMD_MYSQL in $0"; exit 1; }
+type -p $CMD_PHP &>/dev/null || { echo "$CMD_PHP not found. Set CMD_PHP as environment variable!"; exit 1; }
+type -p $CMD_MYSQL &>/dev/null || { echo "$CMD_MYSQL not found. Set CMD_MYSQL as environment variable!"; exit 1; }
 
 case "$1" in
 "")         CONFIG=""
