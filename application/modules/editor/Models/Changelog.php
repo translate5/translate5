4472c7ee8d0b6cf57cabf7049fc6aaf033c6d0df
Christoph Lamparter <christoph@mittagqi.com>
2022-04-13 16:57:35 +0200

    fix getTotalCount signature

--- application/modules/editor/Models/Changelog.php
+++ application/modules/editor/Models/Changelog.php
@@ -66,7 +66,7 @@ class editor_Models_Changelog extends ZfExtended_Models_Entity_Abstract {
      * @param int $userGroupId
      * @return integer
      */
-    public function getTotalCount(){
+    public function getTotalCount(): int {
         $userGroupId = 15;
         $db = $this->db->getAdapter();
         $s = $this->db->select()->where('LEK_change_log.userGroup & '.$db->quote($userGroupId, 'INTEGER').'');
