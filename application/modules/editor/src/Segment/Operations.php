33424db483ed7e922ba3e227e523118e432edf55
Thomas Lauria <thomas@mittagqi.com>
2022-05-23 14:29:05 +0200

    TRANSLATE-2314 Be able to lock/unlock segments in the editor by a PM - refactoring so that Segment Operations are mostly extracted into separate Operations class - batch bookmarking implemented

--- application/modules/editor/src/Segment/Operations.php
+++ application/modules/editor/src/Segment/Operations.php
@@ -21,7 +21,7 @@
   @copyright  Marc Mittag, MittagQI - Quality Informatics
   @author     MittagQI - Quality Informatics
   @license    GNU AFFERO GENERAL PUBLIC LICENSE version 3 with plugin-execption
- 			 http://www.gnu.org/licenses/agpl.html http://www.translate5.net/plugin-exception.txt
+              http://www.gnu.org/licenses/agpl.html http://www.translate5.net/plugin-exception.txt
 
  END LICENSE AND COPYRIGHT
  */
