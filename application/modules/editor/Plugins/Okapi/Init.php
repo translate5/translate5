7c9b78b2bd01665533e424fc45c8224f638407bc
Axel <axel@mittagqi.com>
2022-06-08 14:59:20 +0200

    ENHANCEMENT: evaluate system default bconf-id only once on customer index handler

--- application/modules/editor/Plugins/Okapi/Init.php
+++ application/modules/editor/Plugins/Okapi/Init.php
@@ -742,7 +742,8 @@ class editor_Plugins_Okapi_Init extends ZfExtended_Plugin_Abstract {
     {
         /** @var ZfExtended_View $view */
         $view = $event->getParam('view');
-        $bconf = new editor_Plugins_Okapi_Models_Bconf();
+        // cache to prevent multiple fetching of the non-customer default bconf
+        $defaultBconfId = NULL;
         $meta = new editor_Models_Db_CustomerMeta();
         $metas = $meta->fetchAll('defaultBconfId IS NOT NULL')->toArray();
         $bconfIds = array_column($metas, 'defaultBconfId', 'customerId');
@@ -750,7 +751,11 @@ class editor_Plugins_Okapi_Init extends ZfExtended_Plugin_Abstract {
             if(array_key_exists($customer['id'], $bconfIds)){
                 $customer['defaultBconfId'] = (int) $bconfIds[$customer['id']];
             } else {
-                $customer['defaultBconfId'] = $bconf->getDefaultBconfId();
+                if($defaultBconfId === NULL){
+                    $bconf = new editor_Plugins_Okapi_Models_Bconf();
+                    $defaultBconfId = $bconf->getDefaultBconfId();
+                }
+                $customer['defaultBconfId'] = $defaultBconfId;
             }
         }
     }
