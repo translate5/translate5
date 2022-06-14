7a2b429c10ffabd8a6eef9a0a0f5c9ec957a7dd8
Axel <axel@mittagqi.com>
2022-06-14 16:34:28 +0200

    - Rename Bconffilter to BconfFilter II

---
+++ application/modules/editor/Plugins/Okapi/public/js/model/BconfFilterModel.js
@@ -0,0 +1,166 @@
+/*
+ START LICENSE AND COPYRIGHT
+
+ This file is part of translate5
+
+ Copyright (c) 2013 - 2022 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+
+ Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
+
+ This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
+ as published by the Free Software Foundation and appearing in the file agpl3-license.txt
+ included in the packaging of this file.  Please review the following information
+ to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3 requirements will be met:
+ http://www.gnu.org/licenses/agpl.html
+
+ There is a plugin exception available for use with this release of translate5 for
+ translate5: Please see http://www.translate5.net/plugin-exception.txt or
+ plugin-exception.txt in the root folder of translate5.
+
+ @copyright  Marc Mittag, MittagQI - Quality Informatics
+ @author     MittagQI - Quality Informatics
+ @license    GNU AFFERO GENERAL PUBLIC LICENSE version 3 with plugin-execption
+ 		     http://www.gnu.org/licenses/agpl.html http://www.translate5.net/plugin-exception.txt
+
+ END LICENSE AND COPYRIGHT
+ */
+Ext.define('Editor.plugins.Okapi.model.BconfFilterModel', {
+    extend: 'Ext.data.Model',
+    requires: ['Editor.util.type.StringSet'],
+    alias: 'model.bconffilterModel',
+    getId: function(){
+        var bconfId = this.get('bconfId'),
+            okapiId = this.get('okapiId');
+        return `${bconfId}-.-${okapiId}`; // Slash / as separator will lead to 404
+    },
+    proxy: {
+        idParam: 'id',
+        type: 'rest',
+        url: Editor.data.restpath + 'plugins_okapi_bconffilter',
+        reader: {
+            rootProperty: 'rows',
+        },
+        writer: {
+            encode: true,
+            rootProperty: 'data',
+        },
+        api: {
+            read: undefined /** @link self.proxy.setBconfId sets this for easy filtering */
+        },
+        /**
+         * Sets the id of the currently opened bconf to
+         * - the defaultValue of the bconfId field
+         * - the read api param
+         * @see Editor.plugins.Okapi.view.BconfFilterGrid.initComponent
+         */
+        setBconfId: function(bconfId){
+            var proxy = this;
+            proxy.getModel().getField('bconfId').defaultValue = proxy.bconfId = bconfId;
+            proxy.api.read = proxy.getUrl() + '?bconfId=' + bconfId
+        },
+    },
+    idProperty: 'okapiId',
+    fields: [{
+        name: 'okapiId',
+        type: 'string',
+    }, {
+        name: 'bconfId',
+        type: 'int',
+        //reference: 'bconfmodel', // leads to fiels being null
+        critical: true,
+        defaultValue: 0 /** @see self.proxy.setBconfId */
+    }, {
+        name: 'isCustom',
+        type: 'bool',
+        defaultValue: true,
+        persist: false
+    }, {
+        name: 'description',
+        type: 'string'
+    },
+        /**
+         * @readonly This models a 1:n relation. To manipulate, retrieve value via .get() and use
+         * @see StringSet
+         * @property {bool} unchanged Flag that shows if extensions were changed during an edit
+         * @link BconfFilterGridController.prepareFilterEdit,saveEdit,cancelEdit
+         */
+        {
+            name: 'extensions',
+            persist: true, // Normal saving causes many requests
+            /**
+             * Only allow initializing, changes will be handled by
+             * Must always create new Set for change detection
+             * @see Editor.plugins.Okapi.model.BconfFilterModel.addExtension
+             * @see Editor.plugins.Okapi.model.BconfFilterModel.removeExtension
+             * @return {StringSet}
+             */
+            convert: function(v, rec){ // null is passed after saving the record
+                if(v && v.op){ // Special handler with op key
+                    var set = new StringSet(rec.data[this.name])
+                    set[v.op](v.extension)
+                    return set
+                } else {
+                    var array = Ext.isString(v) ? v.split(/[,\.\s]+/) : Ext.Array.from(v)
+                    return new StringSet(array);
+                }
+            },
+            serialize: function(v){
+                return v.toString();
+            },
+            isEqual: function(a, b){
+                return a.toString() === b.toString();
+            },
+
+        },
+    ],
+    isValid: function(){
+        return this.get('isCustom') // don't save default filters
+            && this.get('bconfId') > 0; // don't save unknow filters from extensions-mapping
+    },
+    /**
+     * Return a displayname for the given id
+     * @return {string} The filterName with the filterId in parantheses behind
+     */
+    getDisplayName(){
+        return this.get('name') + '&nbsp;(' + this.id + ')';
+    },
+
+    /**
+     * Add Extension to this filter. If it belonged to another filter before, remove from there.
+     * @param {String} extension
+     * @param {Editor.plugins.Okapi.model.BconfFilterModel} from Record where to take the extension from#
+     * @param {Boolean} isRevert Inidcates if to set dirty or not
+     */
+    addExtension: function(extension, from, isRevert = false){
+        var extMap = this.store.extMap,
+            filters = Editor.util.Util.getUnfiltered(this.store);
+
+        from = (from !== undefined) ? from : filters.getByKey(extMap.get(extension))
+        if(from){
+            from.removeExtension(extension, null, isRevert);
+        }
+        this.set('extensions', {op: 'add', extension}, {dirty: !isRevert})
+        extMap.set(extension, this.id)
+
+        if(from){
+            return from
+        }
+    },
+    /**
+     * Remove extension this filter. If it belonged to another filter before, remove from there.
+     * @param {String} extension
+     * @param {Editor.plugins.Okapi.model.BconfFilterModel} to
+     */
+    removeExtension: function(extension, to, isRevert){
+        var filters = Editor.util.Util.getUnfiltered(this.store);
+
+        this.set('extensions', {op: 'delete', extension}, {dirty: !isRevert})
+        this.extMap.delete(extension)
+
+        // TODO: defaults 'to' receiver based on current system default (via global varaible?)
+        if(to){
+            to.addExtension(extension, null, isRevert);
+            return to
+        }
+    },
+});
---
+++ application/modules/editor/Plugins/Okapi/public/js/view/BconfFilterGrid.js
@@ -0,0 +1,233 @@
+/*
+ START LICENSE AND COPYRIGHT
+
+ This file is part of translate5
+
+ Copyright (c) 2013 - 2022 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+
+ Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
+
+ This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
+ as published by the Free Software Foundation and appearing in the file agpl3-license.txt
+ included in the packaging of this file.  Please review the following information
+ to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3 requirements will be met:
+ http://www.gnu.org/licenses/agpl.html
+
+ There is a plugin exception available for use with this release of translate5 for
+ translate5: Please see http://www.translate5.net/plugin-exception.txt or
+ plugin-exception.txt in the root folder of translate5.
+
+ @copyright  Marc Mittag, MittagQI - Quality Informatics
+ @author     MittagQI - Quality Informatics
+ @license    GNU AFFERO GENERAL PUBLIC LICENSE version 3 with plugin-execption
+ 		     http://www.gnu.org/licenses/agpl.html http://www.translate5.net/plugin-exception.txt
+
+ END LICENSE AND COPYRIGHT
+ */
+/**
+ * Grid for viewing and editing the different Okapi Filter configurations of a bconf
+ * The supported
+ * @property {Ext.grid.plugin.Editing} editingPlugin Undocumented property used for shorthand access (for this.findPlugin('rowediting'))
+ * @see Ext.grid.plugin.Editing.init
+ */
+Ext.define('Editor.plugins.Okapi.view.BconfFilterGrid', {
+    extend: 'Ext.grid.Panel',
+    requires: [
+        'Editor.plugins.Okapi.view.BconfFilterGridController',
+        'Editor.plugins.Okapi.store.BconfFilterStore'
+    ],
+    alias: 'widget.bconffiltergrid',
+    controller: 'bconffilterGridController',
+    config: {
+        bconf: null,
+    },
+    editingPlugin: null, // placeholder for undocumented property, see JSDoc
+    plugins: [{
+        ptype: 'rowediting',
+        clicksToEdit: 3, // QUIRK: 1 not possible, triggers on actioncolumns TODO: limit to non actionCols, add pointerCls
+        removeUnmodified: true,
+    }],
+    title: {text: 'Okapi Filters', flex: 0},
+    initComponent: function(){
+        var me = this,
+            bconf = me.getBconf().getData();
+        me.title.text += ` in <i data-qtip="${bconf.description}">${bconf.name}.bconf</i>`;
+        me.callParent();
+        me.getStore().getProxy().setBconfId(bconf.id); // for records and backend filter
+    },
+    helpSection: 'useroverview',
+    cls: 'actionColGrid',
+    text_cols: {
+        customFilterName: '#UT#Customized Okapi Filter Type',
+        name: '#UT#Name',
+        extensions: '#UT#Extensions',
+        description: '#UT#Description',
+        action: '#UT#Actions',
+        notes: '#UT#Notes',
+        upload: '#UT#upload',
+        srx: '#UT#SRX',
+        pipeline: '#UT#Pipeline',
+    },
+    strings: {
+        configuration: '#UT#Configur Filter',
+        remove: '#UT#Remove',
+        copy: '#UT#Copy',
+        upload: '#UT#Upload',
+        addBconf: '#UT#Add Bconf',
+        showDefaultFilter: '#UT#Show Okapi Defaults Filters',
+        customizeFilter: '#UT#Customize Filter',
+        in: '#in',
+    },
+    store: {
+        type: 'bconffilterStore'
+    },
+    viewConfig: {
+        getRowClass: function(bconf){
+            if(!bconf.get('editable')){
+                return 'not-editable';
+            }
+            return '';
+        },
+        reference: 'gridview'
+    },
+    initConfig: function(instanceConfig){
+        var me = this,
+            itemFilter = function(item){ // TODO: Add authorization check
+                return true;
+            },
+            config = {
+                header: {
+                    defaults: {margin: '0 10 0'},
+                    items: [{
+                        xtype: 'button',
+                        reference: 'showDefaultsBtn',
+                        enableToggle: true,
+                        toggleHandler: 'toggleDefaultsFilter',
+                        text: '#UT#Show Okapi Defaults Filters',
+                    }, {
+                        xtype: 'tbspacer',
+                        flex: 1
+                    }, {
+                        xtype: 'textfield',
+                        width: 300,
+                        checkChangeBuffer: 300,
+                        itemId: 'search',
+                        flex: 1,
+                        emptyText: Editor.plugins.Okapi.view.BconfGrid.prototype.strings.searchEmptyText,
+                        triggers: {
+                            clear: {
+                                cls: Ext.baseCSSPrefix + 'form-clear-trigger',
+                                handler: field => field.setValue(null) || field.focus(),
+                                hidden: true
+                            }
+                        },
+                    }, {
+                        xtype: 'button',
+                        iconCls: 'x-fa fa-undo',
+                        text: me.strings.refresh,
+                        handler: function(btn){
+                            btn.up('grid').getStore().reload();
+                        }
+                    },
+                    ]
+                },
+                columns: [{
+                    xtype: 'gridcolumn',
+                    dataIndex: 'name',
+                    editor: {
+                        xtype: 'textfield',
+                        allowOnlyWhitespace: false,
+                        lastVal: [],
+                        validator: function(name){
+                            if(name === this.lastVal[0]){ // already validated
+                                return this.lastVal[1];
+                            }
+                            var view = this.column.getView(),
+                                records = view.getStore().getData().getSource().items,
+                                nameIsUnique = !records.some(rec => rec.data.name === name && rec.id !== view.selection.id)
+                                    || Editor.plugins.Okapi.view.BconfGrid.prototype.strings.nameExists; // errormessage
+                            this.lastVal = [name, nameIsUnique]; // cache validation result
+                            return nameIsUnique;
+                        }
+                    },
+                    stateId: 'name',
+                    width: 300,
+                    filter: {
+                        type: 'string'
+                    },
+                    text: me.text_cols.name
+                }, {
+                    xtype: 'gridcolumn',
+                    dataIndex: 'okapiId',
+                    width: 300,
+
+                    text: 'okapiId'
+                }, {
+                    xtype: 'gridcolumn',
+                    dataIndex: 'mimeType',
+                    width: 300,
+                    editor: {
+                        xtype: 'textfield'
+                    },
+                    text: 'mimeType'
+                }, {
+                    xtype: 'gridcolumn',
+                    dataIndex: 'extensions',
+                    width: 200,
+                    stateId: 'extensions',
+                    filter: {
+                        type: 'string'
+                    },
+                    text: me.text_cols.extensions,
+                    /**
+                     * @property {object} changelog Saves changes for rollback
+                     * @see BconfFilterGridController.prepareFilterEdit
+                     */
+                    editor: { //TODO: add tooltip (tpl?) with current filter of extension
+                        xtype: 'tagfield',
+                        itemId: 'extMap',
+                        queryMode: 'local',
+                        createNewOnEnter: true,
+                        createNewOnBlur: true,
+                        filterPickList: true, // ture clears list on custom value
+                        changelog: null,
+                    },
+                }, {
+                    xtype: 'gridcolumn',
+                    dataIndex: 'description',
+                    stateId: 'notes',
+                    flex: 1,
+                    editor: {
+                        xtype: 'textfield'
+                    },
+                    text: me.text_cols.notes
+                }, {
+                    xtype: 'actioncolumn',
+                    width: 3 * 28 + 8 + 28,
+                    stateId: 'okapiGridActionColumn',
+                    align: 'center',
+                    text: me.text_cols.action,
+                    items: Ext.Array.filter([{
+                        tooltip: me.strings.configuration,
+                        isAllowedFor: 'bconfEdit',
+                        isDisabled: 'isEditDisabled',
+                        glyph: 'f044@FontAwesome5FreeSolid',
+                        handler: 'editfilter'
+                    }, {
+                        tooltip: me.strings.configuration,
+                        isAllowedFor: 'isFromTranslate5',
+                        glyph: 'f24d@FontAwesome5FreeSolid',
+                        handler: 'copy'
+                    }, {
+                        tooltip: me.strings.remove,
+                        isAllowedFor: 'bconfDelete',
+                        isDisabled: 'isDeleteDisabled',
+                        glyph: 'f2ed@FontAwesome5FreeSolid',
+                        handler: 'delete'
+                    }], itemFilter)
+                }],
+            };
+        return me.callParent([Ext.apply(config, instanceConfig)]);
+    },
+
+});
