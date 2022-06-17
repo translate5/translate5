205a3a0f379282001527708c178ceb3ce9773286
leonkj <leon@mittagqi.com>
2022-06-17 11:36:22 +0300

    TRANSLATE-2270 Fix tags class in editor

--- application/modules/editor/Plugins/TMMaintenance/public/resources/editor/editor.js
+++ application/modules/editor/Plugins/TMMaintenance/public/resources/editor/editor.js
@@ -3,12 +3,13 @@ import {Essentials} from "@ckeditor/ckeditor5-essentials";
 import {Paragraph} from "@ckeditor/ckeditor5-paragraph";
 import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting';
 import ImageInline from "@ckeditor/ckeditor5-image/src/imageinline";
+import {GeneralHtmlSupport} from "@ckeditor/ckeditor5-html-support";
 
 function create(element) {
     return ClassicEditor.create(
         element,
         {
-            plugins: [Essentials, Paragraph, SourceEditing, ImageInline],
+            plugins: [Essentials, Paragraph, GeneralHtmlSupport, SourceEditing, ImageInline],
             toolbar: [ 'sourceEditing'],
             htmlSupport: {
                 allow: [
