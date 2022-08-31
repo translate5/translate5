2fca8637645f49c9c8e19d4b75bc3f016ae64124
Thomas Lauria <thomas@mittagqi.com>
2022-08-31 13:11:47 +0200

    TRANSLATE-3041: Wrong whitespace tag numbering leads to non working whitespace added QA check - changed the re-placement in the AutoQA to a unified place - changed character tags to show their placeholder instead of <NR/> in the UI

--- application/modules/editor/Models/Segment/Whitespace.php
+++ application/modules/editor/Models/Segment/Whitespace.php
@@ -48,19 +48,52 @@ class editor_Models_Segment_Whitespace {
      */
     const ENTITY_MODE_OFF = 'off';
     
-    const WHITESPACE_TAGS = ['hardReturn', 'softReturn', 'macReturn', 'space', 'tab', 'char'];
-
     /**
      * @var array
      */
-    const WHITESPACE_TAG_LIST = [
+    const WHITESPACE_TAGS = [
         'hardReturn',
         'softReturn',
         'macReturn',
-        'char',
-        'tab',
         'space',
+        'tab',
+        'char',
     ];
+
+    /**
+     * general replacement character for unknown characters (U+FFFD)
+     */
+    const LABEL_REPLACEMENT_CHARACTER = '�';
+
+    /**
+     * Label for spaces
+     * U+00B7      c2 b7       &middot;    &#183;      MIDDLE DOT
+     */
+    const LABEL_SPACE = '·';
+
+    /**
+     * Label for newlines
+     * U+21B5      e2 86 b5    &crarr;     &#8629;     DOWNWARDS ARROW WITH CORNER LEFTWARDS
+     */
+    const LABEL_NEWLINE = '↵';
+
+    /**
+     * Label for tabs
+     * U+2192      e2 86 92    &rarr;      &#8594;     RIGHTWARDS ARROW
+     */
+    const LABEL_TAB = '→';
+
+    /**
+     * Label for nbsb
+     * U+23B5	⎵	e2 8e b5		&#9141;	⎵ 	BOTTOM SQUARE BRACKET
+     */
+    const LABEL_NBSP = '⎵';
+
+    /**
+     * Label for characters
+     * U+25A1 White Square as replacement for characters
+     */
+    const LABEL_CHARACTER = '□';
 
     /**
      * Return search and replace map
@@ -83,29 +116,38 @@ class editor_Models_Segment_Whitespace {
      * List of unicode characters to be protected
      * @var array
      */
-    protected array $protectedUnicodeList = [
-        '/\p{Co}/u', //Alle private use chars
-        '/\x{2028}/u', //Hex UTF-8 bytes or codepoint 	E2 80 A8//schutzbedürftiger Whitespace + von mssql nicht vertragen
-        '/\x{2029}/u', //Hex UTF-8 bytes 	E2 80 A9//schutzbedürftiger Whitespace + von mssql nicht vertragen
-        //we do not escape that any more - mssql not in use '"\x{201E}"u', //Hex UTF-8 bytes 	E2 80 9E //von mssql nicht vertragen
-        //we do not escape that any more - mssql not in use '"\x{201C}"u' //Hex UTF-8 bytes 	E2 80 9C//von mssql nicht vertragen
-        '/\x{0003}/u', //Hex UTF-8 bytes or codepoint of END OF TEXT //TS-240
-        '/\x{0008}/u', //Hex UTF-8 bytes or codepoint of backspace
-        '/\x{0009}/u', //Hex UTF-8 bytes or codepoint of horizontal tab
-        '/\x{000B}/u', //Hex UTF-8 bytes or codepoint of vertical tab
-        '/\x{000C}/u', //Hex UTF-8 bytes or codepoint of page feed
-        '/\x{0085}/u', //Hex UTF-8 bytes or codepoint of control sign for next line
-        '/\x{00A0}/u', //Hex UTF-8 bytes or codepoint of protected space
-        '/\x{1680}/u', //Hex UTF-8 bytes or codepoint of Ogam space
-        '/\x{180E}/u', //Hex UTF-8 bytes or codepoint of mongol vocal divider
-        '/\x{2028}/u', //Hex UTF-8 bytes or codepoint of line separator
-        '/\x{202F}/u', //Hex UTF-8 bytes or codepoint of small protected space
-        '/\x{205F}/u', //Hex UTF-8 bytes or codepoint of middle mathematical space
-        '/\x{3000}/u', //Hex UTF-8 bytes or codepoint of ideographic space
-        '/\x{200B}/u', //Hex UTF-8 bytes or codepoint of zero width space
-        '/\x{FEFF}/u', //Hex UTF-8 bytes or codepoint of ZERO WIDTH NO-BREAK SPACE / unicode BOM
-        '/[\x{2000}-\x{200A}]/u', //Hex UTF-8 bytes or codepoint of eleven different small spaces, Haarspatium and em space
-    ]; //Hex UTF-8 bytes 	E2 80 9C//von mssql nicht vertragen
+    const PROTECTED_CHARACTERS = [
+        //array semantics:
+        // 'regex'     => ['utf8 encoding as done by our unpack', 'name as from https://www.compart.com/en/unicode/'],
+        '/\p{Co}/u'   => [], //All private use chars, as HTML &#xE000; empty array to trigger default text
+        '/\x{0003}/u' => ['ts' => '03', 'text' => '[ETX]', 'title' => 'End of Text (ETX)'], //TS-240
+        '/\x{0008}/u' => ['ts' => '08', 'text' => '[BS]', 'title' => 'Backspace (BS)'],
+        '/\x{0009}/u' => ['ts' => '09', 'text' => '[HT]', 'title' => 'Character Tabulation (HT,TAB)'],
+        '/\x{000B}/u' => ['ts' => '0b', 'text' => '[VT]', 'title' => 'Line Tabulation (VT)'],
+        '/\x{000C}/u' => ['ts' => '0c', 'text' => '[FF]', 'title' => 'Form Feed (FF)'],
+        '/\x{0085}/u' => ['ts' => 'c285', 'text' => '[NEL]', 'title' => 'Next Line (NEL)'],
+        '/\x{00A0}/u' => ['ts' => 'c2a0', 'text' => self::LABEL_NBSP, 'title' => 'No-Break Space (NBSP)'],
+        '/\x{1680}/u' => ['ts' => 'e19a80', 'text' => self::LABEL_CHARACTER, 'title' => 'Ogham Space Mark'],
+        '/\x{180E}/u' => ['ts' => 'e1a08e', 'text' => '[MVS]', 'title' => 'Mongolian Vowel Separator (MVS)'],
+        '/\x{2000}/u' => ['ts' => 'e28080', 'text' => self::LABEL_CHARACTER, 'title' => 'En Quad'],
+        '/\x{2001}/u' => ['ts' => 'e28081', 'text' => self::LABEL_CHARACTER, 'title' => 'Em Quad'],
+        '/\x{2002}/u' => ['ts' => 'e28082', 'text' => self::LABEL_CHARACTER, 'title' => 'En Space'],
+        '/\x{2003}/u' => ['ts' => 'e28083', 'text' => self::LABEL_CHARACTER, 'title' => 'Em Space'],
+        '/\x{2004}/u' => ['ts' => 'e28084', 'text' => self::LABEL_CHARACTER, 'title' => 'Three-Per-Em Space'],
+        '/\x{2005}/u' => ['ts' => 'e28085', 'text' => self::LABEL_CHARACTER, 'title' => 'Four-Per-Em Space'],
+        '/\x{2006}/u' => ['ts' => 'e28086', 'text' => self::LABEL_CHARACTER, 'title' => 'Six-Per-Em Space'],
+        '/\x{2007}/u' => ['ts' => 'e28087', 'text' => self::LABEL_CHARACTER, 'title' => 'Figure Space'],
+        '/\x{2008}/u' => ['ts' => 'e28088', 'text' => self::LABEL_CHARACTER, 'title' => 'Punctuation Space'],
+        '/\x{2009}/u' => ['ts' => 'e28089', 'text' => self::LABEL_CHARACTER, 'title' => 'Thin Space'],
+        '/\x{200A}/u' => ['ts' => 'e2808a', 'text' => self::LABEL_CHARACTER, 'title' => 'Hair Space'],
+        '/\x{200B}/u' => ['ts' => 'e2808b', 'text' => '[SWSP]', 'title' => 'Zero Width Space (ZWSP)'],
+        '/\x{2028}/u' => ['ts' => 'e280a8', 'text' => '[LS]', 'title' => 'Line Separator'],
+        '/\x{2029}/u' => ['ts' => 'e280a9', 'text' => '[PS]', 'title' => 'Paragraph Separator'],
+        '/\x{202F}/u' => ['ts' => 'e280af', 'text' => '[NNBSP]', 'title' => 'Narrow No-Break Space (NNBSP)'],
+        '/\x{205F}/u' => ['ts' => 'e2819f', 'text' => '[MMSP]', 'title' => 'Medium Mathematical Space (MMSP)'],
+        '/\x{3000}/u' => ['ts' => 'e38080', 'text' => self::LABEL_CHARACTER, 'title' => 'Ideographic Space'],
+        '/\x{FEFF}/u' => ['ts' => 'efbbbf', 'text' => '[BOM]', 'title' => 'Zero Width No-Break Space (BOM, ZWNBSP)'],
+    ];
 
     /**
      * short tag incrementor, initialized from outside
@@ -119,11 +161,19 @@ class editor_Models_Segment_Whitespace {
      */
     private array $tagShortcutNumberMap = [];
 
+    private array $protectedCharLabels = [];
+
     /**
-     * flag if tagShortcutNumbers should be collected on usage into $this->tagShortcutNumberMap
+     * public flag if tagShortcutNumbers should be collected on usage into $this->tagShortcutNumberMap
      * @var bool
      */
     public bool $collectTagNumbers = false;
+
+    public function __construct()
+    {
+        $labeledCharacters = array_filter(self::PROTECTED_CHARACTERS);
+        $this->protectedCharLabels = array_combine(array_column($labeledCharacters, 'ts'), $labeledCharacters);
+    }
 
     /**
      * protects all whitespace and special characters coming from the import formats
@@ -164,8 +214,9 @@ class editor_Models_Segment_Whitespace {
                 return $this->maskSpecialContent('char', '&#'.$match[1].';', 1);
             }, $textNode);
         }
-        
-        return preg_replace_callback($this->protectedUnicodeList, function ($match) {
+
+        $regexList = array_keys(self::PROTECTED_CHARACTERS);
+        return preg_replace_callback($regexList, function ($match) {
             //always one single character is masked, so length = 1
             return $this->maskSpecialContent('char', $match[0], 1);
         }, $textNode);
@@ -201,6 +252,50 @@ class editor_Models_Segment_Whitespace {
         return preg_replace_callback('#<(space|char|tab) ts="([A-Fa-f0-9]*)"( length="[0-9]+")?/>#', function ($match) {
             return pack('H*', $match[2]);
         }, $content);
+    }
+
+    /**
+     * convert whitespace-tags with their, so to say, placeholder-characters.
+     *
+     * This is done for several reasons:
+     * 1.Prevent 'hello<tab..>world' to be converted to 'helloworld', so no even ordinary space between those words is kept
+     * 2.Make it more visually obvious for developers what's inside by looking at segment's *EditToSort-column contents directly in database
+     * 3.Make it simplier for Editor.plugins.SpellCheck.controller.Editor.mindTags() to calculate words/phrases coords adjustments
+     *
+     * @param string $content
+     * @return string
+     */
+    public function convertForStripping($content) {
+        $pattern = '#<('.join('|', self::WHITESPACE_TAGS).')( ts="([A-Fa-f0-9]*)")?( length="([0-9]+)")? ?/>#';
+        return preg_replace_callback($pattern, function ($match) {
+            $wholeTag = $match[0];
+            $tagName = $match[1];
+            $length = strlen($match[5]) > 0 ? $match[5] : '1'; //if length not given defaults to 1
+            $renderData = $this->getTagRenderData($tagName, $length, $wholeTag);
+            $tagLabel = $renderData['text'];
+            if($tagName == 'char' && strlen($tagLabel) > 1) {
+                //characters may not have a length, but labels with a longer text "[BOM]",
+                // therefore we replace them with one replacement char
+                return self::LABEL_REPLACEMENT_CHARACTER;
+            }
+            return $tagLabel;
+        }, $content);
+    }
+
+    /**
+     * replaces some of the nice labeled special characters back to their original content
+     * @param string $content
+     * @return string
+     */
+    public static function replaceLabelledCharacters(string $content): string {
+        return str_replace([
+            self::LABEL_NBSP,
+            self::LABEL_NEWLINE,
+            self::LABEL_TAB,
+            self::LABEL_SPACE
+            // Note: first item in the second arg is not an ordinary space having code 32,
+            // but is a non-breaking space having code 16
+        ], [" ", "\n", "\t", ' '], $content);
     }
     
     /**
@@ -279,7 +374,7 @@ class editor_Models_Segment_Whitespace {
 
         $xml = ZfExtended_Factory::get('editor_Models_Import_FileParser_XmlParser', [['normalizeTags' => false]]);
 
-        $xml->registerElement(join(', ', self::WHITESPACE_TAG_LIST), null, function ($tagName, $key, $opener) use ($xml){
+        $xml->registerElement(join(', ', self::WHITESPACE_TAGS), null, function ($tagName, $key, $opener) use ($xml){
             //if there is no length attribute, use length = 1
             $length = $xml->getAttribute($opener['attributes'], 'length', 1);
             $xml->replaceChunk($key, $this->handleWhitespaceTags($xml->getChunk($key), $tagName, $length));
@@ -292,7 +387,7 @@ class editor_Models_Segment_Whitespace {
             $xml->replaceChunk($key, $this->handleProtectedTags($type, $id, $content));
         });
 
-        $validTags = self::WHITESPACE_TAG_LIST;
+        $validTags = self::WHITESPACE_TAGS;
         $validTags[] = 'protectedTag';
         $result = $xml->parse($segment, true, $validTags);
         $xmlChunks = $xml->getAllChunks();
@@ -307,8 +402,6 @@ class editor_Models_Segment_Whitespace {
      * @return editor_Models_Import_FileParser_Tag
      */
     private function handleWhitespaceTags(string $wholeTag, string $tagName, string $length): editor_Models_Import_FileParser_Tag {
-        $cls = ' '.$tagName;
-
         //if collecting, we just collect and do not check the map
         if($this->collectTagNumbers) {
             $this->tagShortcutNumberMap[$wholeTag][] = $shortTagNumber = $this->currentShortTagNumber++;
@@ -324,50 +417,8 @@ class editor_Models_Segment_Whitespace {
             }
         }
         $title = '&lt;'.$shortTagNumber.'/&gt;: ';
-
-        //generate the html tag for the editor
-        switch ($tagName) {
-            // ↵    U+21B5      e2 86 b5    &crarr;     &#8629;     DOWNWARDS ARROW WITH CORNER LEFTWARDS
-            //'hardReturn' => ['text' => '&lt;↵ hardReturn/&gt;'], //in title irgendwas mit <hardReturn/>
-            //'softReturn' => ['text' => '&lt;↵ softReturn/&gt;'], //in title irgendwas mit <softReturn/>
-            //'macReturn' => ['text' => '&lt;↵ macReturn/&gt;'],  //in title irgendwas mit <macReturn/>
-            case 'hardReturn':
-            case 'softReturn':
-            case 'macReturn':
-                $cls = ' newline';
-                $text = '↵';
-                $title .= 'Newline';
-                break;
-            case 'space':
-                // ·    U+00B7      c2 b7       &middot;    &#183;      MIDDLE DOT
-                //'space' => ['text' => '&lt;·/&gt;'],
-                $text = str_repeat('·',$length);
-                $title .= $length.' whitespace character'.($length>1?'s':'');
-                break;
-            case 'tab':
-                // →    U+2192      e2 86 92    &rarr;      &#8594;     RIGHTWARDS ARROW
-                //'tab' => ['text' => '&lt;→/&gt;'],
-                $text = str_repeat('→',$length);
-                $title .= $length.' tab character'.($length>1?'s':'');
-                break;
-            case 'char':
-            default:
-                //'char' => ['text' => 'protected Special character'],
-                if($wholeTag == '<char ts="c2a0" length="1"/>'){
-                    //new type non breaking space: U+00A0
-                    //symbolyzed in word as:
-                    //U+00B0	°	c2 b0	&deg;	° 	&#176;	° 	DEGREE SIGN
-                    //in unix tools:
-                    //U+23B5	⎵	e2 8e b5		&#9141;	⎵ 	BOTTOM SQUARE BRACKET
-                    $text = '⎵';
-                    $cls = ' nbsp';
-                    $title .= 'Non breaking space';
-                }
-                else {
-                    $text = 'protected Special-Character';
-                    $title .= 'protected Special-Character';
-                }
-        }
+        $renderData = $this->getTagRenderData($tagName, $length, $wholeTag);
+        $title .= $renderData['title'];
 
         $tagObj = new editor_Models_Import_FileParser_WhitespaceTag();
         $tagObj->originalContent = $wholeTag;
@@ -375,9 +426,9 @@ class editor_Models_Segment_Whitespace {
         $tagObj->tagNr = $shortTagNumber;
         $tagObj->id = $tagName;
         $tagObj->tag = $tagName;
-        $tagObj->text = $text;
+        $tagObj->text = $renderData['text'];
         //title: Only translatable with using ExtJS QTips in the frontend, as title attribute not possible
-        $tagObj->renderTag($length, $title, $cls);
+        $tagObj->renderTag($length, $title, $renderData['cls']);
         return $tagObj;
     }
 
@@ -387,5 +438,58 @@ class editor_Models_Segment_Whitespace {
     public function resetTagNumberMap()
     {
         $this->tagShortcutNumberMap = [];
+    }
+
+    /**
+     * generate the text, title and cls for the internal whitespace tag
+     *
+     * @param string $tagName
+     * @param string $length
+     * @param string $wholeTag
+     * @return array
+     */
+    private function getTagRenderData(string $tagName, string $length, string $wholeTag): array
+    {
+        $cls = ' '.$tagName;
+        switch ($tagName) {
+            // ↵    U+21B5      e2 86 b5    &crarr;     &#8629;     DOWNWARDS ARROW WITH CORNER LEFTWARDS
+            //'hardReturn' => ['text' => '&lt;↵ hardReturn/&gt;'], //in title irgendwas mit <hardReturn/>
+            //'softReturn' => ['text' => '&lt;↵ softReturn/&gt;'], //in title irgendwas mit <softReturn/>
+            //'macReturn' => ['text' => '&lt;↵ macReturn/&gt;'],  //in title irgendwas mit <macReturn/>
+            case 'hardReturn':
+            case 'softReturn':
+            case 'macReturn':
+                $cls = ' newline';
+                $text = self::LABEL_NEWLINE;
+                $title = 'Newline';
+                break;
+            case 'space':
+                $text = str_repeat(self::LABEL_SPACE, $length);
+                $title = $length . ' whitespace character' . ($length > 1 ? 's' : '');
+                break;
+            case 'tab':
+
+                $text = str_repeat(self::LABEL_TAB, $length);
+                $title = $length . ' tab character' . ($length > 1 ? 's' : '');
+                break;
+            case 'char':
+            default:
+                //'char' => ['text' => 'protected Special character'],
+                $ts = preg_replace('/<[^>]+ts="([^"]+)"[^>]*>/', '$1', $wholeTag);
+
+                if (isset($this->protectedCharLabels[$ts])) {
+                    $text = $this->protectedCharLabels[$ts]['text'];
+                    $title = $this->protectedCharLabels[$ts]['title'];
+                } else {
+                    $text = self::LABEL_REPLACEMENT_CHARACTER;
+                    $title = 'protected Special-Character';
+                }
+
+                //special handling for non breaking spaces: U+00A0
+                if ($ts == 'c2a0') {
+                    $cls = ' nbsp';
+                }
+        }
+        return ['cls' => $cls, 'text' => $text, 'title' => $title];
     }
 }
