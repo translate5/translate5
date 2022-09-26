12c637d58f5fc5d4e6c9992ac83c40ac02a29502
Axel <axel@mittagqi.com>
2022-09-27 00:25:48 +0200

    Fix minor quirk

--- public/modules/editor/js/app/view/quality/FalsePositives.js
+++ public/modules/editor/js/app/view/quality/FalsePositives.js
@@ -58,7 +58,7 @@ Ext.define('Editor.view.quality.FalsePositives', {
      */
     startEditing: function(records, segmentId, isActive){
         this.isActive = isActive;
-        if(isActive && records && records.length > 0){
+        if(isActive && records && records.length){
             this.createCheckboxes(records);
         }
     },
