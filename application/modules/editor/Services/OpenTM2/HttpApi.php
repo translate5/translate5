c59ffd8300c7c46fded875018f575df8325fd1da
Thomas Lauria <thomas@mittagqi.com>
2022-09-27 17:37:37 +0200

    TRANSLATE-2988: TM prefix changes - remove TM prefix from specificData and add it on the fly - for security reasons - TM prefix is changable only on config level 1 - on CLI

--- application/modules/editor/Services/OpenTM2/HttpApi.php
+++ application/modules/editor/Services/OpenTM2/HttpApi.php
@@ -136,6 +136,10 @@ class editor_Services_OpenTM2_HttpApi extends editor_Services_Connector_HttpApiA
             // so we ensure that there is a path, although this would lead to an 404
             $fileName = 'i/do/not/exist';
         }
+        $prefix = Zend_Registry::get('config')->runtimeOptions->LanguageResources->opentm2->tmprefix;
+        if(!empty($prefix)) {
+            $fileName = $prefix.'-'.$fileName;
+        }
         $url = urlencode($fileName).'/'.ltrim($urlSuffix, '/');
         return $this->getHttp($method, $url);
     }
