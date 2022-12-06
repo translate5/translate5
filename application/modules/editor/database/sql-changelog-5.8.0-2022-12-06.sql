dc72d3274bcf9c7a33c9d374d209c51badcacf84
Thomas Lauria <thomas@mittagqi.com>
2022-12-06 11:22:36 +0100

    change log and submodules release translate5 - 5.8.0

---
+++ application/modules/editor/database/sql-changelog-5.8.0-2022-12-06.sql
@@ -0,0 +1,38 @@
+
+-- /*
+-- START LICENSE AND COPYRIGHT
+--
+--  This file is part of translate5
+--
+--  Copyright (c) 2013 - 2017 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+--
+--  Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
+--
+--  This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
+--  as published by the Free Software Foundation and appearing in the file agpl3-license.txt
+--  included in the packaging of this file.  Please review the following information
+--  to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3 requirements will be met:
+--  http://www.gnu.org/licenses/agpl.html
+--
+--  There is a plugin exception available for use with this release of translate5 for
+--  translate5: Please see http://www.translate5.net/plugin-exception.txt or
+--  plugin-exception.txt in the root folder of translate5.
+--
+--  @copyright  Marc Mittag, MittagQI - Quality Informatics
+--  @author     MittagQI - Quality Informatics
+--  @license    GNU AFFERO GENERAL PUBLIC LICENSE version 3 with plugin-execption
+-- 			 http://www.gnu.org/licenses/agpl.html http://www.translate5.net/plugin-exception.txt
+--
+-- END LICENSE AND COPYRIGHT
+-- */
+
+-- userGroup calculation: basic: 1; editor: 2; pm: 4; admin: 8
+            
+INSERT INTO `LEK_change_log` (`dateOfChange`, `jiraNumber`, `type`, `title`, `description`, `userGroup`) VALUES ('2022-12-06', 'TRANSLATE-3055', 'feature', 'VisualReview / VisualTranslation - Connect visual reflow via HTTP to headless browser instance', '* Changed usage of headless Browser for the visual to use a docker-image
+* Added own Worker for the Text-Reflow Conversion
+* Legacy Cleanup: Changed fallback-implementation for visuals, where the text-reflow fails to use the translate5 standard scroller & icon library. This fixes issues with missing annotation-icons in those cases', '15'),
+('2022-12-06', 'TRANSLATE-2185', 'feature', 'Installation & Update - Prepare translate5 for usage with docker', 'Introducing the setup of translate5 and the used services as docker containers.', '15'),
+('2022-12-06', 'TRANSLATE-3127', 'change', 'Editor general - Change the order of form components in "Manual QA inside segment" fieldset', 'MQM widget form fields ordering changed', '15'),
+
+('2022-12-06', 'TRANSLATE-3133', 'bugfix', 'TermPortal - TermPortal: sort filterwindow dropdowns alphabetically', 'Filter window Clients and TermCollection dropdowns are now sorted alphabetically', '15'),
+('2022-12-06', 'TRANSLATE-3123', 'bugfix', 'Import/Export - Tbx import: duplicated attributes should be deleted', 'TBX import: removed term-level attributes duplicates', '15');
