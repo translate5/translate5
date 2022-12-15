c1fe1a8de127ae707dcf1576a332f4d9d8bfe357
Pavel Perminov <pavel.perminov.23@gmail.com>
2022-12-15 18:52:37 +0300

    https://jira.translate5.net/browse/TRANSLATE-3123 Tbx import: handling duplicated attributes
    database/377-TRANSLATE-3123-fix-same-dataTypeId-diff-type.php
     - solved 6th (of 6) type of problems found on my local instance and reported by
       DataTypeConsistencyCheck->checkAttributesAgainstDataTypes()
     - simplified
       - added findOrCreateDataType($dataType, $problem, &$byType)
       - added updateAttributes($dataType, $problem, $whereColumn = 'dataTypeId')

--- application/modules/editor/database/377-TRANSLATE-3123-fix-same-dataTypeId-diff-type.php
+++ application/modules/editor/database/377-TRANSLATE-3123-fix-same-dataTypeId-diff-type.php
@@ -33,7 +33,7 @@ END LICENSE AND COPYRIGHT
 set_time_limit(0);
 
 //uncomment the following line, so that the file is not marked as processed:
-$this->doNotSavePhpForDebugging = false;
+// $this->doNotSavePhpForDebugging = false;
 
 //should be not __FILE__ in the case of wanted restarts / renaming etc...
 // and must not be a constant since in installation the same named constant would we defined multiple times then
@@ -49,7 +49,7 @@ if(empty($this) || empty($argv) || $argc < 5 || $argc > 7) {
     die("please dont call the script direct! Call it by using DBUpdater!\n\n");
 }
 
-/* @var $dataType editor_Models_Terminology_Models_AttributeModel */
+/* @var $dataType editor_Models_Terminology_Models_AttributeDataType */
 $dataType = ZfExtended_Factory::get(editor_Models_Terminology_Models_AttributeDataType::class);
 
 /* @var $db Zend_Db_Adapter_Pdo_Mysql */
@@ -60,161 +60,100 @@ $byType = $db->query("
     SELECT `type`, `id` FROM `terms_attributes_datatype`
 ")->fetchAll(PDO::FETCH_KEY_PAIR);
 
+/**
+ * Find datatype-record by attribute-record's type (e.g $problem['attributeType']),
+ * or create new datatype-record based on attribute-record's type and elementName (e.g. $problem['attributeTag'])
+ *
+ * @param editor_Models_Terminology_Models_AttributeDataType $dataType
+ * @param array $problem
+ * @param array $byType
+ * @throws Zend_Db_Statement_Exception
+ * @throws ZfExtended_Models_Entity_Exceptions_IntegrityConstraint
+ * @throws ZfExtended_Models_Entity_Exceptions_IntegrityDuplicateKey
+ * @throws ZfExtended_Models_Entity_NotFoundException
+ */
+function findOrCreateDataType(editor_Models_Terminology_Models_AttributeDataType $dataType, $problem, &$byType) {
+
+    // If there is an existing datatype-record having type same as attribute-record type
+    if ($dataTypeId = $byType[$problem['attributeType']] ?? 0) {
+
+        // Load datatype-record by id
+        $dataType->load($dataTypeId);
+
+    // Else
+    } else {
+
+        // Init new datatype-record
+        $dataType->init([
+            'label' => $problem['attributeTag'],
+            'type'  => $problem['attributeType']
+        ]);
+
+        // Save it
+        $dataType->save();
+
+        // Add to dict to be further available
+        $byType[$dataType->getType()] = $dataType->getId();
+    }
+}
+
+/**
+ * Update attributes at the step where we surely know the correct values for `dataTypeId`, `type` and `elementName`
+ *
+ * @param editor_Models_Terminology_Models_AttributeDataType $dataType
+ * @param $problem
+ * @param string $whereColumn
+ */
+function updateAttributes(editor_Models_Terminology_Models_AttributeDataType $dataType, $problem, $whereColumn = 'dataTypeId') {
+
+    // Set attribute-record's dataTypeId to be as per datatype-record found attribute-record's type
+    $dataType->db->getAdapter()->query("
+        UPDATE `terms_attributes` 
+        SET 
+            `dataTypeId` = '{$dataType->getId()}', 
+            `elementName` = '{$dataType->getLabel()}',
+            `type` = '{$dataType->getType()}'
+        WHERE 1
+          AND `$whereColumn` = '{$problem['datatypeId']}'
+          AND `collectionId` = '{$problem['collectionId']}'
+          AND `elementName`  = '{$problem['attributeTag']}'
+          AND `type`         = '{$problem['attributeType']}'
+    ");
+}
+
 // Get checker
 $checker = new \editor_Models_Terminology_DataTypeConsistencyCheck();
 
 // Foreach 
 foreach ($checker->checkAttributesAgainstDataTypes() as $problem) {
 
-    // If it's a note-datatype (which have label=note but type='')
+    // If it's a note-datatype (e.g. have label='note' and type='')
     if (!$problem['datatypeType']) {
 
-        // If attribute-record's type is 'note'
-        if ($problem['attributeType'] == 'note') {
+        // If attribute-record's type is 'note', it means we just need to load note-datatype-record
+        // into $dataType model instance for further processing, but for doing that we just need to
+        // make sure datatype-record's id to be picked from $byType[''] in findOrCreateDataType() call
+        $_problem = $problem['attributeType'] == 'note'
+            ? ['attributeType' => '']
+            : $problem;
 
-            // Update attribute-records' elementName and type to be as per dataTypeId
-            $db->query("
-                UPDATE `terms_attributes` 
-                SET `elementName` = '{$problem['datatypeTag']}', `type` = '{$problem['datatypeType']}' 
-                WHERE `dataTypeId` = '{$problem['datatypeId']}' AND `collectionId` = '{$problem['collectionId']}'
-            ");
-        }
+        // Load correct datatype-record into $dataType model instance (such record is created, if need)
+        findOrCreateDataType($dataType, $_problem, $byType);
 
-    // Else if attribute-record's dataTypeId is NULL
-    } else if ($problem['datatypeType'] == 'NOT EXISTENT') {
-
-        // It means $problem['datatypeId'] - is an id of attribute-record, affected by the problem
-        $attributeId = $problem['datatypeId'];
-
-        // If there is an existing datatype-record having type same as attribute-record type
-        if ($dataTypeId = $byType[$problem['attributeType']] ?? 0) {
-
-            // Load datatype-record by id
-            $dataType->load($dataTypeId);
-
-            // Set attribute-record's dataTypeId to be as per datatype-record found attribute-record's type
-            $db->query("
-                UPDATE `terms_attributes` 
-                SET `dataTypeId` = '$dataTypeId', `elementName` = '{$dataType->getLabel()}'  
-                WHERE `id` = '$attributeId'
-            ");
-
-        // Else if there is no existing datatype-record found by attribute-record's type
-        } else {
-
-            // Create new datatype-record
-        }
-
-    // Else if attribute-record's type is as per dataTypeId
-    } else if ($problem['datatypeType'] == $problem['attributeType']) {
-
-        // Update attribute-records' elementName to be as per dataTypeId as well
-        $db->query("
-            UPDATE `terms_attributes` 
-            SET `elementName` = '{$problem['datatypeTag']}' 
-            WHERE `dataTypeId` = '{$problem['datatypeId']}' AND `collectionId` = '{$problem['collectionId']}'
-        ");
+        // Update attributes
+        updateAttributes($dataType, $problem);
 
     // Else
     } else {
 
-        // If there is an existing datatype-record having type same as attribute-record type
-        if ($dataTypeId = $byType[$problem['attributeType']] ?? 0) {
+        // If attribute-record's dataTypeId is NULL then value of $problem['datatypeId']
+        // is an id of attribute-record rather than id of datatype-record
+        $whereColumn = $problem['datatypeType'] == 'NOT EXISTENT' ? 'id' : 'dataTypeId';
 
-            // Load datatype-record by id
-            $dataType->load($dataTypeId);
+        // Load correct datatype-record into $dataType model instance (such record is created, if need)
+        findOrCreateDataType($dataType, $problem, $byType);
 
-            // Set attribute-record's dataTypeId to be as per datatype-record found attribute-record's type
-            $db->query("
-                UPDATE `terms_attributes`
-                SET `dataTypeId` = '$dataTypeId', `elementName` = '{$dataType->getLabel()}'  
-                WHERE 1
-                  AND `dataTypeId`   = '{$problem['datatypeId']}'
-                  AND `collectionId` = '{$problem['collectionId']}'
-                  AND `elementName`  = '{$problem['attributeTag']}'
-                  AND `type`         = '{$problem['attributeType']}'
-            ");
-
-        // Else
-        } else {
-
-            // Init new datatype-record
-            $dataType->init([
-                'label' => $problem['attributeTag'],
-                'type'  => $problem['attributeType'],
-            ]);
-
-            // Save it
-            $dataType->save();
-
-            // Add to dict to be further available
-            $dataTypeId = $byType[$dataType->getType()] = $dataType->getId();
-
-            // Update attribute-records' elementName to be as per dataTypeId as well
-            $db->query("
-                UPDATE `terms_attributes` 
-                SET `dataTypeId` = '$dataTypeId' 
-                WHERE 1
-                  AND `dataTypeId`   = '{$problem['datatypeId']}'
-                  AND `collectionId` = '{$problem['collectionId']}'
-                  AND `elementName`  = '{$problem['attributeTag']}'
-                  AND `type`         = '{$problem['attributeType']}'
-            ");
-        }
+        // Update attributes
+        updateAttributes($dataType, $problem, $whereColumn);
     }
 }
-
-// Foreach dataTypeId having more than 1 type usages
-/*foreach ($diff as $dataTypeId => $types) {
-
-    // Load datatype-record by id
-    $dataType->load($dataTypeId);
-
-    // Prepare data for new datatype-record
-    $dataTypeCtor = $dataType->toArray(); unset($dataTypeCtor['id'], $dataTypeCtor['type']);
-
-    // Get value of type-prop defined for that record
-    $correctType = $dataType->getType();
-
-    // Foreach type
-    foreach (explode(',', $types) as $type) {
-
-        // If $type is not as per dataTypeId
-        if ($type != $correctType) {
-
-            // If such $type is NOT already exist within some another datatype-record
-            if (!isset($byType[$type])) {
-
-                // Create new datatype-record based on current one but with different $type
-                $dataType_new = clone $dataType;
-                $dataType_new->init($dataTypeCtor);
-                $dataType_new->setType($type);
-                $dataType_new->save();
-
-                // Append that to $byType
-                $byType[$type] = $dataType_new->getId();
-            }
-
-            // Update usages
-            $db->query("
-                UPDATE `terms_attributes` 
-                SET `dataTypeId` = '{$byType[$type]}' 
-                WHERE `dataTypeId` = '{$dataTypeId}' AND `type` = ?
-            ", $type);
-        }
-    }
-
-    // Make sure attribute-record's elementName to be same as per dataTypeId
-    $db->query("
-        UPDATE `terms_attributes` `ta`, `terms_attributes_datatype` `tad`
-        SET `ta`.`elementName` = `tad`.`label`
-        WHERE `tad`.`id` = `ta`.`dataTypeId`
-    ");
-
-    // Fix cases when attribute-record's dataTypeId is NULL, but type is
-    $db->query("
-        UPDATE `terms_attributes` `ta`, `terms_attributes_datatype` `tad`
-        SET `ta`.`dataTypeId` = `tad`.`id`
-        WHERE ISNULL(`ta`.`dataTypeId`) AND `tad`.`type` = `ta`.`type`    
-    ");
-}*/
