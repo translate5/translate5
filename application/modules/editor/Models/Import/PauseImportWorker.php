261e7c7f32340eee051fd3b102e01ece9617be4d
leonkj <leon@mittagqi.com>
2022-12-27 16:00:59 +0200

    TRANSLATE-3140 Add pause worker implementation

---
+++ application/modules/editor/Models/Import/PauseImportWorker.php
@@ -0,0 +1,66 @@
+<?php
+/*
+START LICENSE AND COPYRIGHT
+
+ This file is part of translate5
+
+ Copyright (c) 2013 - 2021 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+
+ Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
+
+ This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
+ as published by the Free Software Foundation and appearing in the file agpl3-license.txt
+ included in the packaging of this file.  Please review the following information
+ to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3 requirements will be met:
+ http://www.gnu.org/licenses/agpl.html
+
+ There is a plugin exception available for use with this release of translate5 for
+ translate5: Please see http://www.translate5.net/plugin-exception.txt or
+ plugin-exception.txt in the root folder of translate5.
+
+ @copyright  Marc Mittag, MittagQI - Quality Informatics
+ @author     MittagQI - Quality Informatics
+ @license    GNU AFFERO GENERAL PUBLIC LICENSE version 3 with plugin-execption
+			 http://www.gnu.org/licenses/agpl.html http://www.translate5.net/plugin-exception.txt
+
+END LICENSE AND COPYRIGHT
+*/
+
+declare(strict_types=1);
+
+namespace MittagQI\Translate5\Models\Import;
+
+class PauseImportWorker extends \editor_Models_Task_AbstractWorker
+{
+    public const PROCESSOR = 'processor';
+    
+    protected function validateParameters($parameters = []): bool
+    {
+        return isset($parameters[self::PROCESSOR])
+            && class_exists($parameters[self::PROCESSOR])
+            && in_array(PauseWorkerProcessorInterface::class, class_implements($parameters[self::PROCESSOR]), true);
+    }
+
+    protected function work(): bool
+    {
+        $params = $this->workerModel->getParameters();
+
+        /** @var PauseWorkerProcessorInterface $processor */
+        $processor = \ZfExtended_Factory::get($params[self::PROCESSOR]);
+
+        $sleepTime = $processor->getSleepTime();
+        $time = 0;
+        $maxTime = $processor->getMaxWaitTime();
+
+        while ($time < $maxTime) {
+            if (!$processor->shouldWait($this->task)) {
+                break;
+            }
+
+            $time+= $sleepTime;
+            sleep($sleepTime);
+        }
+
+        return true;
+    }
+}
--- application/modules/editor/Plugins/MatchAnalysis/Init.php
+++ application/modules/editor/Plugins/MatchAnalysis/Init.php
@@ -27,6 +27,9 @@
  */
 
 use MittagQI\Translate5\LanguageResource\Pretranslation\BatchCleanupWorker;
+use MittagQI\Translate5\Models\Import\PauseImportWorker;
+use MittagQI\Translate5\Plugins\MatchAnalysis\PauseMatchAnalysisProcessor;
+use MittagQI\Translate5\Plugins\MatchAnalysis\PauseMatchAnalysisWorker;
 
 class editor_Plugins_MatchAnalysis_Init extends ZfExtended_Plugin_Abstract {
     protected static string $description = 'Provides the match-analysis and pre-translation against language-resources.';
@@ -312,6 +315,11 @@ class editor_Plugins_MatchAnalysis_Init extends ZfExtended_Plugin_Abstract {
         if(!empty($this->batchAssocs) && $workerParameters['batchQuery']){
             $this->queueBatchWorkers($task, $workerParameters, $parentWorkerId);
         }
+
+        $worker = ZfExtended_Factory::get(PauseMatchAnalysisWorker::class);
+        $worker->init($taskGuid, [PauseImportWorker::PROCESSOR => PauseMatchAnalysisProcessor::class]);
+        $worker->queue($parentWorkerId);
+
         // init worker and queue it
         $worker = ZfExtended_Factory::get('editor_Plugins_MatchAnalysis_Worker');
         /* @var $worker editor_Plugins_MatchAnalysis_Worker */
