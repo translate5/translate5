1b2de5fed4ccc3ceef364c3c411d48555bb35138
Axel <axel@mittagqi.com>
2023-02-15 14:07:58 +0100

    - ENHANCEMENT: add t5memory version to service check

--- application/modules/editor/src/Service/T5Memory.php
+++ application/modules/editor/src/Service/T5Memory.php
@@ -28,6 +28,9 @@ END LICENSE AND COPYRIGHT
 
 namespace MittagQI\Translate5\Service;
 
+use Zend_Http_Client;
+use ZfExtended_Factory;
+
 /**
  * The t5memory languageResource Service
  */
@@ -37,7 +40,38 @@ final class T5Memory extends DockerServiceAbstract {
         'name' => 'runtimeOptions.LanguageResources.opentm2.server',
         'type' => 'list',
         'url' => 'http://t5memory.:4040/t5memory',
-        'healthcheck' => '/', // requesting th ebas url will retrieve a 200 status
+        'healthcheck' => '_service/resources', // composes to "http://t5memory.:4040/t5memory_service/resources" requesting this resources url will retrieve a 200 status and the version
         'additive' => true // TODO: is this neccessary ?
     ];
+
+    protected function checkConfiguredHealthCheckUrl(string $url): bool
+    {
+        // special healthcheck url also retrieves the version
+        try {
+            $httpClient = ZfExtended_Factory::get(Zend_Http_Client::class);
+            $httpClient->setUri($url);
+            // $httpClient->setHeaders('Accept', 'application/json');
+            $response = $httpClient->request('GET');
+            // the status request must return 200
+            if($response->getStatus() === 200) {
+                // for now there is a BUG in the t5memory response for /resources and we have to parse dirtily
+                $matches = [];
+                if(preg_match('~"Version"\s*:\s*"([^"]+)"~', $response->getBody(), $matches) === 1){
+                    $this->version = (count($matches) > 0) ? $matches[1] : null;
+                }
+                /* implementation when BUG is fixed
+                $props = json_decode($response->getBody());
+                if$props && property_exists($props, 'Version')){
+                    $this->version = $props->Version;
+                }
+                */
+                return true;
+            }
+            return false;
+
+        } catch (Throwable $e){
+
+            return false;
+        }
+    }
 }
