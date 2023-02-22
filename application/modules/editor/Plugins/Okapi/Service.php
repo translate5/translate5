0f47519319811831f8af1714131663843315dfc9
Axel <axel@mittagqi.com>
2023-02-22 14:30:47 +0100

    added OKAPI version to service check & checked other configured servers

--- application/modules/editor/Plugins/Okapi/Service.php
+++ application/modules/editor/Plugins/Okapi/Service.php
@@ -28,54 +28,135 @@ END LICENSE AND COPYRIGHT
 
 namespace MittagQI\Translate5\Plugins\Okapi;
 
+use editor_Utils;
 use MittagQI\Translate5\Service\DockerServiceAbstract;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Zend_Config;
+use Zend_Http_Client;
+use Zend_Http_Client_Exception;
 use ZfExtended_Factory;
-use editor_Plugins_Okapi_Connector;
 
-final class Service extends DockerServiceAbstract {
+final class Service extends DockerServiceAbstract
+{
 
     protected array $configurationConfig = [
         'name' => 'runtimeOptions.plugins.Okapi.server',
         'type' => 'string',
-        'url' => 'http://okapi.:8080',
+        'url' => 'http://okapi.:8080/okapi-longhorn/',
         'healthcheck' => '/projects'
     ];
 
+    /**
+     * Differing from the base-implementation we add checking the other configured okapi-services here, not just the used one
+     * (non-PHPdoc)
+     * @see DockerServiceAbstract::check()
+     */
     public function check(): bool
     {
         $services = $this->config->runtimeOptions->plugins->Okapi->server;
         $serviceUsed = $this->config->runtimeOptions->plugins->Okapi->serverUsed;
         $url = (!empty($services) && !empty($serviceUsed)) ? ($services->$serviceUsed ?? null) : null;
-        if(empty($url)){
-            $this->errors[] = 'There is no URL configured.';
+        if (empty($url)) {
+            $this->errors[] = 'There is no URL configured for entry "'.$serviceUsed.'".';
             return false;
         }
         $healthcheckUrl = rtrim($url, '/') . '/projects';
         if (!$this->checkConfiguredHealthCheckUrl($healthcheckUrl)) {
-            $this->errors[] = 'A request on "' . $healthcheckUrl . '" did not bring the expected status "200".';
+            $this->errors[] = 'A request on "' . $healthcheckUrl . '" did not bring the expected status "200" for entry "'.$serviceUsed.'".';
             return false;
         }
+        $this->version = $this->fetchVersion($url, 'before 1.40.0');
+        $this->checkOtherConfiguredServers($services, $serviceUsed);
         return true;
     }
 
-    public function locate(SymfonyStyle $io, mixed $url, bool $doSave=false, array $config = []): bool
+    /**
+     * (non-PHPdoc)
+     * @see DockerServiceAbstract::locate()
+     */
+    public function locate(SymfonyStyle $io, mixed $url, bool $doSave = false, array $config = []): bool
     {
-        if(array_key_exists('remove', $config) && $config['remove'] === true){
+        if (array_key_exists('remove', $config) && $config['remove'] === true) {
             $this->updateConfigurationConfig('runtimeOptions.plugins.Okapi.server', 'string', '', $doSave, $io);
             $this->updateConfigurationConfig('runtimeOptions.plugins.Okapi.serverUsed', 'string', '', $doSave, $io);
             return false;
         }
-
-        if(empty($url)){
+        if (empty($url)) {
             $url = $this->configurationConfig['url'];
         }
-        if($this->checkPotentialServiceUrl($this->getName(), $url, $io)){
-            // TODO FIXME multiple servers / versions ?
-            $this->updateConfigurationConfig('runtimeOptions.plugins.Okapi.server', 'string', '{"okapi-longhorn":"' . $url . '"}', $doSave, $io);
-            $this->updateConfigurationConfig('runtimeOptions.plugins.Okapi.serverUsed', 'string', 'okapi-longhorn', $doSave, $io);
+        if ($this->checkPotentialServiceUrl($this->getName(), $url, $io)) {
+            // validate existing entries, dismiss those not available
+            $newServers = [];
+            if(!empty($this->config->runtimeOptions->plugins->Okapi->server)){
+                foreach($this->config->runtimeOptions->plugins->Okapi->server as $name => $otherUrl){
+                    if(!empty($otherUrl) && $otherUrl != $url && $this->checkConfiguredHealthCheckUrl(rtrim($otherUrl, '/') . '/projects')){
+                        $newServers[$name] = $otherUrl;
+                    }
+                }
+            }
+            // add new entry by it's version as name-suffix (note: we will overwrite other entries like 'okapi-longhorn-xxx' without further notice) ... we use a scheme that is common on the existing instances
+            $version = $this->fetchVersion($url, '');
+            $version = str_ends_with($version, '.0') ? substr($version, 0, -2) : $version;
+            $newName = (empty($version)) ? 'okapi-longhorn' : 'okapi-longhorn-' . editor_Utils::secureFilename(str_replace('.', '', $version));
+            $newServers[$newName] = $url;
+            $this->updateConfigurationConfig('runtimeOptions.plugins.Okapi.server', 'string', json_encode($newServers, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), $doSave, $io);
+            $this->updateConfigurationConfig('runtimeOptions.plugins.Okapi.serverUsed', 'string', $newName, $doSave, $io);
             return true;
         }
         return false;
+    }
+
+    /**
+     * Overwritten to properly show warnings with other configured endpoints
+     * @param string $seperator
+     * @return string
+     */
+    public function getWarning(string $seperator = "\n"): string
+    {
+        return 'Service "' . $this->getName() . '", plugin "' . $this->pluginName . '"'
+            . ' has warnings:'
+            . $seperator . $seperator
+            . implode($seperator, $this->warnings);
+    }
+
+    /**
+     * Retrieves the OKAPI version. This API is only available since OKAPI 1.40.0
+     * @param string $okapiUrl
+     * @param string|null $default
+     * @return string|null
+     * @throws Zend_Http_Client_Exception
+     */
+    private function fetchVersion(string $okapiUrl, string $default = null): ?string
+    {
+        $httpClient = ZfExtended_Factory::get(Zend_Http_Client::class);
+        $httpClient->setUri(rtrim($okapiUrl, '/') . '/status.json');
+        $response = $httpClient->request('GET');
+        if ($response->getStatus() === 200) {
+            $status = json_decode($response->getBody());
+            if (property_exists($status, 'version')) {
+                return $status->version;
+            }
+        }
+        return $default;
+    }
+
+    /**
+     * @param Zend_Config $services
+     * @param string $serviceUsed
+     */
+    private function checkOtherConfiguredServers(Zend_Config $services, string $serviceUsed)
+    {
+        foreach($services as $name => $url){
+            if($name !== $serviceUsed){
+                if (empty($url)) {
+                    $this->warnings[] = 'There is no URL configured for entry "'.$name.'.';
+                } else {
+                    $healthcheckUrl = rtrim($url, '/') . '/projects';
+                    if (!$this->checkConfiguredHealthCheckUrl($healthcheckUrl)) {
+                        $this->warnings[] = 'A request on "' . $healthcheckUrl . '" did not bring the expected status "200" for entry "'.$name.'.';
+                    }
+                }
+            }
+        }
     }
 }
