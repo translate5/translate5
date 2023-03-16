27f9a8d15ec7f8f9da841361b1186d89556e95a2
Thomas Lauria <thomas@mittagqi.com>
2023-03-16 17:29:07 +0100

    change log and submodules release translate5 - 5.9.2

---
+++ application/modules/editor/database/sql-changelog-5.9.2-2023-03-16.sql
@@ -0,0 +1,33 @@
+
+-- /*
+-- START LICENSE AND COPYRIGHT
+--
+--  This file is part of translate5
+--
+--  Copyright (c) 2013 - 2017 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+--
+--  Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
+--
+--  This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
+--  as published by the Free Software Foundation and appearing in the file agpl3-license.txt
+--  included in the packaging of this file.  Please review the following information
+--  to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3 requirements will be met:
+--  http://www.gnu.org/licenses/agpl.html
+--
+--  There is a plugin exception available for use with this release of translate5 for
+--  translate5: Please see http://www.translate5.net/plugin-exception.txt or
+--  plugin-exception.txt in the root folder of translate5.
+--
+--  @copyright  Marc Mittag, MittagQI - Quality Informatics
+--  @author     MittagQI - Quality Informatics
+--  @license    GNU AFFERO GENERAL PUBLIC LICENSE version 3 with plugin-execption
+-- 			 http://www.gnu.org/licenses/agpl.html http://www.translate5.net/plugin-exception.txt
+--
+-- END LICENSE AND COPYRIGHT
+-- */
+
+-- userGroup calculation: basic: 1; editor: 2; pm: 4; admin: 8
+            
+INSERT INTO `LEK_change_log` (`dateOfChange`, `jiraNumber`, `type`, `title`, `description`, `userGroup`) VALUES ('2023-03-16', 'TRANSLATE-3248', 'bugfix', 'Authentication - Authentication for users with empty db password', 'Fix a problem when error is produced on authentication for users with empty database password.', '15'),
+('2023-03-16', 'TRANSLATE-3247', 'bugfix', 'TermPortal - Duplicated attributes cleanup problem', 'A script to cleanup duplicated term attributes did not run successfully in some circumstances and was blocking the whole DB update procedure.', '15'),
+('2023-03-16', 'TRANSLATE-3238', 'bugfix', 'Task Management - Not supported file format for translator package: Disable download button', 'Translator package export/import buttons will be disabled in case the task does not allow or support export this.', '15');
