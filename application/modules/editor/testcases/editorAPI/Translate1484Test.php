903950c71b1a609a1c931dc85d65dbb1a3810a45
aleksandar <aleksandar@mittagqi.com>
2023-03-24 15:45:19 +0100

    TRANSLATE-3061 FIX API Tests - Microsoft test - Usage log test: Translate1484Test

--- application/modules/editor/testcases/editorAPI/Translate1484Test.php
+++ application/modules/editor/testcases/editorAPI/Translate1484Test.php
@@ -76,7 +76,7 @@ class Translate1484Test extends editor_Test_JsonTest {
         // we need to order the results to avoid tests failing due to runtime-differences
         $this->sortExportResource($actualObject);
         $this->sortExportResource($expectedObject);
-        $this->assertEquals($expectedObject, $actualObject, "The expected file (exportResults) an the result file does not match.");
+        $this->assertEquals($expectedObject, $actualObject, 'The expected file (exportResults) an the result file does not match.');
     }
 
     /**
