e11297d3c3562b4628524328dae93d719e13133c
Oleksandr Mikhliaiev <oleksandr@mittagqi.com>
2023-06-06 17:20:02 +0300

    TRANSLATE-3321 Add auto-detection of source language to InstantTranslate

--- public/modules/editor/instanttranslate/js/Instanttranslate.js
+++ public/modules/editor/instanttranslate/js/Instanttranslate.js
@@ -90,15 +90,33 @@ function initGui(characterLimit, pretranslatedFiles, dateAsOf, disableInstantTra
         }
     });
 
-    $('#sourceLocale').selectmenu({
+    $('#sourceLocaleText').selectmenu({
         appendTo: "#source",
         open: function() {
             updateLocalesSelectLists($(this));
         },
         change: function() {
             changeLanguage();
+            $("#sourceLocaleFile").val($("#sourceLocaleText").val());
         }
-    }).selectmenu("menuWidget").addClass("overflow localesSelectList");
+    })
+        .selectmenu("menuWidget")
+        .addClass("overflow localesSelectList");
+
+    $('#sourceLocaleFile').selectmenu({
+        appendTo: "#source",
+        open: function() {
+            updateLocalesSelectLists($(this));
+        },
+        change: function() {
+            changeLanguage();
+            $("#sourceLocaleText").val($("#sourceLocaleFile").val());
+        }
+    })
+        .selectmenu("menuWidget")
+        .addClass("overflow localesSelectList");
+    $('#sourceLocaleFile').selectmenu('widget').hide();
+
     $('#targetLocale').selectmenu({
         appendTo: "#target",
         open: function() {
@@ -134,13 +152,16 @@ function initGui(characterLimit, pretranslatedFiles, dateAsOf, disableInstantTra
     });
 
     //check if the source and the target are the same
-    if($('#sourceLocale').val() === $('#targetLocale').val()){
-        setTargetFirstAvailable($('#sourceLocale').val());
+    if($('#sourceLocaleText').val() === $('#targetLocale').val()){
+        setTargetFirstAvailable($('#sourceLocaleText').val());
     }
     $("#sourceText").attr('maxlength', characterLimit);
 
     // press the button to swap source-/target-language
     $('.switchSourceTarget').on("click", function() {
+        if ($(this).hasClass('switchSourceTargetDisabled')) {
+            return;
+        }
         swapLanguages();
     });
 
@@ -177,7 +198,7 @@ function initGui(characterLimit, pretranslatedFiles, dateAsOf, disableInstantTra
 
     $(document).on('click', '.term-proposal' , function() {
         var text = getInputTextValueTrim(),
-            lang = $("#sourceLocale").val(),
+            lang = $('#sourceLocaleFile').selectmenu('widget').is(":hidden") ? $("#sourceLocaleText").val() : $("#sourceLocaleFile").val(),
             textProposal = $(this).attr('data-term'),
             langProposal = $("#targetLocale").val(),
             isMT = $(this).parents('.copyable').find('.translation-result').data('languageresource-type') === 'mt';
@@ -438,7 +459,7 @@ function setfileUploadLanguageCombinationsAvailable() {
  * @returns boolean
  */
 function isFileUploadAvailable() {
-    var langComb = $('#sourceLocale').val() + '|' + $('#targetLocale').val();
+    var langComb = $('#sourceLocaleFile').val() + '|' + $('#targetLocale').val();
     return $.inArray(langComb, fileUploadLanguageCombinationsAvailable) !== -1 && Editor.data.apps.instanttranslate.fileTranslation;
 }
 /***
@@ -498,16 +519,18 @@ function updateLocalesSelectLists(el) {
  * @returns {Boolean}
  */
 function hasEnginesForLanguageCombination() {
-	var returnValue = isSourceTargetAvailable($("#sourceLocale").val(), $("#targetLocale").val()),
-		isDisableButton = !isSourceTargetAvailable($("#targetLocale").val(), $("#sourceLocale").val()); //switch source and target so the other way arround is checked
+    const
+        sourceLocale = $('#sourceLocaleFile').selectmenu('widget').is(":hidden") ? $("#sourceLocaleText").val() : $("#sourceLocaleFile").val(),
+        targetLocale = $("#targetLocale").val();
+
 	//the button is disabled when for the target as source there is no source as target
-	$("#switchSourceTarget").prop("disabled", isDisableButton);
-    if(isDisableButton){
-        $("#switchSourceTarget").addClass( "switchSourceTargetDisabled" );
+    if (!isSourceTargetAvailable(targetLocale, sourceLocale)) {
+        $(".switchSourceTarget").addClass( "switchSourceTargetDisabled" );
     } else {
-        $("#switchSourceTarget").removeClass( "switchSourceTargetDisabled" );
+        $(".switchSourceTarget").removeClass( "switchSourceTargetDisabled" );
     }
-    return returnValue;
+
+    return isSourceTargetAvailable(sourceLocale, targetLocale);
 }
 
 /***
@@ -516,21 +539,28 @@ function hasEnginesForLanguageCombination() {
  * @param targetRfc
  * @returns {Boolean}
  */
-function isSourceTargetAvailable(sourceRfc,targetRfc){
-    var targetLocalesAvailable = getLocalesAccordingToReference ('accordingToSourceLocale', sourceRfc),
-        useSub = Editor.data.instanttranslate.showSublanguages,
-        hasLang = false;
-    if(useSub){
+function isSourceTargetAvailable(sourceRfc, targetRfc){
+    if ('auto' === sourceRfc || 'auto' === targetRfc) {
+        return 'auto' === sourceRfc;
+    }
+
+    const targetLocalesAvailable = getLocalesAccordingToReference ('accordingToSourceLocale', sourceRfc),
+        useSub = Editor.data.instanttranslate.showSublanguages;
+    let hasLang = false;
+
+    if (useSub) {
         return targetLocalesAvailable.indexOf(targetRfc) !== -1;
     }
     // when no sublangauges are used, use fuzzy matching on localeToCheck, since in the backend fuzzy latching will be used as well
     $.each(targetLocalesAvailable, function(index) {
-        var locale = targetLocalesAvailable[index];
+        let locale = targetLocalesAvailable[index];
         hasLang = targetRfc === locale.split('-')[0];
-        if(hasLang){
+
+        if (hasLang) {
             return false;
         }
     });
+
     return hasLang;
 }
 
@@ -703,7 +733,7 @@ function translateText(textToTranslate, translationInProgressID){
         dataType: "json",
         type: "POST",
         data: {
-            'source': $("#sourceLocale").val(),
+            'source': $("#sourceLocaleText").val(),
             'target': $("#targetLocale").val(),
             'text': textToTranslate,
             'escape': 1,
@@ -1090,7 +1120,7 @@ function requestFileTranslate(){
         data.append('file', uploadedFiles[0]);
     }
     
-    data.append('source', $('#sourceLocale').val());
+    data.append('source', $('#sourceLocaleFile').val());
     data.append('target', $('#targetLocale').val());
 
     $.ajax({
@@ -1473,17 +1503,18 @@ function changeLanguage(){
 }
 
 function swapLanguages(){
+    const sourceLangIdSelector = $('#sourceLocaleFile').selectmenu('widget').is(":hidden") ? "#sourceLocaleText" : "#sourceLocaleFile";
     // detect old languages
-    var $oldSourceLang = $('#sourceLocale').val(),
+    var $oldSourceLang = $(sourceLangIdSelector).val(),
         $oldTargetLang = $('#targetLocale').val(),
-        allSourceLangs = $.map($("#sourceLocale option[value]"), function(option) {
+        allSourceLangs = $.map($(sourceLangIdSelector + " option[value]"), function(option) {
             return $(option).attr("value");
         }),
         allTargetLangs = $.map($("#targetLocale option[value]"), function(option) {
             return $(option).attr("value");
         });
-        
-    //check if oldLangs are missing in other language, if yes try fuzzying, 
+
+    //check if oldLangs are missing in other language, if yes try fuzzying,
     // if still nothing found keep logic as it was (use the last one of the select)
     
     if($.inArray($oldSourceLang, allTargetLangs) < 0) {
@@ -1523,11 +1554,14 @@ function swapLanguages(){
         return;
     }
 
+
     // now swap the language selections
-    $("#sourceLocale").val($oldTargetLang);
+    $("#sourceLocaleText").val($oldTargetLang);
+    $("#sourceLocaleFile").val($oldTargetLang);
     $("#targetLocale").val($oldSourceLang);
     
-    $("#sourceLocale").selectmenu("refresh");
+    $("#sourceLocaleText").selectmenu("refresh");
+    $("#sourceLocaleFile").selectmenu("refresh");
     $("#targetLocale").selectmenu("refresh");
 
     // renew the results, if there are any
@@ -1573,7 +1607,9 @@ function showGui(){
  */
 function showSourceIsText(){
     $('.show-if-source-is-text').show();
+    $('#sourceLocaleText').selectmenu('widget').show();
     $('.show-if-source-is-file').hide();
+    $('#sourceLocaleFile').selectmenu('widget').hide();
     $('#translations').show();
     $("#sourceIsText").removeClass('source-text-error');
     $('#sourceText').focus();
@@ -1592,7 +1628,9 @@ function showSourceIsText(){
 function showSourceIsFile(){
     hideSourceError();
     $('.show-if-source-is-text').hide();
+    $('#sourceLocaleText').selectmenu('widget').hide();
     $('.show-if-source-is-file').show();
+    $('#sourceLocaleFile').selectmenu('widget').show();
     $('#translations').hide();
     $('body').removeClass('sourceIsText');
     $('body').addClass('sourceIsFile');
