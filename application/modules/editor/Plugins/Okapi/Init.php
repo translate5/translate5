a5f16b6443c3e06f5fdfb455f3b585673be7b6db
Axel <axel@mittagqi.com>
2023-06-06 14:15:25 +0200

    HOTFIX: Ian/Figma forgot an attribute

--- application/modules/editor/Plugins/Okapi/Init.php
+++ application/modules/editor/Plugins/Okapi/Init.php
@@ -48,7 +48,7 @@ class editor_Plugins_Okapi_Init extends ZfExtended_Plugin_Abstract {
      * This must be increased each time, a git-based fprm or srx is changed
      * @var int
      */
-    const BCONF_VERSION_INDEX = 4;
+    const BCONF_VERSION_INDEX = 5;
 
     /**
      * The filename of the system default import bconf
