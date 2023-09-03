ba6b8fd531f4bdac570963e24288f1e37c8ef323
Pavel Perminov <pavel.perminov.23@gmail.com>
2023-09-03 21:57:21 +0300

    https://jira.translate5.net/browse/TRANSLATE-1436 Add or propose terminology directly from translate5 task
    1.AttributeController
     - changed everythere
       - editor_Models_Terminology_Models_AttributeModel to AttributeModel::class
       - editor_Models_Terminology_Models_TermModel to TermModel::class
       - use editor_Models_TermCollection_TermCollection to TermCollection::class
     - _attrupdateCheckProcessStatusChangeIsAllowed()
       - moved certain code to TermModel::allowedProcessStatuses()
    2.TermController
     - changed everythere
       - editor_Models_Terminology_Models_TermModel to TermModel::class
     - postAction():
       - processStatus-param and sourceProcessStatus-param are now respected if given
       - added jflush-msg if checkExisting-param is given
     - _postCheckParams()
       - added jcheck-rules for processStatus and sourceProcessStatus params
         - with TermModel::allowedProcessStatuses() usage
       - special is now invoked if checkExisting-param is given
         - source or target term is attached to target or source if exists
    	 - failure flushed if both exists under same termEntryId
    	 - both saved under new termEntryId if not exist
    3.editor_Models_Terminology_Models_TermModel
     - added searchTermEntryIdsBy(int $collectionId, string $language, string $term)
     - added static allowedProcessStatuses(string $current = 'unprocessed', bool $includeCurrent = true)

--- application/modules/editor/Controllers/TermController.php
+++ application/modules/editor/Controllers/TermController.php
@@ -25,6 +25,7 @@ START LICENSE AND COPYRIGHT
 
 END LICENSE AND COPYRIGHT
 */
+use editor_Models_Terminology_Models_TermModel as TermModel;
 
 /**
  *
@@ -39,10 +40,10 @@ class editor_TermController extends ZfExtended_RestController
     /**
      * @var string
      */
-    protected $entityClass = 'editor_Models_Terminology_Models_TermModel';
+    protected $entityClass = TermModel::class;
 
     /**
-     * @var editor_Models_Terminology_Models_TermModel
+     * @var TermModel
      */
     protected $entity;
 
@@ -128,6 +129,7 @@ class editor_TermController extends ZfExtended_RestController
                 'language' => $_['sourceLang']['rfc5646'],
                 'term' => trim($params['sourceTerm']),
                 'status' => 'preferredTerm', // which status should be set initially ?
+                'processStatus' => $this->getParam('sourceProcessStatus') ?? 'unprocessed',
             ], $te)->insert([
                 'userName' => $this->user()->getUserName(),
                 'userGuid' => $this->user()->getUserGuid()
@@ -147,6 +149,7 @@ class editor_TermController extends ZfExtended_RestController
             'language' => $params['language'],
             'term' => $params['term'],
             'status' => $termNoteStatus->getDefaultTermStatus(),
+            'processStatus' => $this->getParam('processStatus') ?? 'unprocessed',
         ], $te)->insert([
             'note' => trim($params['note'] ?? ''),
             'userName' => $this->user()->getUserName(),
@@ -178,6 +181,16 @@ class editor_TermController extends ZfExtended_RestController
             'termTbxId' => $this->entity->getTermTbxId(),
             'termEntryTbxId' => $this->entity->getTermEntryTbxId(),
         ]);
+
+        // If we checked whether those source and/or target terms already exist for theirs languages inside given collection
+        if ($this->getParam('checkExisting')) {
+
+            // Flush success
+            $this->view->assign([
+                'success' => true,
+                'msg' => 'Done'
+            ]);
+        }
     }
 
     /**
@@ -200,7 +213,7 @@ class editor_TermController extends ZfExtended_RestController
         $params = $this->getRequest()->getParams();
 
         // Validate params
-        return $this->jcheck([
+        $result = $this->jcheck([
             'collectionId' => [
                 'req' => true,                                                      // required
                 'rex' => 'int11',                                                   // regular expression preset key or raw expression
@@ -232,8 +245,78 @@ class editor_TermController extends ZfExtended_RestController
                 'req' => isset($params['sourceTerm']),
                 'rex' => 'rfc5646',
                 'key' => 'LEK_languages.rfc5646'
+            ],
+            'processStatus,sourceProcessStatus' => [
+                'fis' => join(',', TermModel::allowedProcessStatuses())
             ]
         ]);
+
+        // If we should check whether those source and/or target terms already exist for theirs languages inside given collection
+        if ($this->getParam('checkExisting')) {
+
+            // Define ternEntryIdA array
+            $termEntryIdA = ['target' => [], 'source' => []];
+
+            // Get array of termEntryIds where target term exists, if any
+            $termEntryIdA['target'] = $this->entity->searchTermEntryIdsBy(
+                $this->getParam('collectionId'),
+                $this->getParam('language'),
+                $this->getParam('term'),
+            );
+
+            // If source term is given
+            if ($result['sourceLang'] ?? 0) {
+
+                // Get termEntryId-array where source term exists, if any
+                $termEntryIdA['source'] = $this->entity->searchTermEntryIdsBy(
+                    $this->getParam('collectionId'),
+                    $this->getParam('sourceLang'),
+                    $this->getParam('sourceTerm'),
+                );
+            }
+
+            // If there is at least one termEntry where both source and target terms exists
+            if (array_intersect($termEntryIdA['target'], $termEntryIdA['source'])) {
+
+                // Flush failure
+                $this->jflush(false, 'Both terms are already existing within in selected term collection');
+
+            // Else if at least one termEntryId found for target term
+            } else if ($termEntryIdA['target']) {
+
+                // If we have source term proposal given - then we spoof the things
+                // so that source term to be attached as a translation for existing target term
+                if ($params['sourceTerm'] ?? 0) {
+
+                    // Spoof target-params with source-params and setup termEntryId-param
+                    $result['language'] = $result['sourceLang'];
+                    $this->setParam('language',      $params['sourceLang']);
+                    $this->setParam('term',          $params['sourceTerm']);
+                    $this->setParam('processStatus', $params['sourceProcessStatus']);
+                    $this->setParam('termEntryId',   $termEntryIdA['target'][0]);
+
+                    // Unset to prevent duplicates
+                    unset ($result['sourceLang']);
+
+                // Else flush failure
+                } else {
+                    $this->jflush(false, 'Term already exists in selected term collection');
+                }
+
+            // Else if at least one termEntryId found for source term
+            } else if ($termEntryIdA['source']) {
+
+                // Setup termEntryId-param so that target term will be
+                // attached as a translation for existing source term
+                $this->setParam('termEntryId', $termEntryIdA['source'][0]);
+
+                // Unset to prevent source term from being duplicated
+                unset ($result['sourceLang']);
+            }
+        }
+
+        // Return
+        return $result;
     }
 
     /**
@@ -285,7 +368,7 @@ class editor_TermController extends ZfExtended_RestController
      *
      * @param array $data
      * @param editor_Models_Terminology_Models_TermEntryModel $te
-     * @return editor_Models_Terminology_Models_TermModel
+     * @return TermModel
      */
     protected function _postTermInit(array $data, editor_Models_Terminology_Models_TermEntryModel $te) {
 
@@ -588,7 +671,7 @@ class editor_TermController extends ZfExtended_RestController
 
             // Fetch ids of ALL terms matching last search, excluding ids given by 'except'-param
             $termIds = ZfExtended_Factory
-                ::get('editor_Models_Terminology_Models_TermModel')
+                ::get(TermModel::class)
                 ->searchTermByParams(
                     $_SESSION['lastParams'] + ['except' => $this->getParam('except')],
                     $total
