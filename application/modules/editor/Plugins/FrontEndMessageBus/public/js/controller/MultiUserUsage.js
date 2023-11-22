a9b0938c0a5c5eb3c352c70a933cca2719240da4
Axel <axel@mittagqi.com>
2023-11-22 23:59:38 +0100

    Revert "Auxiliary commit to revert individual files from 162336edd1b89dbac9d95912a763e93fa9246e72"
    This reverts commit 680a311d7a8cbafd1963e33e7bcc05593baa4310.

--- application/modules/editor/Plugins/FrontEndMessageBus/public/js/controller/MultiUserUsage.js
+++ application/modules/editor/Plugins/FrontEndMessageBus/public/js/controller/MultiUserUsage.js
@@ -127,11 +127,9 @@ Ext.define('Editor.plugins.FrontEndMessageBus.controller.MultiUserUsage', {
         var schema = location.protocol.match('https') ? 'wss' : 'ws',
             host = conf.socketServer.httpHost || window.location.hostname,
             port = conf.socketServer.port || (schema === 'wss' ? 443 : 80),
-            path = conf.socketServer.route;
+            path = '/' + schema + conf.socketServer.route;
 
         url.push(schema, '://', host, ':', port, path);
-
-        console.log('MESSAGEBUS: ' + url.join(''));
 
         Ext.Ajax.setDefaultHeaders(Ext.apply({
             'X-Translate5-MessageBus-ConnId': conf.connectionId
