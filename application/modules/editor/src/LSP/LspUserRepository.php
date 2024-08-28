e5443857506600ef9555ef2143ce6be3294c015e
Leon <leon@mittagqi.com>
2024-08-28 09:35:31 +0300

    TRANSLATE-3676 CS fix

--- application/modules/editor/src/LSP/LspUserRepository.php
+++ application/modules/editor/src/LSP/LspUserRepository.php
@@ -34,7 +34,6 @@ use MittagQI\Translate5\LSP\Model\Db\LanguageServiceProviderTable;
 use MittagQI\Translate5\LSP\Model\Db\LanguageServiceProviderUserTable;
 use MittagQI\Translate5\LSP\Model\LanguageServiceProvider;
 use MittagQI\Translate5\LSP\Model\LanguageServiceProviderUser;
-use MittagQI\Translate5\Repository\LspRepository;
 use ZfExtended_Factory;
 use ZfExtended_Models_Entity_NotFoundException;
 use ZfExtended_Models_User;
