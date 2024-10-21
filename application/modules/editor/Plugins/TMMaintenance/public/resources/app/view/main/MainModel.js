641ddfa78934fe86022845a892d5fe5c2fcbc116
Pavel Perminov <pavel@mittagqi.com>
2024-10-21 08:27:01 +0000

    Merged in TRANSLATE-4228-change-limit-for-the-firs (pull request #1501)
    https://jira.translate5.net/browse/TRANSLATE-4228 Change limit for the first request in TM Maintenance
    
    * https://jira.translate5.net/browse/TRANSLATE-4228
    Change limit for the first request in TM Maintenance
    
    Plugins/TMMaintenance/
    1.locales/(en|de).json:list.loadingSegmentNumber: "{loading => Loading} segment #"
    2.TMMaintenance.view.main.MainModel
     - added data.loadingTotalAmount
     - formulas.listTitle(): amended the logic of how title is prepared
    3.TMMaintenance.view.main.MainController
     - moved onContainerScrollEnd and loadPageByChunks to SearchFormController
    4.TMMaintenance.view.main.SearchFormController
     - onSearchPress(): reduced chunkSize from 20 to 1 for loadPageByChunks() call
     - loadPageByChunks():
       - amended to load total amount only once first 20 are loaded
       - viewModel.loadingTotalAmount is toggled to indicate whether total amount request is currently running
    5.TMMaintenance.view.main.List
     - scrollable.listeners.scrollend: onContainerScrollEnd() is now called on searchform controller instead of app-main controller
    
    
    Approved-by: Leon Kyz

--- application/modules/editor/Plugins/TMMaintenance/public/resources/app/view/main/MainModel.js
+++ application/modules/editor/Plugins/TMMaintenance/public/resources/app/view/main/MainModel.js
@@ -13,21 +13,31 @@ Ext.define('TMMaintenance.view.main.MainModel', {
         languages: {},
         tms: {},
         totalAmount: false,
+        loadingTotalAmount: false,
         loadingRecordNumber: null
     },
     formulas: {
         listTitle: function(get) {
             var total = get('totalAmount'),
                 chunk = get('loadingRecordNumber'),
-                title = total === false ? get('l10n.list.title') : get('l10n.list.totalAmount');
+                title = '';
 
-            if (total === null) {
-                title += get('l10n.list.calculating');
-            } else if (total !== false) {
-                title += total;
-                if (chunk) {
-                    title += ', ' + get('l10n.list.loadingSegmentNumber') + chunk + ' ...';
+            if (get('loadingTotalAmount') || total !== null) {
+
+                title += total === false
+                    ? get('l10n.list.title')
+                    : get('l10n.list.totalAmount');
+
+                if (total === null) {
+                    title += get('l10n.list.calculating');
+                } else if (total !== false) {
+                    title += total;
                 }
+
+                title += ' ';
+            }
+            if (chunk) {
+                title += get('l10n.list.loadingSegmentNumber') + chunk + ' ...';
             }
             return  title;
         }
