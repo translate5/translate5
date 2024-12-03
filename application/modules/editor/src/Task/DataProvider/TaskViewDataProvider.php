0ab3d3c13a0e7e1aad4e1cd0e0acb9367933de97
Oleksandr Mikhliaiev <oleksandr@mittagqi.com>
2024-12-03 09:39:48 +0200

    TRANSLATE-3676 Filter user data in task payload

--- application/modules/editor/src/Task/DataProvider/TaskViewDataProvider.php
+++ application/modules/editor/src/Task/DataProvider/TaskViewDataProvider.php
@@ -30,9 +30,20 @@ declare(strict_types=1);
 
 namespace MittagQI\Translate5\Task\DataProvider;
 
+use editor_Models_Task as Task;
+use editor_Workflow_Anonymize;
+use MittagQI\Translate5\ActionAssert\Permission\ActionPermissionAssertInterface;
+use MittagQI\Translate5\ActionAssert\Permission\PermissionAssertContext;
+use MittagQI\Translate5\JobAssignment\JobAssignmentViewDataProvider;
+use MittagQI\Translate5\Repository\TaskUserTrackingRepository;
+use MittagQI\Translate5\Repository\UserRepository;
+use MittagQI\Translate5\User\ActionAssert\Permission\UserActionPermissionAssert;
+use MittagQI\Translate5\User\ActionAssert\UserAction;
 use MittagQI\Translate5\User\Model\User;
+use MittagQI\ZfExtended\Acl\SystemResource;
 use Zend_Db_Adapter_Abstract;
 use Zend_Db_Table;
+use ZfExtended_Acl;
 use ZfExtended_Models_Filter;
 
 class TaskViewDataProvider
@@ -40,6 +51,12 @@ class TaskViewDataProvider
     public function __construct(
         private readonly Zend_Db_Adapter_Abstract $db,
         private readonly TaskQuerySelectFactory $taskQuerySelectFactory,
+        private readonly JobAssignmentViewDataProvider $jobAssignmentViewDataProvider,
+        private readonly UserRepository $userRepository,
+        private readonly TaskUserTrackingRepository $userTrackingRepository,
+        private readonly ActionPermissionAssertInterface $userActionPermissionAssert,
+        private readonly editor_Workflow_Anonymize $anonymizer,
+        private readonly ZfExtended_Acl $acl,
     ) {
     }
 
@@ -51,6 +68,12 @@ class TaskViewDataProvider
         return new self(
             Zend_Db_Table::getDefaultAdapter(),
             TaskQuerySelectFactory::create(),
+            JobAssignmentViewDataProvider::create(),
+            new UserRepository(),
+            TaskUserTrackingRepository::create(),
+            UserActionPermissionAssert::create(),
+            new editor_Workflow_Anonymize(),
+            ZfExtended_Acl::getInstance(),
         );
     }
 
@@ -85,10 +108,130 @@ class TaskViewDataProvider
         $totalCount = $this->db->fetchOne($totalSelect);
         $tasks = $this->db->fetchAll($select);
 
+        foreach ($tasks as &$task) {
+            $task = $this->buildTaskView($task, $viewer);
+        }
+
         // TODO: extract logic from TaskController to here
         return [
             'totalCount' => $totalCount,
             'rows' => $tasks,
         ];
+    }
+
+    private function getUserTracking(string $taskGuid, bool $anonymizeUsers, User $viewer): array
+    {
+        $userTracking = $this->userTrackingRepository->getByTaskGuid($taskGuid);
+
+        if ($anonymizeUsers) {
+            return array_map(
+                fn ($rowTrack) => $this->anonymizer->anonymizeUserdata(
+                    $taskGuid,
+                    $rowTrack['userGuid'] ?? '',
+                    $rowTrack,
+                    $viewer->getUserGuid()
+                ),
+                $userTracking
+            );
+        }
+
+        $canSeeAllUsers = $this->acl->isInAllowedRoles(
+            $viewer->getRoles(),
+            SystemResource::ID,
+            SystemResource::SEE_ALL_USERS
+        );
+        $context = new PermissionAssertContext($viewer);
+
+        $resultList = [];
+
+        foreach ($userTracking as $rowTrack) {
+            if (empty($rowTrack['userGuid']) && $canSeeAllUsers) {
+                $resultList[] = $rowTrack;
+
+                continue;
+            }
+
+            $user = $this->userRepository->findByGuid($rowTrack['userGuid']);
+
+            if (! $user && $canSeeAllUsers) {
+                $resultList[] = $rowTrack;
+
+                continue;
+            }
+
+            if ($this->userActionPermissionAssert->isGranted(UserAction::Read, $user, $context)) {
+                $resultList[] = $rowTrack;
+
+                continue;
+            }
+
+            $resultList[] = $this->anonymizer->anonymizeUserdata(
+                $taskGuid,
+                $rowTrack['userGuid'],
+                $rowTrack,
+                $viewer->getUserGuid()
+            );
+        }
+
+        return $resultList;
+    }
+
+    public function buildTaskView(array $task, User $viewer): array
+    {
+        $taskModel = new Task();
+        $taskModel->init($task);
+        $anonymizeUsers = $taskModel->anonymizeUsers();
+
+        $task['users'] = $this->jobAssignmentViewDataProvider->getListFor($task['taskGuid'], $viewer);
+        $task['isUsed'] = false;
+        $task['lockingUsername'] = null;
+
+        foreach ($task['users'] as &$job) {
+            $viewersJob = $job['userGuid'] === $viewer->getUserGuid();
+            $matchingWorkflowStep = $task['workflowStepName'] === $job['workflowStepName'];
+
+            // we need an info about the current user in any case
+            // so we set info from first job of the current user,
+            // but we override info if a later assoc has the matching workflow step name
+            if ($viewersJob && (!isset($task['userRole']) || $matchingWorkflowStep)) {
+                $task['userRole'] = $job['role'];
+                $task['userState'] = $job['state'];
+                $task['userStep'] = $job['workflowStepName'];
+                // processing some trackchanges properties that can't be parted out to the trackchanges-plugin
+                $task['userTrackchangesShow'] = $job['trackchangesShow'];
+                $task['userTrackchangesShowAll'] = $job['trackchangesShowAll'];
+                $task['userTrackchangesAcceptReject'] = $job['trackchangesAcceptReject'];
+            }
+
+            $task['isUsed'] = $task['isUsed'] || !empty($job['usedState']);
+
+            if ($anonymizeUsers) {
+                $job = $this->anonymizer->anonymizeUserdata(
+                    $task['taskGuid'],
+                    $job['userGuid'],
+                    $job,
+                    $viewer->getUserGuid()
+                );
+            }
+        }
+
+        if (! empty($task['lockingUser'])) {
+            $user = $this->userRepository->findByGuid($task['lockingUser']);
+
+            $task['lockingUsername'] = $user?->getUsernameLong() ?? '- not found -';
+        }
+
+        $task['userTracking'] = $this->getUserTracking($task['taskGuid'], $anonymizeUsers, $viewer);
+
+        if (null !== $task['lockingUser'] && $anonymizeUsers) {
+            $task = $this->anonymizer->anonymizeUserdata(
+                $task['taskGuid'],
+                $task['lockingUser'],
+                $task,
+                $viewer->getUserGuid()
+            );
+        }
+
+        return $task;
     }
 }
