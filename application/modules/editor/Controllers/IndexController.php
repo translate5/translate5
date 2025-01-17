97cfb536ed3873555039a7c71cd6651b5c61d275
Volodymyr Kyianenko <volodymyr@mittagqi.com>
2025-01-17 15:43:18 +0000

    Merged in feature/TRANSLATE-4367-enable-the-rootcause-feedback-button-for-specific-users (pull request #1610)
    Feature/TRANSLATE-4367 enable the rootcause feedback button for specific users
    
    * TRANSLATE-4367 Enable rootcause feedback button for specific users
    
    * TRANSLATE-4367 Cleanup
    
    * TRANSLATE-4367 Update feedback form UI text
    
    * Merge branch 'develop' into feature/TRANSLATE-4367-enable-the-rootcause-feedback-button-for-specific-users
    
    
    Approved-by: Aleksandar Mitrev

--- application/modules/editor/Controllers/IndexController.php
+++ application/modules/editor/Controllers/IndexController.php
@@ -293,13 +293,13 @@ class Editor_IndexController extends ZfExtended_Controllers_Action
         $rop = $this->config->runtimeOptions;
 
         $this->view->enableJsLogger = $rop->debug && $rop->debug->enableJsLogger;
-        $this->view->enableJsLoggerFeedback = (! empty($rop->debug->enableJsLoggerFeedback) && str_contains(
+        // Video-recording: If allowed in general, then it can be set by the user after every login.
+        $this->view->Php2JsVars()->set('enableJsLoggerVideoConfig', $rop->debug && $rop->debug->enableJsLoggerVideo);
+        // Feedback button
+        $this->view->Php2JsVars()->set('enableJsLoggerFeedback', ! empty($rop->debug->enableJsLoggerFeedback) && str_contains(
             ',' . $rop->debug->enableJsLoggerFeedback . ',',
             ',' . ZfExtended_Authentication::getInstance()->getLogin() . ','
         ));
-
-        // Video-recording: If allowed in general, then it can be set by the user after every login.
-        $this->view->Php2JsVars()->set('enableJsLoggerVideoConfig', $rop->debug && $rop->debug->enableJsLoggerVideo);
 
         //for initial loading we have to set the restpath to empty string to trigger relative paths in the proxy setups
         $this->view->Php2JsVars()->set('restpath', '');
