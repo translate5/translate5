023a9fdc0c09ee582645002e37c2a964e909003a
Volodymyr Kyianenko <volodymyr@mittagqi.com>
2025-04-08 09:39:56 +0000

    Merge pull request #1741 in mittagqi/translate5 from feature/TRANSLATE-4558-tests-for-batch-set-properties to master
    Feature/TRANSLATE-4558 tests for batch set properties
    
    * Merge branch 'master' into feature/TRANSLATE-4558-tests-for-batch-set-properties
    * TRANSLATE-4558 Add integration tests for project filtering based requests
    * TRANSLATE-4558 Make tests non-fragile
    * TRANSLATE-4558 Add API test
    * TRANSLATE-4558 Update integration tests
    * Merge branch 'develop' into feature/TRANSLATE-4558-tests-for-batch-set-properties
    * TRANSLATE-4558 Add integration tests / fix batch setting issue
    * Merged in bugfix/TRANSLATE-3647-xliff-by-okapi-for-bconfs-in-zIp (pull request #1738)
    
    Fix Xliff by Okapi handling for BConfs in zip
    
    * Fix Xliff by Okapi handling for BConfs in zip
    
    
    Approved-by: Axel Becher
    * -
    * TRANSLATE-4558 Add unit tests and integration test skeletion
    * Merged in bugfix/TRANSLATE-3202-upgrade-to-okapi-version-1.4.7 (pull request #1710)
    
    TRANSLATE-3202 Fix Okapi version fetching in upgrade
    * TRANSLATE-3202 Fix Okapi version fetching in upgrade
    
    Approved by Oleksandr Mikhliaiev

---
+++ application/modules/editor/testcases/Unit/Workflow/BatchSet/TaskBatchSetDeadlineDateTest.php
@@ -0,0 +1,106 @@
+<?php
+/*
+START LICENSE AND COPYRIGHT
+
+ This file is part of translate5
+
+ Copyright (c) 2013 - 2024 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+
+ Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
+
+ This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
+ as published by the Free Software Foundation and appearing in the file agpl3-license.txt
+ included in the packaging of this file.  Please review the following information
+ to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3 requirements will be met:
+ http://www.gnu.org/licenses/agpl.html
+
+ There is a plugin exception available for use with this release of translate5 for
+ translate5: Please see http://www.translate5.net/plugin-exception.txt or
+ plugin-exception.txt in the root folder of translate5.
+
+ @copyright  Marc Mittag, MittagQI - Quality Informatics
+ @author     MittagQI - Quality Informatics
+ @license    GNU AFFERO GENERAL PUBLIC LICENSE version 3 with plugin-execption
+             http://www.gnu.org/licenses/agpl.html http://www.translate5.net/plugin-exception.txt
+
+END LICENSE AND COPYRIGHT
+*/
+
+declare(strict_types=1);
+
+namespace MittagQI\Translate5\Test\Unit\Workflow\BatchSet;
+
+use MittagQI\Translate5\JobAssignment\UserJob\BatchUpdate\UserJobDeadlineBatchUpdater;
+use MittagQI\Translate5\Repository\UserJobRepository;
+use MittagQI\Translate5\Task\BatchSet\BatchSetTaskGuidsProvider;
+use MittagQI\Translate5\Task\BatchSet\Exception\InvalidDeadlineDateStringProvidedException;
+use MittagQI\Translate5\Task\BatchSet\Exception\InvalidWorkflowProvidedException;
+use MittagQI\Translate5\Task\BatchSet\Exception\InvalidWorkflowStepProvidedException;
+use MittagQI\Translate5\Task\BatchSet\Setter\TaskBatchSetDeadlineDate;
+use PHPUnit\Framework\TestCase;
+use REST_Controller_Request_Http as Request;
+use ZfExtended_Logger;
+
+class TaskBatchSetDeadlineDateTest extends TestCase
+{
+    private const updateType = 'deadlineDate';
+
+    private TaskBatchSetDeadlineDate $taskBatchSetDeadlineDate;
+
+    protected function setUp(): void
+    {
+        $logger = $this->createMock(ZfExtended_Logger::class);
+        $userJobRepository = $this->createMock(UserJobRepository::class);
+        $userJobDeadlineBatchUpdater = $this->createMock(UserJobDeadlineBatchUpdater::class);
+        $taskGuidsProvider = $this->createMock(BatchSetTaskGuidsProvider::class);
+        $this->taskBatchSetDeadlineDate = new TaskBatchSetDeadlineDate($logger, $userJobRepository, $userJobDeadlineBatchUpdater, $taskGuidsProvider);
+    }
+
+    public function testSupportsDeadline(): void
+    {
+        self::assertTrue($this->taskBatchSetDeadlineDate->supports(self::updateType));
+    }
+
+    public function testEmptyWorkflow(): void
+    {
+        $request = self::getRequest();
+        $request->setParam('batchWorkflow', '');
+        $this->expectException(InvalidWorkflowProvidedException::class);
+        $this->taskBatchSetDeadlineDate->process($request);
+    }
+
+    public function testEmptyWorkflowStep(): void
+    {
+        $request = self::getRequest();
+        $request->setParam('batchWorkflowStep', '');
+        $this->expectException(InvalidWorkflowStepProvidedException::class);
+        $this->taskBatchSetDeadlineDate->process($request);
+    }
+
+    public function testEmptyDeadlineDate(): void
+    {
+        $request = self::getRequest();
+        $request->setParam('deadlineDate', '');
+        $this->expectException(InvalidDeadlineDateStringProvidedException::class);
+        $this->taskBatchSetDeadlineDate->process($request);
+    }
+
+    public function testIncorrectDeadlineDate(): void
+    {
+        $request = self::getRequest();
+        $request->setParam('deadlineDate', 'DUMMY_STRING');
+        $this->expectException(InvalidDeadlineDateStringProvidedException::class);
+        $this->taskBatchSetDeadlineDate->process($request);
+    }
+
+    private static function getRequest(): Request
+    {
+        $request = new Request();
+        $request->setParam('updateType', self::updateType);
+        $request->setParam('batchWorkflow', 'default');
+        $request->setParam('batchWorkflowStep', 'translation');
+        $request->setParam('deadlineDate', '2025-03-05T09:43:00');
+
+        return $request;
+    }
+}
