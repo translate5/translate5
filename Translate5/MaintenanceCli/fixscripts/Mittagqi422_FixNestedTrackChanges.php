7ea555b8b10f8e3b51dc5266f3517fc8bf90579e
Leon Kyz <leon@mittagqi.com>
2025-11-19 13:53:38 +0000

    Merge durchgeführt in hotfix/MITTAGQI-422-script-to-fix-nested-trackchanges (Pull-Request #2233)
    Hotfix/MITTAGQI-422 script to fix nested trackchanges
    
    * https://jira.translate5.net/browse/TRANSLATE-5117
    * progress
    
    * * finish feature
    
    * doc
    
    * * remove unnecessary dependencies that were resolved by adding SegmentTagSequence
    * add TrackChanges-Cleanup to Segment Preparation / Sanitizing (Why is that in a class "UpdateSegmentDtoFactory" ? Bad design ...)
    
    * Merge commit '38f2bb2f362dfee34af6107212547e9e1e73a799' into hotfix/TRANSLATE-5117-trackchanges-nested-del-in-ins
    
    * -
    
    * -
    
    * - splitting validate & fix to be possible to log nesting
    
    * * add logging for nested TrackChanges
    
    * -
    
    * - extract dependency
    
    * * more data for user in log-entry
    
    * -
    
    * PR changes
    
    * PR changes
    
    * https://jira.translate5.net/browse/MITTAGQI-422
    * improve fix-sript methodology by adding $doFix and making $doReport the default
    * adjust existing fix-script
    * Add basework for nested TrackChanges Fixer
    
    * * finished TrackChanges fix-script (UNTESTED!)
    * fix segment:setcontent command deficiancies
    
    * Merge branch 'master' into hotfix/MITTAGQI-422-script-to-fix-nested-trackchanges
    
    * -
    
    * -
    
    * * finalizations
    
    * -
    
    * MITTAGQI-422 Fix regex, fix dry-run message
    
    
    Genehmigt von: Thomas Lauria

---
+++ Translate5/MaintenanceCli/fixscripts/Mittagqi422_FixNestedTrackChanges.php
@@ -0,0 +1,137 @@
+<?php
+/*
+START LICENSE AND COPYRIGHT
+
+ This file is part of translate5
+
+ Copyright (c) 2013 - 2017 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+
+ Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
+
+ This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
+ as published by the Free Software Foundation and appearing in the file agpl3-license.txt
+ included in the packaging of this file.  Please review the following information
+ to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3 requirements will be met:
+ http://www.gnu.org/licenses/agpl.html
+
+ There is a plugin exception available for use with this release of translate5 for
+ translate5 plug-ins that are distributed under GNU AFFERO GENERAL PUBLIC LICENSE version 3:
+ Please see http://www.translate5.net/plugin-exception.txt or plugin-exception.txt in the root
+ folder of translate5.
+
+ @copyright  Marc Mittag, MittagQI - Quality Informatics
+ @author     MittagQI - Quality Informatics
+ @license    GNU AFFERO GENERAL PUBLIC LICENSE version 3 with plugin-execption
+             http://www.gnu.org/licenses/agpl.html http://www.translate5.net/plugin-exception.txt
+
+END LICENSE AND COPYRIGHT
+*/
+
+use MittagQI\Translate5\Segment\Tag\SegmentTagSequence;
+use Translate5\MaintenanceCli\FixScript\FixScriptAbstract;
+
+/**
+ * Fixes faulty (nested) TrackChanges-Tags due to problems with the new RichText-Editor
+ * The editor was released in ## [7.28.0] - 2025-08-15, so we just care for segments created after this date
+ */
+class Mittagqi422_FixNestedTrackChanges extends FixScriptAbstract
+{
+    public const TASK_BATCH = 10;
+
+    private array $affectedTaskGuids;
+
+    private int $numFixedSegments;
+
+    public function fix(): void
+    {
+        $this->numFixedSegments = 0;
+        // we fetch all task-guids after the release - these may be a few thousand for industry clients
+        $this->affectedTaskGuids = $this->db->fetchCol(
+            "SELECT `taskGuid` FROM LEK_task WHERE `created` > '2025-08-15 00:00:00'"
+        );
+        $this->info(count($this->affectedTaskGuids) . ' tasks potentially affected after 2025-08-15' . "\n");
+
+        $batch = [];
+        foreach ($this->affectedTaskGuids as $taskGuid) {
+            if (count($batch) > self::TASK_BATCH) {
+                $this->processTaskBatch($batch);
+                $batch = [];
+            }
+            $batch[] = $taskGuid;
+        }
+        if (count($batch) > 0) {
+            $this->processTaskBatch($batch);
+        }
+
+        if ($this->numFixedSegments > 0) {
+            $this->io->warning('Fixed ' . $this->numFixedSegments . ' segments.');
+        } else {
+            $this->io->success('No segments have to be fixed.');
+        }
+    }
+
+    private function processTaskBatch(array $taskGuids): void
+    {
+        $affectedSegments = $this->db->fetchAssoc(
+            "SELECT `segmentId`, `name` FROM `LEK_segment_data` WHERE" .
+            " `taskGuid` IN ('" . implode("','", $taskGuids) . "')" .
+            " AND (`edited` REGEXP '<ins[^>]*>(?:(?!<\/ins>)[\\\\s\\\\S])*<del' OR `edited` REGEXP '<del[^>]*>(?:(?!<\/del>)[\\\\s\\\\S])*<ins') OR `edited` REGEXP '<ins[^>]*>(?:(?!<\/ins>)[\\\\s\\\\S])*<ins' OR `edited` REGEXP '<del[^>]*>(?:(?!<\/del>)[\\\\s\\\\S])*<del'"
+        );
+        // array like: segmentId => [fields] where field ias "source" or "target" and will always be the edited variant ...
+        $segmentMap = [];
+        foreach ($affectedSegments as $segment) {
+            if (array_key_exists($segment['segmentId'], $segmentMap)) {
+                $segmentMap[$segment['segmentId']][] = $segment['name'];
+            } else {
+                $segmentMap[$segment['segmentId']] = [$segment['name']];
+            }
+        }
+        foreach ($segmentMap as $segmentId => $fields) {
+            $this->processSegment($segmentId, $fields);
+        }
+    }
+
+    private function processSegment(int $segmentId, array $fields): void
+    {
+        try {
+            $segment = ZfExtended_Factory::get(editor_Models_Segment::class);
+            $segment->load($segmentId);
+        } catch (ZfExtended_Models_Entity_NotFoundException $e) {
+            $this->error('Segment with id "' . $segmentId . '" could not be found or other errors occured: ' .
+                $e->getMessage());
+
+            return;
+        }
+
+        $savedFields = [];
+
+        foreach ($fields as $field) {
+            $markup = $segment->get($field . 'Edit');
+            $tags = new SegmentTagSequence($markup);
+
+            if ($tags->hasNestedTrackChangesTags()) {
+                $tags->normalizeTrackChangesTags();
+                $newMarkup = $tags->render();
+                $toSort = $segment->stripTags($newMarkup, $field === 'source');
+                if ($this->doFix) {
+                    $segment->set($field . 'Edit', $newMarkup);
+                    $segment->set($field . 'EditToSort', $toSort);
+                }
+
+                $savedFields[] = $field . 'Edit';
+            }
+        }
+
+        if (count($savedFields) > 0) {
+            if ($this->doFix) {
+                $segment->save();
+            }
+            $this->info(
+                $this->doFix ? 'Fixed ' : 'Will fix ' . 'Segment ' . $segmentId . ', Nr. ' . $segment->getSegmentNrInTask() .
+                ' in task ' . $segment->getTaskGuid() . ' where fields "' .
+                implode('", "', $savedFields) . '" were affected' . "\n"
+            );
+            $this->numFixedSegments++;
+        }
+    }
+}
