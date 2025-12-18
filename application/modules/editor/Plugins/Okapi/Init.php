e2946afc162d20b3ea62794b138d86b39548d1e1
Volodymyr Kyianenko <volodymyr@mittagqi.com>
2025-12-18 13:34:46 +0000

    Merged in bugfix/TRANSLATE-5160-modify-default-okapi-mif-settings (pull request #2267)
    TRANSLATE-5160: Modify default okapi MIF settings
    
    * TRANSLATE-5160: Modify default okapi MIF settings
    
    * Merge branch 'master' into bugfix/TRANSLATE-5160-modify-default-okapi-mif-settings
    
    * Merged develop into bugfix/TRANSLATE-5160-modify-default-okapi-mif-settings
    
    
    Approved-by: Axel Becher

--- application/modules/editor/Plugins/Okapi/Init.php
+++ application/modules/editor/Plugins/Okapi/Init.php
@@ -69,7 +69,7 @@ class editor_Plugins_Okapi_Init extends ZfExtended_Plugin_Abstract
      * This must be increased each time, a git-based fprm or srx is changed
      * @var int
      */
-    public const BCONF_VERSION_INDEX = 10;
+    public const BCONF_VERSION_INDEX = 11;
 
     /**
      * The filename of the system default import bconf
