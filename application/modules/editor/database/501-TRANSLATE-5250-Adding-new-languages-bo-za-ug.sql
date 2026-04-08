22e844397a38861e37a58868d4039ebf29283438
Stephan Bergmann <stephan@mittagqi.com>
2026-04-08 16:11:45 +0000

    Merged in feature/TRANSLATE-5250-Adding-new-languages-bo-za-ug (pull request #2429)
    Feature/TRANSLATE-5250 Adding new languages bo za ug
    
    * Merge branch 'develop' of bitbucket.org:mittagqi/translate5 into develop
    
    * Merge branch 'develop' of bitbucket.org:mittagqi/translate5 into develop
    
    * Merge branch 'develop' of bitbucket.org:mittagqi/translate5 into develop
    
    * Merge branch 'develop' of bitbucket.org:mittagqi/translate5 into develop
    
    * Merge branch 'develop' of bitbucket.org:mittagqi/translate5 into develop
    
    * Merge branch 'develop' of bitbucket.org:mittagqi/translate5 into develop
    
    * Merge branch 'develop' of bitbucket.org:mittagqi/translate5 into develop
    
    * Merge branch 'develop' of bitbucket.org:mittagqi/translate5 into develop
    
    * Merge branch 'develop' of bitbucket.org:mittagqi/translate5 into develop
    
    * Merge branch 'develop' of bitbucket.org:mittagqi/translate5 into develop
    
    * Merge branch 'develop' of bitbucket.org:mittagqi/translate5 into develop
    
    * Merge branch 'develop' of bitbucket.org:mittagqi/translate5 into develop
    
    * Merge branch 'develop' of bitbucket.org:mittagqi/translate5 into develop
    
    * Merge branch 'develop' of bitbucket.org:mittagqi/translate5 into develop
    
    * Merge branch 'develop' of bitbucket.org:mittagqi/translate5 into develop
    
    * Merge branch 'develop' of bitbucket.org:mittagqi/translate5 into develop
    
    * TRANSLATE-5250 Adding new languages (bo, za, ug)
    
    * TRANSLATE-5250 Adding new languages (bo, za, ug)
    
    * Merged develop into feature/TRANSLATE-5250-Adding-new-languages-bo-za-ug
    
    * Merged develop into feature/TRANSLATE-5250-Adding-new-languages-bo-za-ug
    
    * Merged develop into feature/TRANSLATE-5250-Adding-new-languages-bo-za-ug
    
    * TRANSLATE-5250 Adding new languages (bo, za, ug)
    revert faulty added lines
    
    * Merged develop into feature/TRANSLATE-5250-Adding-new-languages-bo-za-ug
    
    * TRANSLATE-5250 Adding new languages (bo, za, ug)
    revert faulty added lines
    revert wrongliny commited change
    
    * Merge branch 'feature/TRANSLATE-5250-Adding-new-languages-bo-za-ug' of bitbucket.org:mittagqi/translate5 into feature/TRANSLATE-5250-Adding-new-languages-bo-za-ug
    
    
    Approved-by: Aleksandar Mitrev

---
+++ application/modules/editor/database/501-TRANSLATE-5250-Adding-new-languages-bo-za-ug.sql
@@ -0,0 +1,54 @@
+-- /*
+-- START LICENSE AND COPYRIGHT
+--
+--  This file is part of translate5
+--
+--  Copyright (c) 2013 - '.(date('Y')).' Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+--
+--  Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
+--
+--  This file may be used under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE version 3
+--  as published by the Free Software Foundation and appearing in the file agpl3-license.txt
+--  included in the packaging of this file.  Please review the following information
+--  to ensure the GNU AFFERO GENERAL PUBLIC LICENSE version 3 requirements will be met:
+--  http://www.gnu.org/licenses/agpl.html
+--
+--  There is a plugin exception available for use with this release of translate5 for
+--  translate5: Please see http://www.translate5.net/plugin-exception.txt or
+--  plugin-exception.txt in the root folder of translate5.
+--
+--  @copyright  Marc Mittag, MittagQI - Quality Informatics
+--  @author     MittagQI - Quality Informatics
+--  @license    GNU AFFERO GENERAL PUBLIC LICENSE version 3 with plugin-execption
+-- 			 http://www.gnu.org/licenses/agpl.html http://www.translate5.net/plugin-exception.txt
+--
+-- END LICENSE AND COPYRIGHT
+-- */
+
+
+/* add new language 'Zhuang' */
+INSERT INTO LEK_languages SET
+                                `langName` = 'Zhuang',
+                                `lcid` = NULL,
+                                `rfc5646` = 'za',
+                                `iso3166Part1alpha2` = 'cn',
+                                `sublanguage` = 'za-CN',
+                                `rtl` = 0,
+                                `iso6393` = 'zha',
+                                `hidden` = 0
+ON DUPLICATE KEY UPDATE id=id;
+
+
+
+/* update existing language 'Tibetisch' */
+UPDATE LEK_languages SET
+    `iso6393` = 'bod'
+WHERE `rfc5646` = 'bo';
+
+/* update existing language 'Uigurisch' */
+UPDATE LEK_languages SET
+    `lcid` = 1152,
+    `iso3166Part1alpha2` = 'cn',
+    `sublanguage` = 'ug-CN',
+    `iso6393` = 'uig'
+WHERE `rfc5646` = 'ug';
